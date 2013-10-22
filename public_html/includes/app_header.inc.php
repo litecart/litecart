050522cd8dbc7f532f4900453b42e64a0a5885a4
timint <info@tim-international.net>
2013-10-22 04:39:20 +0200

    * version change

--- public_html/includes/app_header.inc.php
+++ public_html/includes/app_header.inc.php
@@ -1,7 +1,7 @@
 <?php
   
   define('PLATFORM_NAME', 'LiteCart');
-  define('PLATFORM_VERSION', '1.0.1.3');
+  define('PLATFORM_VERSION', '1.0.1.4');
   
 // Start redirecting output to the output buffer
   ob_start();
