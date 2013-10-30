0f382864e0ecefc69070df2c59c613aac4a4bc17
timint <info@tim-international.net>
2013-10-30 14:10:46 +0100

    * Active class for currency and language

--- public_html/includes/boxes/currencies.inc.php
+++ public_html/includes/boxes/currencies.inc.php
@@ -5,7 +5,7 @@
 <?php
   foreach ($system->currency->currencies as $currency) {
     if ($currency['status']) {
-      echo '<li><a href="javascript:set_currency(\''. $currency['code'] .'\');">'. $currency['code'] .'</a></li>' . PHP_EOL;
+      echo '<li'. (($currency['code'] == $system->currency->selected['code']) ? ' class="active"' : '') .'><a href="javascript:set_currency(\''. $currency['code'] .'\');">'. $currency['code'] .'</a></li>' . PHP_EOL;
     }
   }
 ?>
