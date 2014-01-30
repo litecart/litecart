eda2b18b486c194c465b6d67f8fe6adfb93bec79
timint <info@tim-international.net>
2014-01-30 04:52:10 +0100

    - Remove uncompressed jqplot files * Report widget lines to bars

---
+++ public_html/install/upgrade_patches/1.1.1.inc.php
@@ -0,0 +1,45 @@
+<?php
+  $deleted_files = array(
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.barRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.BezierCurveRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.blockRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.bubbleRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.canvasAxisLabelRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.canvasAxisTickRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.canvasOverlay.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.canvasTextRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.categoryAxisRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.ciParser.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.cursor.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.dateAxisRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.donutRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.dragable.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.enhancedLegendRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.funnelRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.highlighter.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.json2.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.logAxisRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.mekkoAxisRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.mekkoRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.meterGaugeRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.mobile.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.ohlcRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.pieRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.pointLabels.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.pyramidAxisRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.pyramidGridRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.pyramidRenderer.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/plugins/jqplot.trendline.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/excanvas.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/jquery.jqplot.css',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/jquery.jqplot.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/jquery.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jqplot/jquery.min.js',
+  );
+  
+  foreach ($deleted_files as $pattern) {
+    if (!file_delete($pattern)) {
+      die('<span class="error">[Error]</span></p>');
+    }
+  }
+?>
