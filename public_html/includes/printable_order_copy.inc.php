65bed4587702fb27154ed9ca4ca85f0fbe2f3b22
timint <info@tim-international.net>
2014-03-12 01:09:05 +0100

    + Tax percentage in order copy

--- public_html/includes/printable_order_copy.inc.php
+++ public_html/includes/printable_order_copy.inc.php
@@ -79,11 +79,11 @@
         <td align="left"><?php echo $item['sku']; ?></td>
       <?php if (!empty(customer::$data['display_prices_including_tax'])) { ?>
         <td style="padding: 5px 10px; white-space: nowrap; text-align: right; width: 100px;"><?php echo currency::format($item['price'] + $item['tax'], false, false, $order['currency_code'], $order['currency_value']); ?></td>
-        <td style="padding: 5px 10px; white-space: nowrap; text-align: right; width: 100px;"><?php echo currency::format($item['tax'], false, false, $order['currency_code'], $order['currency_value']); ?></td>
+        <td style="padding: 5px 10px; white-space: nowrap; text-align: right; width: 100px;"><?php echo currency::format($item['tax'], false, false, $order['currency_code'], $order['currency_value']); ?> (<?php echo round($item['tax']/$item['price']*100); ?> %)</td>
         <td style="padding: 5px 10px; white-space: nowrap; text-align: right; width: 100px;"><?php echo currency::format($item['quantity'] * ($item['price'] + $item['tax']), false, false, $order['currency_code'], $order['currency_value']); ?></td>
       <?php } else { ?>
         <td style="padding: 5px 10px; white-space: nowrap; text-align: right; width: 100px;"><?php echo currency::format($item['price'], false, false, $order['currency_code'], $order['currency_value']); ?></td>
-        <td style="padding: 5px 10px; white-space: nowrap; text-align: right; width: 100px;"><?php echo currency::format($item['tax'], false, false, $order['currency_code'], $order['currency_value']); ?></td>
+        <td style="padding: 5px 10px; white-space: nowrap; text-align: right; width: 100px;"><?php echo currency::format($item['tax'], false, false, $order['currency_code'], $order['currency_value']); ?> (<?php echo round($item['tax']/$item['price']*100); ?> %)</td>
         <td style="padding: 5px 10px; white-space: nowrap; text-align: right; width: 100px;"><?php echo currency::format($item['quantity'] * $item['price'], false, false, $order['currency_code'], $order['currency_value']); ?></td>
       <?php } ?>
       </tr>
