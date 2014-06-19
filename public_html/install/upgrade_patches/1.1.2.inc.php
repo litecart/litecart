5865caba68673f2bbf181eb16794d4e0bbe4dbf5
timint <info@tim-international.net>
2014-06-19 18:24:32 +0200

    - Remove jQuery 1.10.2

--- public_html/install/upgrade_patches/1.1.2.inc.php
+++ public_html/install/upgrade_patches/1.1.2.inc.php
@@ -1,4 +1,13 @@
 <?php
-  // Nothing to process
+  $deleted_files = array(
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jquery/jquery-1.10.2.min.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT . 'jquery/jquery-1.10.2.min.map',
+  );
+  
+  foreach ($deleted_files as $pattern) {
+    if (!file_delete($pattern)) {
+      die('<span class="error">[Error]</span></p>');
+    }
+  }
 
 ?>
