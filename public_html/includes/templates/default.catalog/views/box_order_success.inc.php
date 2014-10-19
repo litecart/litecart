9b2de36476b001460add11844f58eccf39354025
timint <info@tim-international.net>
2014-10-19 02:12:05 +0200

    * Fix order success fancybox

--- public_html/includes/templates/default.catalog/views/box_order_success.inc.php
+++ public_html/includes/templates/default.catalog/views/box_order_success.inc.php
@@ -4,7 +4,7 @@
   </div>
   <div class="content">
     <p><?php echo language::translate('description_order_completed', 'Thank you for shopping in our store. We will process your order shortly.'); ?></p>
-    <p><a href="<?php echo htmlspecialchars($printable_link); ?>"><?php echo language::translate('description_click_printable_copy', 'Click here for a printable copy'); ?></a></p>
+    <p><a href="<?php echo htmlspecialchars($printable_link); ?>" class="fancybox"><?php echo language::translate('description_click_printable_copy', 'Click here for a printable copy'); ?></a></p>
     
     <?php if ($payment_receipt) { ?>
     <?php echo $payment_receipt; ?>
