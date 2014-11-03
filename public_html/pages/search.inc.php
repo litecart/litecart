3ec286aa37cb481e1df85ef884721be811488599
timint <info@tim-international.net>
2014-11-03 18:24:40 +0100

    * Fix issues with search results

--- public_html/pages/search.inc.php
+++ public_html/pages/search.inc.php
@@ -58,7 +58,7 @@
     
     $category_ids = array();
     while ($category = database::fetch($categories_info_query)) {
-      $category_ids[] = "find_in_set('". (int)$category['id'] ."', p.categories)";
+      $category_ids[] = (int)$category['id'];
     }
     
     $manufacturers_info_query = database::query(
@@ -74,33 +74,84 @@
     
     $manufacturer_ids = array();
     while ($manufacturer = database::fetch($manufacturers_info_query)) {
-      $manufacturer_ids[] = $manufacturer['id'];
+      $manufacturer_ids[] = (int)$manufacturer['id'];
     }
     
-    $products_query = database::query(
-      "select p.id
-      from ". DB_TABLE_PRODUCTS ." p
-      left join ". DB_TABLE_PRODUCTS_INFO ." pi on (pi.product_id = p.id and pi.language_code = '". database::input(language::$selected['code']) ."')
-      where p.status
-      and (
-        find_in_set('". database::input($_GET['query']) ."', p.keywords)
-        ". (!empty($category_ids) ? "or " . implode(" or ", $category_ids) : false) ."
-        ". (!empty($manufacturer_ids) ? "or p.manufacturer_id in ('". implode("', '", $manufacturer_ids) . "')" : false) ."
-        or p.code like '%". database::input($_GET['query']) ."%'
+    $sql_select_occurrences = "(0
+      + if(p.upc like '%". database::input($_GET['query']) ."%', 3, 0)
+      + if(p.sku like '%". database::input($_GET['query']) ."%', 3, 0)
+      + if(p.code like '%". database::input($_GET['query']) ."%', 3, 0)
+      + if(p.keywords like '%". database::input($_GET['query']) ."%', 2, 0)
+      + if(pi.name like '%". database::input($_GET['query']) ."%', 2, 0)
+      + if(pi.short_description like '%". database::input($_GET['query']) ."%', 1, 0)
+      + if(pi.description like '%". database::input($_GET['query']) ."%', 1, 0)
+      + if(find_in_set('". implode("', ", $category_ids) ."', p.categories), 1, 0)
+      + if(p.manufacturer_id and p.manufacturer_id in ('". implode("', '", database::input($manufacturer_ids)) ."'), 1, 0)
+    ) as occurrences";
+    
+    $sql_price_column = "if(pp.`". database::input(currency::$selected['code']) ."`, pp.`". database::input(currency::$selected['code']) ."` / ". (float)currency::$selected['value'] .", pp.`". database::input(settings::get('store_currency_code')) ."`)";
+    $sql_campaign_price_column = "if(`". database::input(currency::$selected['code']) ."`, `". database::input(currency::$selected['code']) ."` / ". (float)currency::$selected['value'] .", `". database::input(settings::get('store_currency_code')) ."`)";
+    
+    $query = 
+      "select p.*, pi.name, pi.short_description, m.name as manufacturer_name, ". $sql_price_column ." as price, pc.campaign_price, if(pc.campaign_price, pc.campaign_price, if(pc.campaign_price, pc.campaign_price, ". $sql_price_column .")) as final_price, " . $sql_select_occurrences ."
+      
+      from (
+        select id, code, upc, sku, manufacturer_id, categories, keywords, product_groups, image, tax_class_id, quantity, views, date_created
+        from ". DB_TABLE_PRODUCTS ."
+        where status
+        and (date_valid_from <= '". date('Y-m-d H:i:s') ."')
+        and (year(date_valid_to) < '1971' or date_valid_to >= '". date('Y-m-d H:i:s') ."')
+      ) p
+      
+      left join ". DB_TABLE_PRODUCTS_INFO ." pi on (pi.product_id = p.id and pi.language_code = '". language::$selected['code'] ."')
+      left join ". DB_TABLE_MANUFACTURERS ." m on (m.id = p.manufacturer_id)
+      left join ". DB_TABLE_PRODUCTS_PRICES ." pp on (pp.product_id = p.id)
+      left join (
+        select product_id, ". $sql_campaign_price_column ." as campaign_price
+        from ". DB_TABLE_PRODUCTS_CAMPAIGNS ."
+        where (start_date <= '". date('Y-m-d H:i:s') ."')
+        and (year(end_date) < '1971' or end_date >= '". date('Y-m-d H:i:s') ."')
+        order by end_date asc
+      ) pc on (pc.product_id = p.id)
+      
+      where (
+        p.code like '%". database::input($_GET['query']) ."%'
         or p.sku like '%". database::input($_GET['query']) ."%'
         or p.upc like '%". database::input($_GET['query']) ."%'
+        or p.keywords like '%". database::input($_GET['query']) ."%'
+        ". (!empty($category_ids) ? "or find_in_set('". implode("', ", $category_ids) ."', p.categories)" : false) ."
+        ". (!empty($manufacturer_ids) ? "or p.manufacturer_id in ('". implode("', '", $manufacturer_ids) ."')" : false) ."
         or pi.name like '%". database::input($_GET['query']) ."%'
         or pi.short_description like '%". database::input($_GET['query']) ."%'
         or pi.description like '%". database::input($_GET['query']) ."%'
-      );"
-    );
+      )
+      order by %sql_sort;
+    ";
     
-    $product_ids = array();
-    while ($product = database::fetch($products_query)) {
-      $product_ids[] = $product['id'];
+    switch($_GET['sort']) {
+      case 'name':
+        $query = str_replace("%sql_sort", "name asc", $query);
+        break;
+      case 'price':
+        $query = str_replace("%sql_sort", "final_price asc", $query);
+        break;
+      case 'date':
+        $query = str_replace("%sql_sort", "date_created desc", $query);
+        break;
+      case 'occurrences':
+        $query = str_replace("%sql_sort", "occurrences desc", $query);
+        break;
+      case 'rand':
+        $query = str_replace("%sql_sort", "rand()", $query);
+        break;
+      case 'popularity':
+      default:
+        $query = str_replace("%sql_sort", "(p.views / timestampdiff(day, now(), from_unixtime(p.date_created))) desc", $query);
+        $sql_global_sort = "";
+        break;
     }
     
-    $products_query = functions::catalog_products_query(array('products' => $product_ids, 'sort' => $_GET['sort']));
+    $products_query = database::query($query);
     if (database::num_rows($products_query) > 0) {
     
       if ($_GET['page'] > 1) database::seek($products_query, (settings::get('items_per_page') * ($_GET['page']-1)));
