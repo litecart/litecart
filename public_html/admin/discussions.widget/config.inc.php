13a5d98b4d9a9f4d85b362e5d9305821d6e7d0cc
timint <info@tim-international.net>
2014-11-26 03:32:24 +0100

    + Discussions widget

---
+++ public_html/admin/discussions.widget/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$widget_config = array(
+  'name' => language::translate('title_discussions', 'Discussions'),
+  'file' => 'discussions.inc.php',
+  'priority' => 4,
+);
+
+?>
---
+++ public_html/admin/discussions.widget/discussions.inc.php
@@ -0,0 +1,73 @@
+<?php
+  
+  $cache_file = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'discussions.cache';
+  
+  if (file_exists($cache_file) && filemtime($cache_file) > strtotime('-6 hours')) {
+    echo file_get_contents($cache_file);
+    return;
+  }
+  
+  $url = document::link('http://forums.litecart.net/feed/rss/');
+  
+  $rss = functions::http_fetch($url);
+  
+  $rss = simplexml_load_string($rss);
+  
+  if (empty($rss->channel->item)) return;
+  
+  $columns = array();
+  
+  $col = 0;
+  $count = 0;
+  $total = 0;
+  foreach ($rss->channel->item as $item) {
+    if (!isset($count) || $count == 3) {
+      $count = 0;
+      $col++;
+    }
+    $columns[$col][] = $item;
+    $count++;
+    $total++;
+    if ($total == 12) break;
+  }
+  
+  ob_start();
+?>
+<div class="widget">
+  <table style="width: 100%;" class="dataTable">
+    <tr class="header">
+      <th colspan="4" align="left"><?php echo language::translate('title_most_recent_forum_topics', 'Most Recent Forum Topics'); ?></th>
+    </tr>
+    <tr>
+<?php
+  foreach ($columns as $column) {
+    echo '<td style="vertical-align: top;">' . PHP_EOL
+       . '  <table style="width: 100%;">' . PHP_EOL;
+    foreach ($column as $item) {
+      if (!isset($rowclass) || $rowclass == 'even') {
+        $rowclass = 'odd';
+      } else {
+        $rowclass = 'even';
+      }
+?>
+        <tr>
+          <td><a href="<?php echo (string)$item->link; ?>" target="_blank"><?php echo (string)$item->title; ?></a><br/>
+            <span style="color: #666;"><?php echo strftime('%e %b', strtotime($item->pubDate)); ?> <?php echo language::translate('text_by', 'by'); ?> <?php echo (string)$item->author; ?></span>
+          </td>
+        </tr>
+<?php
+    }
+    echo '  </table>' . PHP_EOL
+       . '</td>' . PHP_EOL;
+  }
+?>
+    </tr>
+  </table>
+</div>
+<?php
+  $buffer = ob_get_clean();
+  
+  file_put_contents($cache_file, $buffer);
+  
+  echo $buffer;
+?>
