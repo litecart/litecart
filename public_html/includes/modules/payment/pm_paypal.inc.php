cc19efacb4d8a5c1a08d3c8fa37fefcc30b94df7
timint <info@tim-international.net>
2014-12-08 03:24:49 +0100

    * Fix issues with discount in Paypal

--- public_html/includes/modules/payment/pm_paypal.inc.php
+++ public_html/includes/modules/payment/pm_paypal.inc.php
@@ -6,7 +6,7 @@
     public $name = 'Paypal Standard';
     public $description = '';
     public $author = 'LiteCart Dev Team';
-    public $version = '1.0.5';
+    public $version = '1.1';
     public $website = 'https://www.paypal.com/cgi-bin/webscr?cmd=_help';
     public $priority = 0;
     
@@ -20,7 +20,7 @@
       
     // If not in geo zone
       if (!empty($this->settings['geo_zone_id'])) {
-        if (!$GLOBALS['system']->functions->reference_in_geo_zone($this->settings['geo_zone_id'], $customer['country_code'], $customer['zone_code'])) return;
+        if (!functions::reference_in_geo_zone($this->settings['geo_zone_id'], $customer['country_code'], $customer['zone_code'])) return;
       }
       
       if (empty($this->settings['merchant_email'])) return;
@@ -31,12 +31,12 @@
           array(
             'id' => 'card',
             'icon' => $this->settings['icon'],
-            'name' => $GLOBALS['system']->language->translate(__CLASS__.':title_card_payment', 'Card Payment'),
-            'description' => $GLOBALS['system']->language->translate(__CLASS__.':description', 'Secure and simple money transactions made by Paypal.'),
+            'name' => language::translate(__CLASS__.':title_card_payment', 'Card Payment'),
+            'description' => language::translate(__CLASS__.':description', 'Secure and simple money transactions made by Paypal.'),
             'fields' => '',
             'cost' => 0,
             'tax_class_id' => 0,
-            'confirm' => $GLOBALS['system']->language->translate(__CLASS__.':title_pay_now', 'Pay Now'),
+            'confirm' => language::translate(__CLASS__.':title_pay_now', 'Pay Now'),
           ),
         )
       );
@@ -58,11 +58,11 @@
         'rm'            => '0',
         'business'      => $this->settings['merchant_email'],
         'currency_code' => $order->data['currency_code'],
-        'cbt'           => $GLOBALS['system']->language->translate('paypal:title_finalize_order', 'Finalize Order'),
-        'return'        => $GLOBALS['system']->document->link('order_process.php'),
-        'cancel_return' => $GLOBALS['system']->document->link('checkout.php'),
-        //'notify_url'    => $GLOBALS['system']->document->link('callback.php', array('order_uid' => $order->data['uid'])), // We're not using IPN callbacks
-        'charset'       => $GLOBALS['system']->language->selected['charset'],
+        'cbt'           => language::translate('paypal:title_finalize_order', 'Finalize Order'),
+        'return'        => document::link('order_process.php'),
+        'cancel_return' => document::link('checkout.php'),
+        //'notify_url'    => document::link('callback.php', array('order_uid' => $order->data['uid'])), // We're not using IPN callbacks
+        'charset'       => language::$selected['charset'],
         'custom'        => $order->data['uid'],
       );
       
@@ -72,11 +72,15 @@
     // Reason: Paypal don't do discounts if orders specify tax
       $order_contains_discount = false;
       foreach ($order->data['items'] as $item) {
-        if ($item['price'] < 0) $order_contains_discount = true;
+        if ($item['price'] < 0) {
+          $order_contains_discount = true;
+          break;
+        }
       }
       foreach ($order->data['order_total'] as $row) {
         if ($row['calculate'] && $row['value'] < 0) {
           $order_contains_discount = true;
+          break;
         }
       }
       
@@ -86,12 +90,13 @@
         foreach ($order->data['items'] as $item) {
           if ($item['price'] < 0) {
             if (!isset($fields['discount_amount_cart'])) $fields['discount_amount_cart'] = 0;
-            $fields['discount_amount_cart'] += (-$item['price'] + -$item['tax']) * $item['quantity'];
+            $fields['discount_amount_cart'] += $item['quantity'] * $this->_format_raw(-$item['price'], $order->data['currency_code'], $order->data['currency_value']);
+            $fields['discount_amount_cart'] += $item['quantity'] * $this->_format_raw(-$item['tax'], $order->data['currency_code'], $order->data['currency_value']);
           } else {
             $fields['item_name_'.$item_no] = $item['name'];
             $fields['item_number_'.$item_no] = $item['product_id'] . (!empty($item['option_id']) ? ':'.$item['product_id'] : '');
             $fields['quantity_'.$item_no] = $item['quantity'];
-            $fields['amount_'.$item_no] = $GLOBALS['system']->currency->format($item['price'], false, true) + $GLOBALS['system']->currency->format($item['tax'], false, true);
+            $fields['amount_'.$item_no] = $this->_format_raw($item['price'], $order->data['currency_code'], $order->data['currency_value']) + $this->_format_raw($item['tax'], $order->data['currency_code'], $order->data['currency_value']);
             $item_no++;
           }
         }
@@ -100,12 +105,13 @@
           if ($row['calculate']) {
             if ($row['value'] < 0) {
               if (!isset($fields['discount_amount_cart'])) $fields['discount_amount_cart'] = 0;
-              $fields['discount_amount_cart'] += -$row['value'] + -$row['tax'];
+              $fields['discount_amount_cart'] += $this->_format_raw(-$row['value'], $order->data['currency_code'], $order->data['currency_value']);
+              $fields['discount_amount_cart'] += $this->_format_raw(-$row['tax'], $order->data['currency_code'], $order->data['currency_value']);
             } else {
               $fields['item_name_'.$item_no] = $row['title'];
               $fields['item_number_'.$item_no] = $row['module_id'];
               $fields['quantity_'.$item_no] = '1';
-              $fields['amount_'.$item_no] = $GLOBALS['system']->currency->format($row['value'], false, true, $order->data['currency_code'], $order->data['currency_value']) + $GLOBALS['system']->currency->format($row['tax'], false, true, $order->data['currency_code'], $order->data['currency_value']);
+              $fields['amount_'.$item_no] = $this->_format_raw($row['value'], $order->data['currency_code'], $order->data['currency_value']) + $this->_format_raw($row['tax'], $order->data['currency_code'], $order->data['currency_value']);
               $item_no++;
             }
           }
@@ -118,8 +124,8 @@
           $fields['item_name_'.$item_no] = $item['name'];
           $fields['item_number_'.$item_no] = $item['product_id'] . (!empty($item['option_id']) ? ':'.$item['product_id'] : '');
           $fields['quantity_'.$item_no] = $item['quantity'];
-          $fields['amount_'.$item_no] = $GLOBALS['system']->currency->format($item['price'], false, true, $order->data['currency_code'], $order->data['currency_value']);
-          $fields['tax_'.$item_no] = $GLOBALS['system']->currency->format($item['tax'], false, true, $order->data['currency_code'], $order->data['currency_value']);
+          $fields['amount_'.$item_no] = $this->_format_raw($item['price'], $order->data['currency_code'], $order->data['currency_value']);
+          $fields['tax_'.$item_no] = $this->_format_raw($item['tax'], $order->data['currency_code'], $order->data['currency_value']);
           $item_no++;
         }
         
@@ -128,8 +134,8 @@
             $fields['item_name_'.$item_no] = $row['title'];
             $fields['item_number_'.$item_no] = $row['module_id'];
             $fields['quantity_'.$item_no] = '1';
-            $fields['amount_'.$item_no] = $GLOBALS['system']->currency->format($row['value'], false, true, $order->data['currency_code'], $order->data['currency_value']);
-            $fields['tax_'.$item_no] = $GLOBALS['system']->currency->format($row['tax'], false, true, $order->data['currency_code'], $order->data['currency_value']);
+            $fields['amount_'.$item_no] = $this->_format_raw($row['value'], $order->data['currency_code'], $order->data['currency_value']);
+            $fields['tax_'.$item_no] = $this->_format_raw($row['tax'], $order->data['currency_code'], $order->data['currency_value']);
             $item_no++;
           }
         }
@@ -148,8 +154,7 @@
       );
     }
     
-    public function verify() {
-      global $order;
+    public function verify($order) {
       
       $errors = array();
       
@@ -172,7 +177,7 @@
         $gateway_url = 'https://www.sandbox.paypal.com/cgi-bin/webscr';
       }
       
-      $result = $GLOBALS['system']->functions->http_fetch($gateway_url, $post_fields);
+      $result = functions::http_fetch($gateway_url, $post_fields);
       $result = explode("\n", $result);
       
       $txdata = array();
@@ -186,17 +191,17 @@
     // Calculate order total in order currency
       $order_total = 0;
       foreach ($order->data['items'] as $item) {
-        $order_total += $GLOBALS['system']->currency->format($item['quantity'] * $item['price'], false, true, $order->data['currency_code'], $order->data['currency_value']);
-        $order_total += $GLOBALS['system']->currency->format($item['quantity'] * $item['tax'], false, true, $order->data['currency_code'], $order->data['currency_value']);
+        $order_total += $this->_format_raw($item['quantity'] * $item['price'], $order->data['currency_code'], $order->data['currency_value']);
+        $order_total += $this->_format_raw($item['quantity'] * $item['tax'], $order->data['currency_code'], $order->data['currency_value']);
       }
       foreach ($order->data['order_total'] as $row) {
         if ($row['calculate']) {
-          $order_total += $GLOBALS['system']->currency->format($row['value'], false, true, $order->data['currency_code'], $order->data['currency_value']);
-          $order_total += $GLOBALS['system']->currency->format($row['tax'], false, true, $order->data['currency_code'], $order->data['currency_value']);
+          $order_total += $this->_format_raw($row['value'], $order->data['currency_code'], $order->data['currency_value']);
+          $order_total += $this->_format_raw($row['tax'], $order->data['currency_code'], $order->data['currency_value']);
         }
       }
       
-      if (empty($txdata)) $errors[] = $GLOBALS['system']->language->translate(__CLASS__.':error_transaction_not_verified', 'Error: Payment transaction could not be verified by Paypal.');
+      if (empty($txdata)) $errors[] = language::translate(__CLASS__.':error_transaction_not_verified', 'Error: Payment transaction could not be verified by Paypal.');
       if ($txdata['payment_status'] != 'Completed') $errors[] = 'Payment status indicates not completed.';
       if (round($txdata['mc_gross']) != round($order_total)) $errors[] = 'Payment amount '. $txdata['mc_gross'] .' is not equal to order amount ('. $order_total .').';
       if ($txdata['mc_currency'] != $order->data['currency_code']) $errors[] = 'Payment currency ('. $txdata['mc_currency'] .') should have been '. $order->data['currency_code'];
@@ -218,69 +223,73 @@
     public function callback() {
     }
     
+    private function _format_raw($value, $currency_code, $currency_value) {
+      return number_format($value * $currency_value, currency::$currencies[$currency_code]['decimals'], '.', '');
+    }
+    
     function settings() {
       return array(
         array(
           'key' => 'status',
           'default_value' => '1',
-          'title' => $GLOBALS['system']->language->translate(__CLASS__.':title_status', 'Status'),
-          'description' => $GLOBALS['system']->language->translate(__CLASS__.':description_status', 'Enables or disables the module.'),
+          'title' => language::translate(__CLASS__.':title_status', 'Status'),
+          'description' => language::translate(__CLASS__.':description_status', 'Enables or disables the module.'),
           'function' => 'toggle("e/d")',
         ),
         array(
           'key' => 'icon',
           'default_value' => 'images/payment/paypal.png',
-          'title' => $GLOBALS['system']->language->translate(__CLASS__.':title_icon', 'Icon'),
-          'description' => $GLOBALS['system']->language->translate(__CLASS__.':description_icon', 'Web path of the icon to be displayed.'),
+          'title' => language::translate(__CLASS__.':title_icon', 'Icon'),
+          'description' => language::translate(__CLASS__.':description_icon', 'Web path of the icon to be displayed.'),
           'function' => 'input()',
         ),
         array(
           'key' => 'merchant_email',
           'default_value' => '',
-          'title' => $GLOBALS['system']->language->translate(__CLASS__.':title_merchant_email', 'Merchant E-mail'),
-          'description' => $GLOBALS['system']->language->translate(__CLASS__.':description_merchant_email', 'Your Paypal registered merchant e-mail address.'),
+          'title' => language::translate(__CLASS__.':title_merchant_email', 'Merchant E-mail'),
+          'description' => language::translate(__CLASS__.':description_merchant_email', 'Your Paypal registered merchant e-mail address.'),
           'function' => 'input()',
         ),
         array(
           'key' => 'pdt_auth_token',
           'default_value' => '',
-          'title' => $GLOBALS['system']->language->translate(__CLASS__.':title_pdt_auth_token', 'PDT Auth Token'),
-          'description' => $GLOBALS['system']->language->translate(__CLASS__.':description_pdt_auth_token', 'Your Paypal PDT authorization token (see your Paypal account).'),
+          'title' => language::translate(__CLASS__.':title_pdt_auth_token', 'PDT Auth Token'),
+          'description' => language::translate(__CLASS__.':description_pdt_auth_token', 'Your Paypal PDT authorization token (see your Paypal account).'),
           'function' => 'input()',
         ),
         array(
           'key' => 'gateway',
           'default_value' => 'Sandbox',
-          'title' => $GLOBALS['system']->language->translate(__CLASS__.':title_gateway', 'Gateway'),
-          'description' => $GLOBALS['system']->language->translate(__CLASS__.':description_gateway', 'Select your Paypal payment gateway.'),
+          'title' => language::translate(__CLASS__.':title_gateway', 'Gateway'),
+          'description' => language::translate(__CLASS__.':description_gateway', 'Select your Paypal payment gateway.'),
           'function' => 'radio(\'Production\',\'Sandbox\')',
         ),
         array(
           'key' => 'order_status_id_complete',
           'default_value' => '0',
-          'title' => $GLOBALS['system']->language->translate(__CLASS__.':title_order_status', 'Order Status') .': '. $GLOBALS['system']->language->translate(__CLASS__.':title_complete', 'Complete'),
-          'description' => $GLOBALS['system']->language->translate(__CLASS__.':description_order_status_success', 'Give successful orders made with this payment module the following order status.'),
+          'title' => language::translate(__CLASS__.':title_order_status', 'Order Status') .': '. language::translate(__CLASS__.':title_complete', 'Complete'),
+          'description' => language::translate(__CLASS__.':description_order_status_success', 'Give successful orders made with this payment module the following order status.'),
           'function' => 'order_status()',
         ),
         array(
           'key' => 'order_status_id_error',
           'default_value' => '0',
-          'title' => $GLOBALS['system']->language->translate(__CLASS__.':title_order_status', 'Order Status') .': '. $GLOBALS['system']->language->translate(__CLASS__.':title_error', 'Error'),
-          'description' => $GLOBALS['system']->language->translate(__CLASS__.':description_order_status_error', 'Give failed orders made with this payment module the following order status.'),
+          'title' => language::translate(__CLASS__.':title_order_status', 'Order Status') .': '. language::translate(__CLASS__.':title_error', 'Error'),
+          'description' => language::translate(__CLASS__.':description_order_status_error', 'Give failed orders made with this payment module the following order status.'),
           'function' => 'order_status()',
         ),
         array(
           'key' => 'geo_zone_id',
           'default_value' => '',
-          'title' => $GLOBALS['system']->language->translate(__CLASS__.':title_geo_zone_limitation', 'Geo Zone Limitation'),
-          'description' => $GLOBALS['system']->language->translate(__CLASS__.':description_geo_zone', 'Limit this module to the selected geo zone. Otherwise leave blank.'),
+          'title' => language::translate(__CLASS__.':title_geo_zone_limitation', 'Geo Zone Limitation'),
+          'description' => language::translate(__CLASS__.':description_geo_zone', 'Limit this module to the selected geo zone. Otherwise leave blank.'),
           'function' => 'geo_zones()',
         ),
         array(
           'key' => 'priority',
           'default_value' => '0',
-          'title' => $GLOBALS['system']->language->translate(__CLASS__.':title_priority', 'Priority'),
-          'description' => $GLOBALS['system']->language->translate(__CLASS__.':description_priority', 'Process this module in the given priority order.'),
+          'title' => language::translate(__CLASS__.':title_priority', 'Priority'),
+          'description' => language::translate(__CLASS__.':description_priority', 'Process this module in the given priority order.'),
           'function' => 'int()',
         ),
       );
