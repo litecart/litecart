950bea9644637c1a52a9140a01d97e4a96254792
timint <info@tim-international.net>
2014-12-19 19:21:36 +0100

    * Fix site footer logout link

--- public_html/includes/templates/default.catalog/views/box_site_footer.inc.php
+++ public_html/includes/templates/default.catalog/views/box_site_footer.inc.php
@@ -38,19 +38,7 @@
               <?php } else { ?>
               <li><a href="<?php echo document::href_ilink('order_history'); ?>"><?php echo language::translate('title_order_history', 'Order History'); ?></a></li>
               <li><a href="<?php echo document::href_ilink('edit_account'); ?>"><?php echo language::translate('title_edit_account', 'Edit Account'); ?></a></li>
-              <li><a href="javascript:logout();"><?php echo language::translate('title_logout', 'Logout'); ?></a></li>
-              <script>
-                function logout() {
-                  var form = $('<?php
-                    echo str_replace(array("\r", "\n"), '', functions::form_draw_form_begin('logout_form', 'post')
-                                                          . functions::form_draw_hidden_field('logout', 'true')
-                                                          . functions::form_draw_form_end()
-                    );
-                  ?>');
-                  $(document.body).append(form);
-                  form.submit();
-                }
-              </script>
+              <li><a href="<?php echo document::href_ilink('logout'); ?>"><?php echo language::translate('title_logout', 'Logout'); ?></a></li>
               <?php } ?>
             </ul>
           </nav>
