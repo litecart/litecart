28e8ab0d1854cd6e67522f2bab2b90ac150fb89f
timint <info@tim-international.net>
2015-02-20 21:25:28 +0100

    * Fix problem with sales where one month was not a unique pile

--- public_html/admin/sales.widget/sales.inc.php
+++ public_html/admin/sales.widget/sales.inc.php
@@ -22,21 +22,21 @@
   for ($timestamp = strtotime('-1 years'); date('Y-m', $timestamp) <= date('Y-m'); $timestamp = strtotime('+1 month', $timestamp)) {
     
     $orders_query = database::query(
-      "select sum(payment_due - tax_total) as total_sales, tax_total as total_tax from ". DB_TABLE_ORDERS ."
+      "select sum(payment_due - tax_total) as total_sales, sum(tax_total) as total_tax from ". DB_TABLE_ORDERS ."
       where order_status_id in ('". implode("', '", $order_statuses) ."')
       and date_created >= '". date('Y-m-d H:i:s', mktime(0, 0, 0, date('m', $timestamp), 1, date('Y', $timestamp))) ."'
       and date_created <= '". date('Y-m-d H:i:s', mktime(23, 59, 59, date('m', $timestamp), date('t', $timestamp), date('Y', $timestamp))) ."';"
     );
     $orders = database::fetch($orders_query);
     
-    $monthly_sales[date('Y-m', $timestamp)] = '[\''. strftime('%b', $timestamp) .'\', '. (int)$orders['total_sales'] .']';
-    $monthly_tax[date('Y-m', $timestamp)] = '[\''. strftime('%b', $timestamp) .'\', '. (int)$orders['total_tax'] .']';
+    $monthly_sales[date('Y-m', $timestamp)] = '[\''. strftime('%b %Y', $timestamp) .'\', '. (int)$orders['total_sales'] .']';
+    $monthly_tax[date('Y-m', $timestamp)] = '[\''. strftime('%b %Y', $timestamp) .'\', '. (int)$orders['total_tax'] .']';
   }
+  
 ?>
   <div id="chart-sales-monthly" style="float: left; width: 50%; height: 150px;"></div>
   <script>
     var bar1 = [<?php echo implode(',', $monthly_sales); ?>];
-    var bar2 = [<?php echo implode(',', $monthly_tax); ?>];
     var plot1 = $.jqplot('chart-sales-monthly', [bar1], {
       title: '<?php echo language::translate('title_sales', 'Sales'); ?> (<?php echo sprintf(language::translate('title_s_months', '%s months'), '12'); ?>)',
       grid:{
