cb9cc1b3433e73c6d2a5a9a60dc5d57a09842782
timint <info@tim-international.net>
2015-02-20 21:30:24 +0100

    * Move cookie acceptance to bottom (due to Google indexing)

--- public_html/includes/templates/default.catalog/layouts/default.inc.php
+++ public_html/includes/templates/default.catalog/layouts/default.inc.php
@@ -1,20 +1,20 @@
 <?php
 // Cookie acceptance - By EU law
   if (empty($_COOKIE['cookies_accepted'])) {
-    if (!isset(document::$snippets['top'])) document::$snippets['top'] = '';
-    document::$snippets['top'] =  '<div id="cookies-acceptance-wrapper">' . PHP_EOL
-                                . '  <div id="cookies-acceptance" class="twelve-eighty">' . PHP_EOL
-                                . '    ' . language::translate('terms_cookies_acceptance', 'We rely on cookies to provide our services. By using our services, you agree to our use of cookies.') .' '. functions::form_draw_button('accept_cookies', language::translate('title_ok', 'OK'), 'button') . PHP_EOL
-                                . '  </div>' . PHP_EOL
-                                . '</div>' . PHP_EOL
-                                . '<script src="'. WS_DIR_EXT .'jquery/jquery.cookie.min.js"></script>' . PHP_EOL
-                                . '<script>' . PHP_EOL
-                                . '  $("button[name=\'accept_cookies\']").click(function(){' . PHP_EOL
-                                . '    $("#cookies-acceptance-wrapper").fadeOut();' . PHP_EOL
-                                . '    $.cookie("cookies_accepted", "1", {path: "'. WS_DIR_HTTP_HOME .'", expires: 365});' . PHP_EOL
-                                . '  });' . PHP_EOL
-                                . '</script>'
-                                . document::$snippets['top'];
+    if (!isset(document::$snippets['bottom'])) document::$snippets['bottom'] = '';
+    document::$snippets['bottom'] =  '<div id="cookies-acceptance-wrapper">' . PHP_EOL
+                                   . '  <div id="cookies-acceptance" class="twelve-eighty">' . PHP_EOL
+                                   . '    ' . language::translate('terms_cookies_acceptance', 'We rely on cookies to provide our services. By using our services, you agree to our use of cookies.') .' '. functions::form_draw_button('accept_cookies', language::translate('title_ok', 'OK'), 'button') . PHP_EOL
+                                   . '  </div>' . PHP_EOL
+                                   . '</div>' . PHP_EOL
+                                   . '<script src="'. WS_DIR_EXT .'jquery/jquery.cookie.min.js"></script>' . PHP_EOL
+                                   . '<script>' . PHP_EOL
+                                   . '  $("button[name=\'accept_cookies\']").click(function(){' . PHP_EOL
+                                   . '    $("#cookies-acceptance-wrapper").fadeOut();' . PHP_EOL
+                                   . '    $.cookie("cookies_accepted", "1", {path: "'. WS_DIR_HTTP_HOME .'", expires: 365});' . PHP_EOL
+                                   . '  });' . PHP_EOL
+                                   . '</script>'
+                                   . document::$snippets['bottom'];
   }
 ?>
 <!DOCTYPE html>
@@ -100,6 +100,10 @@
           <div class="content">
             <!--snippet:content-->
           </div>
+        </div>
+        
+        <div class="bottom">
+          <!--snippet:bottom-->
         </div>
       </div>
     </div>
