0749c1aca0f3d3371653fda22a7c2cebe778997c
Anton Troedson <troedsonanton@gmail.com>
2015-02-21 12:26:38 +0100

    Making changes to category structure. Creating a meany-to-meany table and adding category_id to product(used as defualt category)

--- public_html/install/structure.sql
+++ public_html/install/structure.sql
@@ -392,7 +392,7 @@ CREATE TABLE `lc_products` (
   `supplier_id` int(11) NOT NULL,
   `delivery_status_id` int(11) NOT NULL,
   `sold_out_status_id` int(11) NOT NULL,
-  `categories` varchar(64) NOT NULL,
+  `category_id` int(11) NOT NULL,
   `product_groups` varchar(128) NOT NULL,
   `keywords` varchar(256) NOT NULL,
   `code` varchar(64) NOT NULL,
@@ -426,6 +426,12 @@ CREATE TABLE `lc_products` (
   KEY `purchases` (`purchases`),
   KEY `views` (`views`),
   KEY `product_groups` (`product_groups`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE {DATABASE_COLLATION};
+-- --------------------------------------------------------
+CREATE TABLE `lc_products_categories` (
+   `product_id` int(11) NOT NULL,
+   `category_id` int(11) NOT NULL,
+   PRIMARY KEY(`product_id`, `category_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE {DATABASE_COLLATION};
 -- --------------------------------------------------------
 CREATE TABLE `lc_products_campaigns` (
