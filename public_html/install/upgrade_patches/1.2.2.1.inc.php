8517ea2c612bd37c9c0495674ce7646f2004a72d
timint <info@tim-international.net>
2015-03-05 17:42:09 +0100

    * Fix 1.2.2.1 upgrade patch

--- public_html/install/upgrade_patches/1.2.2.1.inc.php
+++ public_html/install/upgrade_patches/1.2.2.1.inc.php
@@ -12,4 +12,7 @@
       die('<span class="error">[Error]</span></p>');
     }
   }
+  
+  if (!file_exists(FS_DIR_HTTP_ROOT . WS_DIR_DATA . 'blacklist.txt')) file_xcopy(FS_DIR_HTTP_ROOT . WS_DIR_HTTP_HOME . 'install/data/default/public_html/data/blacklist.txt', FS_DIR_HTTP_ROOT . WS_DIR_DATA . 'blacklist.txt');
+  if (!file_exists(FS_DIR_HTTP_ROOT . WS_DIR_DATA . 'whitelist.txt')) file_xcopy(FS_DIR_HTTP_ROOT . WS_DIR_HTTP_HOME . 'install/data/default/public_html/data/whitelist.txt', FS_DIR_HTTP_ROOT . WS_DIR_DATA . 'whitelist.txt');
 ?>
