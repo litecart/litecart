7479f7d27bedc80a8d3b0d385c91addf0b5b47aa
timint <info@tim-international.net>
2015-04-17 22:04:01 +0200

    * vQmod 2.4.1 * vQmod customizations

--- public_html/includes/classes/vmod.inc.php
+++ public_html/includes/classes/vmod.inc.php
@@ -7,6 +7,7 @@
         if (is_file(FS_DIR_HTTP_ROOT . WS_DIR_HTTP_HOME . 'vqmod/vqmod.php')) {
           require_once FS_DIR_HTTP_ROOT . WS_DIR_HTTP_HOME . 'vqmod/vqmod.php';
           vqmod::bootup(FS_DIR_HTTP_ROOT . WS_DIR_HTTP_HOME, true);
+          vqmod::$replaces[] = array('#^admin/\b#', substr(WS_DIR_ADMIN, strlen(WS_DIR_HTTP_HOME)));
         }
       }
       
