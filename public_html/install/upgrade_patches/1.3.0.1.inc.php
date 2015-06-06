1f385da6cff43ff137936ea1212373a774b31b19
timint <info@tim-international.net>
2015-06-06 16:31:09 +0200

    * Upgrade jQuery to 1.11.3

--- public_html/install/upgrade_patches/1.3.0.1.inc.php
+++ public_html/install/upgrade_patches/1.3.0.1.inc.php
@@ -3,6 +3,8 @@
   $deleted_files = array(
     FS_DIR_HTTP_ROOT . WS_DIR_HTTP_HOME .'vqmods/logs/*',
     FS_DIR_HTTP_ROOT . WS_DIR_ADMIN .'vqmods.app/log.inc.php',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT .'jquery/jquery-1.11.2.min.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT .'jquery/jquery-1.11.2.min.map',
   );
   
   foreach ($deleted_files as $pattern) {
