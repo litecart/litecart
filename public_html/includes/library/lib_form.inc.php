908d81a8d6e36739e19326fd4eb169447856a808
timint <info@tim-international.net>
2015-07-27 10:34:48 +0200

    * Form post token

--- public_html/includes/library/lib_form.inc.php
+++ public_html/includes/library/lib_form.inc.php
@@ -52,7 +52,7 @@
     ######################################################################
     
     public static function session_post_token() {
-      return sha1(SESSION_UNIQUE_ID .':'. session_id());
+      return sha1(PLATFORM_NAME . PLATFORM_VERSION . session_id());
     }
   }
   
