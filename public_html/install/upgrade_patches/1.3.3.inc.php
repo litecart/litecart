51333f0249e298a68651764d66be055f09ad5944
timint <info@tim-international.net>
2015-09-13 23:37:54 +0200

    * 1.3.3 version change and patches

---
+++ public_html/install/upgrade_patches/1.3.3.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+// Convert module settings to JSON
+  $query = $database->query(
+    "select * from ". DB_TABLE_SETTINGS ."
+    where (
+      `key` = 'store_template_catalog_settings'
+      or `key` like 'customer_module_%'
+      or `key` like 'job_module_%'
+      or `key` like 'shipping_module_%'
+      or `key` like 'payment_module_%'
+      or `key` like 'order_action_module_%'
+      or `key` like 'order_success_module_%'
+      or `key` like 'order_total_module_%'
+    );"
+  );
+  while($row = $database->fetch($query)) {
+    $new_key = preg_replace('#^((customer|job|shipping|payment|order_action|order_success|order_total)_module_)#', '', $row['key']);
+    $new_value = json_encode(unserialize($row['value']));
+    $database->query(
+      "update  ". DB_TABLE_SETTINGS ."
+      set `key` = '". $database->input($new_key) ."', `value` = '". $database->input($new_value) ."'
+      where `key` = '". $database->input($row['key']) ."'
+      limit 1;"
+    );
+  }
+
+?>
---
+++ public_html/install/upgrade_patches/1.3.3.sql
@@ -0,0 +1,11 @@
+ALTER TABLE `lc_order_statuses` CHANGE `color` `color` VARCHAR(7) NOT NULL;
+-- --------------------------------------------------------
+UPDATE `lc_order_statuses` SET color = '#c0c0c0' WHERE icon = 'fa-chain-broken';
+-- --------------------------------------------------------
+UPDATE `lc_order_statuses` SET color = '#d7d96f' WHERE icon = 'fa-clock-o';
+-- --------------------------------------------------------
+UPDATE `lc_order_statuses` SET color = '#ffa851' WHERE icon = 'fa-cog';
+-- --------------------------------------------------------
+UPDATE `lc_order_statuses` SET color = '#99cc66' WHERE icon = 'fa-truck';
+-- --------------------------------------------------------
+UPDATE `lc_order_statuses` SET color = '#ff6666' WHERE icon = 'fa-times';
