85a4a87f27d602db757ff1e02be8aa791f9468ce
timint <info@tim-international.net>
2015-09-25 04:34:09 +0200

    * Compatibility changes

--- public_html/includes/compatibility.inc.php
+++ public_html/includes/compatibility.inc.php
@@ -1,27 +1,18 @@
 <?php
 
 // Check Version
-  if (version_compare(phpversion(), '5.1.0', '<') == true) {
-    exit('PHP5.1+ Required');
+  if (version_compare(phpversion(), '5.3.0', '<') == true) {
+    exit('PHP5.3+ Required');
   }
 
-// Register Globals
+// (Un)register Globals (PHP <5.4)
   if (ini_get('register_globals')) {
-    ini_set('session.use_cookies', 'On');
-    ini_set('session.use_trans_sid', 'Off');
-    
-    session_set_cookie_params(0, '/');
-    session_start();
-    
-    foreach (array($_REQUEST, $_SESSION, $_SERVER, $_FILES) as $global) {
-      foreach(array_keys($global) as $key) {
-        global $$key;
-        unset($$key);
-      }
+    foreach (array_keys(array_merge($_SERVER, $_ENV, !empty($_SESSION) ? $_SESSION : array(), $_COOKIE, $_REQUEST, $_FILES)) as $key) {
+      if (isset($GLOBALS[$key])) unset($GLOBALS[$key]);
     }
   }
   
-// Magic Quotes Fix
+// Fix Magic Quotes
   if (ini_get('magic_quotes_gpc')) {
     function clean($data) {
       if (is_array($data)) {
@@ -33,7 +24,7 @@
       }
     
       return $data;
-    }			
+    }
     
     $_GET = clean($_GET);
     $_POST = clean($_POST);
@@ -41,31 +32,10 @@
     $_COOKIE = clean($_COOKIE);
   }
   
-// Windows Paths
+// Fix Windows Paths
   $_SERVER['SCRIPT_FILENAME'] = str_replace('\\', '/', $_SERVER['SCRIPT_FILENAME']);
   
- // array_column() as of PHP 5.5
-  if (!function_exists('array_column')) {
-    function array_column(array $array, $column_key, $index_key=null) {
-      $result = array();
-      foreach($array as $arr){
-        if(!is_array($arr)) continue;
-        if (is_null($column_key)) {
-          $value = $arr;
-        } else {
-          $value = $arr[$column_key];
-        }
-        if (!is_null($index_key)) {
-          $key = $arr[$index_key];
-          $result[$key] = $value;
-        } else{
-          $result[] = $value;
-        }
-      }
-      return $result;
-    }
-  }
-  
+// Emulate http_response_code() as of PHP 5.4
   if (!function_exists('http_response_code')) {
     function http_response_code($code = null) {
       if ($code !== NULL) {
@@ -119,6 +89,28 @@
       
       return $code;
       
+    }
+  }
+  
+// Emulate array_column() as of PHP 5.5
+  if (!function_exists('array_column')) {
+    function array_column(array $array, $column_key, $index_key=null) {
+      $result = array();
+      foreach($array as $arr){
+        if(!is_array($arr)) continue;
+        if (is_null($column_key)) {
+          $value = $arr;
+        } else {
+          $value = $arr[$column_key];
+        }
+        if (!is_null($index_key)) {
+          $key = $arr[$index_key];
+          $result[$key] = $value;
+        } else{
+          $result[] = $value;
+        }
+      }
+      return $result;
     }
   }
   
