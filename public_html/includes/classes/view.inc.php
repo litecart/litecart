e334eada01e68c8efbbc9764a252aaecb0f8f812
timint <info@tim-international.net>
2016-02-18 10:18:04 +0100

    + Cleanup snippets in view

--- public_html/includes/classes/view.inc.php
+++ public_html/includes/classes/view.inc.php
@@ -3,15 +3,15 @@
     public $snippets = array();
     public $html = '';
     
-    public function stitch($view=null) {
+    public function stitch($view=null, $cleanup=false) {
       
-      if ($view !== null) {
+      if (!empty($view)) {
         $file = FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES . document::$template .'/'. $view .'.inc.php';
         if (!is_file($file)) $file = FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES . 'default.catalog/'. $view .'.inc.php';
         $this->html = $this->_process_view($file, $this->snippets);
       }
       
-      if (empty($this->html)) return;
+      if (empty($this->html)) return null;
       
       if (!empty($this->snippets)) {
         
@@ -33,6 +33,16 @@
         }
       }
       
+    // Clean orphan snippets
+      if ($cleanup) {
+        $search = array(
+          '/\{snippet:[^\}]+\}/',
+          '/<!--snippet:[^-->]+-->/',
+        );
+
+        $this->html = preg_replace($search, '', $this->html);
+      }
+
       return $this->html;
     }
     
