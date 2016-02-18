12846ca9d2452fca79c28b3b3b882f1e0c0ca9ca
timint <info@tim-international.net>
2016-02-18 10:20:59 +0100

    + Upgrade patch

---
+++ public_html/install/upgrade_patches/1.3.4.1.sql
@@ -0,0 +1,2 @@
+UPDATE `lc_translattions` SET `text_en` = 'Order %order_id has updated to %order_status' where `key` = 'email_subject_order_updated';
+-- --------------------------------------------------------
