6cc925047fa310d69aeda56b7108c501e02f6131
timint <info@tim-international.net>
2016-02-22 10:20:24 +0100

    * Fix product groups error box_similar_products

--- public_html/includes/boxes/box_similar_products.inc.php
+++ public_html/includes/boxes/box_similar_products.inc.php
@@ -8,29 +8,13 @@
   $box_similar_products_cache_id = cache::cache_id('box_similar_products', array('get', 'language', 'currency', 'prices'));
   if (cache::capture($box_similar_products_cache_id, 'file')) {
     
-    $product_groups = array();
-    if ($product->product_group_ids) {
-      foreach ($product->product_group_ids as $product_group) {
-        $product_groups[] = "find_in_set('". database::input($product_group) ."', p.product_groups)";
-      }
-    }
-    
-    $keywords = array();
-    if ($product->keywords != '') {
-      foreach (explode(',', $product->keywords) as $keyword) {
-        $keyword = trim($keyword);
-        if (empty($keyword)) continue;
-        $keywords[] = $keyword;
-      }
-    }
-    
     $products_query = functions::catalog_products_query(array(
       'product_name' => $product->name[language::$selected['code']],
       'categories' => isset($_GET['category_id']) ? array($_GET['category_id']) : array_keys($product->categories),
       'manufacturers' => array($product->manufacturer_id),
-      'product_groups' => $product_groups,
+      'product_groups' => $product->product_group_ids,
       'exclude_products' => $product->id,
-      'keywords' => $keywords,
+      'keywords' => explode(',', $product->keywords),
       'sort' => 'occurrences',
       'limit' => settings::get('box_similar_products_num_items'),
     ));
