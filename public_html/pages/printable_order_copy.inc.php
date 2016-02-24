801751304555e708a6b48d93bdef000b927ceb9e
timint <info@tim-international.net>
2016-02-24 14:52:03 +0100

    * Fix vulnerability in printable_order_copy

--- public_html/pages/printable_order_copy.inc.php
+++ public_html/pages/printable_order_copy.inc.php
@@ -10,10 +10,10 @@
     exit;
   }
   
-  document::$snippets['title'][] = language::translate('title_order', 'Order') .' #'. $_GET['order_id'];
-  
   $order = new ctrl_order('load', $_GET['order_id']);
   
+  document::$snippets['title'][] = language::translate('title_order', 'Order') .' #'. (int)$order->data['id'];
+
   if (empty($order->data['id']) || $_GET['checksum'] != functions::general_order_public_checksum($order->data['id'])) {
     header('HTTP/1.1 400 Bad Request');
     exit;
