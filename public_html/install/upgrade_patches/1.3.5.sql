15eef9b3b7f23b9233e6e8a10fc8cd5810c4abea
timint <info@tim-international.net>
2016-03-10 00:48:20 +0100

    * Minor adjustments

--- public_html/install/upgrade_patches/1.3.5.sql
+++ public_html/install/upgrade_patches/1.3.5.sql
@@ -1,2 +1,6 @@
 INSERT INTO `lc_settings` (`setting_group_key`, `type`, `title`, `description`, `key`, `value`, `function`, `priority`, `date_updated`, `date_created`)
 VALUES ('listings', 'local', 'Auto Decimals', 'Show only decimals if there are any to display.', 'auto_decimals', '1', 'toggle("e/d")', 20, NOW(), NOW());
+-- --------------------------------------------------------
+ALTER TABLE `lc_pages_info` CHANGE COLUMN `content` `content` MEDIUMTEXT NOT NULL AFTER `title`;
+-- --------------------------------------------------------
+ALTER TABLE `lc_zones` ADD INDEX `code` (`code`);
