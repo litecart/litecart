8f72e980cf581c0d1824abca8b2089a1ed1d9d3f
timint <info@tim-international.net>
2016-12-12 01:31:44 +0100

    * Upgrade patch

--- public_html/install/upgrade_patches/1.4.inc.php
+++ public_html/install/upgrade_patches/1.4.inc.php
@@ -1,5 +1,25 @@
 <?php
 
+  $deleted_files = array(
+    FS_DIR_HTTP_ROOT . WS_DIR_ADMIN .'sales.widget/',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT .'fancybox/',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT .'jqplot/',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT .'jquery/jquery-1.12.4.min.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT .'jquery/jquery-migrate-1.4.1.min.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT .'responsiveslider/',
+    FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES .'default.admin/images/fancybox/',
+    FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES .'default.catalog/images/fancybox/',
+    FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES .'default.catalog/styles/ie.css',
+    FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES .'default.catalog/styles/ie8.css',
+    FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES .'default.catalog/styles/ie9.css',
+  );
+
+  foreach ($deleted_files as $pattern) {
+    if (!file_delete($pattern)) {
+      die('<span class="error">[Error]</span></p>');
+    }
+  }
+
 // Patch templates
   foreach (glob(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES . '*/layouts/*.inc.php') as $file) {
     if (preg_match('#default\.(admin|catalog)#', $file)) continue;
@@ -15,6 +35,13 @@
     $find = '<?php list($account, $domain) = explode(\'@\', settings::get(\'store_email\')); echo "<script>document.write(\'<a href=\\"mailto:". $account ."\' + \'@\' + \'". $domain ."\\">". $account ."\' + \'@\' + \'". $domain ."</a>\');</script>"; ?>';
     $replace = '<a href="mailto:<?php echo settings::get(\'store_email\'); ?>"><?php echo settings::get(\'store_email\'); ?></a>';
     $contents = preg_replace('#'. preg_quote($search, '#') .'#', $replace, $contents);
+    file_put_contents($file, $contents);
+  }
+
+  foreach (glob(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES . '*.catalog/*.inc.php') as $file) {
+    if (preg_match('#default\.catalog#', $file)) continue;
+    $contents = file_get_contents($file);
+    $contents = preg_replace('#'. preg_quote('fancybox', '#') .'#', 'lightbox', $contents);
     file_put_contents($file, $contents);
   }
 
