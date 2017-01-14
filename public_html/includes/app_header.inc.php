430c7cc8a59ec64117fffad04734983e372df7d1
timint <info@tim-international.net>
2017-01-15 00:50:27 +0100

    * Error handler to separate file

--- public_html/includes/app_header.inc.php
+++ public_html/includes/app_header.inc.php
@@ -23,60 +23,7 @@
   require_once vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_INCLUDES . 'autoloader.inc.php');
 
 // Set error handler
-  function error_handler($errno, $errstr, $errfile, $errline, $errcontext) {
-    if (!(error_reporting() & $errno)) return;
-    $errfile = preg_replace('#^'. FS_DIR_HTTP_ROOT . WS_DIR_HTTP_HOME .'#', '~/', str_replace('\\', '/', $errfile));
-
-    switch($errno) {
-      case E_WARNING:
-      case E_USER_WARNING:
-        $output = "<b>Warning:</b> $errstr in <b>$errfile</b> on line <b>$errline</b>";
-        break;
-      case E_STRICT:
-      case E_NOTICE:
-      case E_USER_NOTICE:
-        $output = "<b>Notice:</b> $errstr in <b>$errfile</b> on line <b>$errline</b>";
-        break;
-      case E_DEPRECATED:
-      case E_USER_DEPRECATED:
-        $output = "<b>Deprecated:</b> $errstr in <b>$errfile</b> on line <b>$errline</b>";
-        break;
-      default:
-        $output = "<b>Fatal error:</b> $errstr in <b>$errfile</b> on line <b>$errline</b>";
-        break;
-    }
-
-    if (isset($_GET['debug'])) {
-      $backtraces = debug_backtrace();
-      $backtraces = array_slice($backtraces, 2);
-
-      if (!empty($backtraces)) {
-        foreach ($backtraces as $backtrace) {
-          if (empty($backtrace['file'])) continue;
-          $backtrace['file'] = preg_replace('#^'. FS_DIR_HTTP_ROOT . WS_DIR_HTTP_HOME .'#', '~/', str_replace('\\', '/', $backtrace['file']));
-          $output .= "<br />" . PHP_EOL . "  <- <b>{$backtrace['file']}</b> on line <b>{$backtrace['line']}</b> in <b>{$backtrace['function']}()</b>";
-        }
-      }
-    }
-
-    if (in_array(strtolower(ini_get('display_errors')), array('1', 'on', 'true'))) {
-      if (in_array(strtolower(ini_get('html_errors')), array(0, 'off', 'false')) || PHP_SAPI == 'cli') {
-        echo strip_tags($output) . PHP_EOL;
-      } else {
-        echo $output . '<br />' . PHP_EOL;
-      }
-    } else {
-      if (!empty($_SERVER['REQUEST_URI'])) $output .= " {$_SERVER['REQUEST_URI']}";
-    }
-
-    if (ini_get('log_errors')) {
-      error_log(strip_tags($output));
-    }
-
-    if (in_array($errno, array(E_ERROR, E_USER_ERROR))) exit;
-  }
-
-  set_error_handler('error_handler');
+  require_once vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_INCLUDES . 'error_handler.inc.php');
 
 // Set up the system object
   system::init();
---
+++ public_html/includes/error_handler.inc.php
@@ -0,0 +1,52 @@
+<?php
+  function error_handler($errno, $errstr, $errfile, $errline, $errcontext) {
+    if (!(error_reporting() & $errno)) return;
+    $errfile = preg_replace('#^'. FS_DIR_HTTP_ROOT . WS_DIR_HTTP_HOME .'#', '~/', str_replace('\\', '/', $errfile));
+
+    switch($errno) {
+      case E_WARNING:
+      case E_USER_WARNING:
+        $output = "<strong>Warning:</strong> $errstr in <strong>$errfile</strong> on line <strong>$errline</strong><br />" . PHP_EOL;
+        break;
+      case E_STRICT:
+      case E_NOTICE:
+      case E_USER_NOTICE:
+        $output = "<strong>Notice:</strong> $errstr in <strong>$errfile</strong> on line <strong>$errline</strong><br />" . PHP_EOL;
+        break;
+      case E_DEPRECATED:
+      case E_USER_DEPRECATED:
+        $output = "<strong>Deprecated:</strong> $errstr in <strong>$errfile</strong> on line <strong>$errline</strong><br />" . PHP_EOL;
+        break;
+      default:
+        $output = "<strong>Fatal error:</strong> $errstr in <strong>$errfile</strong> on line <strong>$errline</strong><br />" . PHP_EOL;
+        break;
+    }
+
+    $backtrace_output = '';
+    $backtraces = array_slice(debug_backtrace(), 2);
+
+    if (!empty($backtraces)) {
+      foreach ($backtraces as $backtrace) {
+        if (empty($backtrace['file'])) continue;
+        $backtrace['file'] = preg_replace('#^'. FS_DIR_HTTP_ROOT . WS_DIR_HTTP_HOME .'#', '~/', str_replace('\\', '/', $backtrace['file']));
+        $backtrace_output .= "  <- <strong>{$backtrace['file']}</strong> on line <strong>{$backtrace['line']}</strong> in <strong>{$backtrace['function']}()</strong><br />" . PHP_EOL;
+      }
+    }
+
+    if (in_array(strtolower(ini_get('display_errors')), array('1', 'on', 'true'))) {
+      if (in_array(strtolower(ini_get('html_errors')), array(0, 'off', 'false')) || PHP_SAPI == 'cli') {
+        echo strip_tags($output . (isset($_GET['debug']) ? $backtrace_output : ''));
+      } else {
+        echo $output . (isset($_GET['debug']) ? $backtrace_output : '');
+      }
+    }
+
+    if (ini_get('log_errors')) {
+      error_log('['. gethostbyaddr($_SERVER['REMOTE_ADDR']) .'] '. strip_tags($output . $backtrace_output) . (!empty($_SERVER['REQUEST_URI']) ? ' ' . $_SERVER['REQUEST_URI'] : ''));
+    }
+
+    if (in_array($errno, array(E_ERROR, E_USER_ERROR))) exit;
+  }
+
+  set_error_handler('error_handler');
+?>
