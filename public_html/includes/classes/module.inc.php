827fed3fcdb357522652226280e443c1c5c40aba
timint <info@tim-international.net>
2017-01-16 00:10:23 +0100

    * New module system

--- public_html/includes/classes/module.inc.php
+++ public_html/includes/classes/module.inc.php
@@ -1,83 +1,77 @@
 <?php
 
   class module {
-    public $type;
     public $modules;
 
-    public function set_type($type) {
-      $this->type = $type;
+    public function reset() {
+      $this->modules = array();
     }
 
-    public function load($module_id='') {
+    public function load($type, $filter=null) {
 
-      if (empty($module_id)) {
-        $load_modules = explode(';', settings::get($this->type.'_modules'));
-        if (empty($load_modules)) return;
-      } else {
-        $load_modules = array($module_id);
+      $this->reset();
+
+      if (!empty($filter) && !is_array($filter)) $filter = array($filter);
+
+      switch($type) {
+        case 'job':
+          $directory = FS_DIR_HTTP_ROOT . WS_DIR_MODULES . 'jobs/';
+          break;
+        default:
+          $directory = FS_DIR_HTTP_ROOT . WS_DIR_MODULES . $type . '/';
+          break;
       }
 
-      foreach ($load_modules as $module_id) {
+      $modules_query = database::query(
+        "select * from ". DB_TABLE_MODULES ."
+        where type = '". database::input($type) ."'
+        ". (!empty($module_id) ? "and module_id in ('". implode("', '", database::input($filter)) ."')" : "") .";"
+      );
+
+      while($module = database::fetch($modules_query)){
 
       // Uninstall orphan modules
-        if (!is_file(FS_DIR_HTTP_ROOT . WS_DIR_MODULES . $this->type . '/' . $module_id .'.inc.php')) {
-
-          $installed_modules = explode(';', settings::get($this->type.'_modules'));
-
-          $key = array_search($module_id, $installed_modules);
-          if ($key !== false) unset($installed_modules[$key]);
-
+        if (!is_file($directory . $module['module_id'] .'.inc.php')) {
+          /*
           database::query(
-            "update ". DB_TABLE_SETTINGS ."
-            set value = '". database::input(implode(';', $installed_modules)) ."'
-            where `key` = '". $this->type ."_modules'
+            "delete from ". DB_TABLE_MODULES ."
+            where module_id = '". database::input($module['id']) ."'
             limit 1;"
           );
-
-          database::query(
-            "delete from ". DB_TABLE_SETTINGS ."
-            where `key` = '". database::input($this->type.'_module_'. $module_id) ."';"
-          );
-
+          */
           continue;
         }
 
-        $module = new $module_id;
+      // Create object
+        $object = new $module['module_id'];
 
-      // Get settings from database
-        $settings = array();
-        if (settings::get($module_id)) {
-          $settings = unserialize(settings::get($module_id));
+      // Decode settings
+        $settings = json_decode($module['settings'], true);
+
+      // Set settings to object
+        $object->settings = array();
+        foreach ($object->settings() as $setting) {
+          $object->settings[$setting['key']] = isset($settings[$setting['key']]) ? $settings[$setting['key']] : $setting['default_value'];
         }
+        $object->status = (isset($object->settings['status']) && in_array(strtolower($object->settings['status']), array('1', 'active', 'enabled', 'on', 'true', 'yes'))) ? 1 : 0;
+        $object->priority = isset($object->settings[$setting['key']]) ? (int)$object->settings[$setting['key']] : 0;
 
-      // Set settings to module
-        $module->settings = array();
-        foreach ($module->settings() as $setting) {
-          $module->settings[$setting['key']] = isset($settings[$setting['key']]) ? $settings[$setting['key']] : $setting['default_value'];
-        }
-
-        $module->status = isset($module->settings['status']) && in_array(strtolower($module->settings['status']), array('1', 'active', 'enabled', 'on', 'true', 'yes')) ? 1 : 0;
-        $module->priority = isset($module->settings[$setting['key']]) ? (int)$module->settings[$setting['key']] : 0;
-
-        $this->modules[$module->id] = $module;
+      // Add module to list
+        $this->modules[$object->id] = $object;
       }
 
-      if (!empty($this->modules)) $this->sort();
-    }
-
-    private function sort() {
-      if (!function_exists('custom_sort_modules')) {
-        function custom_sort_modules($a, $b) {
+    // Sort modules
+      if (!empty($this->modules)) {
+        uasort($this->modules, function($a, $b) {
           if ((int)$a->priority == (int)$b->priority) {
-            return ($a->name < $b->name) ? 1 : -1;
+            return ($a->name < $b->name) ? -1 : 1;
           } else if ((int)$a->priority > (int)$b->priority) {
             return 1;
           } else {
             return -1;
           }
-        }
+        });
       }
-      uasort($this->modules, 'custom_sort_modules');
     }
   }
 
