14fb3f0b885163f25243cdc4f6693f724bc22f56
timint <info@tim-international.net>
2017-02-10 16:01:27 +0100

    + Login AJAX/lightbox

--- public_html/pages/login.inc.php
+++ public_html/pages/login.inc.php
@@ -5,12 +5,12 @@
 
   header('X-Robots-Tag: noindex');
   document::$snippets['head_tags']['noindex'] = '<meta name="robots" content="noindex" />';
-  document::$snippets['title'][] = language::translate('login:head_title', 'Login');
+  document::$snippets['title'][] = language::translate('title_sign_in', 'Sign In');
+
+  breadcrumbs::add(language::translate('title_sign_in', 'Sign In'));
 
   if (empty($_POST['remember_me'])) $_POST['remember_me'] = false;
   if (empty($_POST['redirect_url'])) $_POST['redirect_url'] = '';
-
-  breadcrumbs::add(language::translate('title_login', 'Log In'));
 
   if (!empty(customer::$data['id'])) notices::add('notice', language::translate('text_already_logged_in', 'You are already logged in'));
 
@@ -19,5 +19,10 @@
   }
 
   $_page = new view();
+
+  if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    echo $_page->stitch('pages/login.ajax');
+  } else {
   echo $_page->stitch('pages/login');
+  }
 ?>
