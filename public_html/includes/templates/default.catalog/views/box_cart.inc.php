d25014f3c7f5d378766b6efc81abfd12f69455bf
timint <info@tim-international.net>
2017-02-13 14:42:55 +0100

    * Fix issue #15

--- public_html/includes/templates/default.catalog/views/box_cart.inc.php
+++ public_html/includes/templates/default.catalog/views/box_cart.inc.php
@@ -7,34 +7,3 @@
     </div>
   </a>
 </div>
-
-<script>
-  function updateCart() {
-    $.ajax({
-      url: '<?php echo document::ilink('ajax/cart.json'); ?>',
-      type: 'get',
-      cache: false,
-      async: true,
-      dataType: 'json',
-      beforeSend: function(jqXHR) {
-        jqXHR.overrideMimeType("text/html;charset=<?php echo language::$selected['charset']; ?>");
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        //alert('Error');
-      },
-      success: function(data) {
-        $('#cart .quantity').html(data['quantity']);
-        $('#cart .formatted_value').html(data['formatted_value']);
-        if (data['quantity'] > 0) {
-          $('#cart img').attr('src', '{snippet:template_path}images/cart_filled.svg');
-        } else {
-          $('#cart img').attr('src', '{snippet:template_path}images/cart.svg');
-        }
-      },
-      complete: function() {
-        $('*').css('cursor', '');
-      }
-    });
-  }
-  var timerCart = setInterval("updateCart()", 60000); // Keeps session alive
-</script>
