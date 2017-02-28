c296d9ac86ba6b85c9db985cd5004424f5943657
timint <info@tim-international.net>
2017-02-28 17:05:36 +0100

    * Fix issue #26

--- public_html/includes/templates/default.catalog/config.inc.php
+++ public_html/includes/templates/default.catalog/config.inc.php
@@ -3,22 +3,22 @@
     array(
       'key' => 'product_modal_window',
       'default_value' => '1',
-      'title' => language::translate(__CLASS__.':title_product_modal_window', 'Product Modal Window'),
-      'description' => language::translate(__CLASS__.':description_product_modal_window', 'Enables or disables the product modal window.'),
+      'title' => language::translate('template:title_product_modal_window', 'Product Modal Window'),
+      'description' => language::translate('template:description_product_modal_window', 'Enables or disables the product modal window.'),
       'function' => 'toggle("e/d")',
     ),
     array(
       'key' => 'sidebar_parallax_effect',
       'default_value' => '1',
-      'title' => language::translate(__CLASS__.':title_sidebar_parallax_effect', 'Sidebar Parallax Effect'),
-      'description' => language::translate(__CLASS__.':description_sidebar_parallax_effect', 'Enables or disables the sidebar parallax effect.'),
+      'title' => language::translate('template:title_sidebar_parallax_effect', 'Sidebar Parallax Effect'),
+      'description' => language::translate('template:description_sidebar_parallax_effect', 'Enables or disables the sidebar parallax effect.'),
       'function' => 'toggle("e/d")',
     ),
     array(
       'key' => 'cookie_acceptance',
       'default_value' => '1',
-      'title' => language::translate(__CLASS__.':title_cookie_acceptance', 'Cookie Acceptance'),
-      'description' => language::translate(__CLASS__.':description_cookie_acceptance', 'Enables or disables the cookie acceptance notice.'),
+      'title' => language::translate('template:title_cookie_acceptance', 'Cookie Acceptance'),
+      'description' => language::translate('template:description_cookie_acceptance', 'Enables or disables the cookie acceptance notice.'),
       'function' => 'toggle("e/d")',
     ),
   );
