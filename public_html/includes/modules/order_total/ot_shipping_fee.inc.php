455bed906cc0dee092a657f92078a5f4cdb506c2
timint <info@tim-international.net>
2017-03-01 19:10:12 +0100

    * Free shipping zone to free shipping by country map

--- public_html/includes/modules/order_total/ot_shipping_fee.inc.php
+++ public_html/includes/modules/order_total/ot_shipping_fee.inc.php
@@ -28,8 +28,7 @@
         'calculate' => true,
       );
 
-      if (!empty($this->settings['free_shipping_amount']) && $this->settings['free_shipping_amount'] > 0) {
-        if (empty($this->settings['free_shipping_geo_zone_id']) || functions::reference_in_geo_zone($this->settings['free_shipping_geo_zone_id'], $order->data['customer']['shipping_address']['country_code'], $order->data['customer']['shipping_address']['zone_code'])) {
+      if (!empty($this->settings['free_shipping_table'])) {
 
         // Calculate cart total
           $subtotal = 0;
@@ -37,8 +36,14 @@
             $subtotal += $item['quantity'] * $item['price'];
           }
 
-        // If below minimum amount
-          if ($subtotal >= $this->settings['free_shipping_amount']) {
+        $free_shipping_table = functions::csv_decode($this->settings['free_shipping_table'], ',');
+
+        if (!empty($free_shipping_table)) {
+          foreach ($free_shipping_table as $row) {
+            if (empty($row['country_code']) || $row['country_code'] != $order->data['customer']['shipping_address']['country_code']) continue;
+            if (!isset($row['min_subtotal']) || $row['min_subtotal'] < 0) continue;
+            if ($subtotal < $row['min_subtotal']) continue;
+
             $output[] = array(
               'title' => language::translate('title_free_shipping', 'Free Shipping'),
               'value' => -$GLOBALS['shipping']->data['selected']['cost'],
@@ -63,18 +68,11 @@
           'function' => 'toggle("e/d")',
         ),
         array(
-          'key' => 'free_shipping_amount',
-          'default_value' => '0',
-          'title' => language::translate(__CLASS__.':title_free_shipping_amount', 'Free Shipping Amount'),
-          'description' => language::translate(__CLASS__.':description_free_shipping_amount', 'Enable free shipping for orders that meet the given cart total amount or above (excluding tax). 0 = disabled'),
-          'function' => 'decimal()',
-        ),
-        array(
-          'key' => 'free_shipping_geo_zone_id',
-          'default_value' => '',
-          'title' => language::translate('title_free_shipping_geo_zone', 'Free Shipping Geo Zone'),
-          'description' => language::translate('modules:description_free_shipping_geo_zone', 'Limit free shipping to the selected geo zone. Otherwise leave blank.'),
-          'function' => 'geo_zones()',
+          'key' => 'free_shipping_table',
+          'default_value' => 'country_code,min_subtotal',
+          'title' => language::translate(__CLASS__.':title_free_shipping_table', 'Free Shipping Table'),
+          'description' => language::translate(__CLASS__.':description_free_shipping_table', 'Free shipping table in standard CSV format with columns.'),
+          'function' => 'bigtext()',
         ),
         array(
           'key' => 'priority',
