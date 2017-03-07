3d02cd78db88ee41139f8e3fa1a3dac97a71e31c
timint <info@tim-international.net>
2017-03-07 23:28:13 +0100

    * Fix issue #10

--- public_html/includes/boxes/box_recently_viewed_products.inc.php
+++ public_html/includes/boxes/box_recently_viewed_products.inc.php
@@ -5,24 +5,47 @@
 
   functions::draw_lightbox();
 
+// Get from catalog
+  $product_ids = array_reverse(array_column(session::$data['recently_viewed_products'], 'id'));
+  $recently_viewed_products_query = functions::catalog_products_query(array('products' => $product_ids));
+
+  $recently_viewed_products = array();
+  while($product = database::fetch($recently_viewed_products_query)) {
+    $recently_viewed_products[$product['id']] = $product;
+  }
+
+// Sort
+  usort($recently_viewed_products, function ($a, $b) use ($product_ids) {
+    $pos_a = array_search($a['id'], $product_ids);
+    $pos_b = array_search($b['id'], $product_ids);
+    return $pos_a - $pos_b;
+  });
+
+// Create list
   $box_recently_viewed_products = new view();
   $box_recently_viewed_products->snippets['products'] = array();
 
   list($width, $height) = functions::image_scale_by_width(160, settings::get('product_image_ratio'));
 
   $count = 0;
-  foreach(array_reverse(session::$data['recently_viewed_products'], true) as $key => $current_product) {
-    if (++$count <= settings::get('box_recently_viewed_products_num_items')) {
-      $box_recently_viewed_products->snippets['products'][$key] = array(
-        'id' => $current_product['id'],
-        'name' => $current_product['name'],
-        'thumbnail' => functions::image_thumbnail(FS_DIR_HTTP_ROOT . WS_DIR_IMAGES . $current_product['image'], $width, $height, settings::get('product_image_clipping'), settings::get('product_image_trim')),
-        'link' => document::ilink('product', array('product_id' => $current_product['id'])),
+  foreach($recently_viewed_products as $product) {
+    $box_recently_viewed_products->snippets['products'][] = array(
+      'id' => $product['id'],
+      'name' => $product['name'],
+      'thumbnail' => functions::image_thumbnail(FS_DIR_HTTP_ROOT . WS_DIR_IMAGES . $product['image'], $width, $height, settings::get('product_image_clipping'), settings::get('product_image_trim')),
+      'link' => document::ilink('product', array('product_id' => $product['id'])),
       );
-    } else {
+    if (++$count >= settings::get('box_recently_viewed_products_num_items')) break;
+  }
+
+// Unset rest
+  $product_ids = array_column($box_recently_viewed_products->snippets['products'], 'id');
+  foreach(array_keys(session::$data['recently_viewed_products']) as $key) {
+    if (!in_array(session::$data['recently_viewed_products'][$key]['id'], $product_ids)) {
       unset(session::$data['recently_viewed_products'][$key]);
     }
   }
 
+// Output
   echo $box_recently_viewed_products->stitch('views/box_recently_viewed_products');
 ?>
