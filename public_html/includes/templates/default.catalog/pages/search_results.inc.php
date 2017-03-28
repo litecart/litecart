8f4b6a55139da3009d80376b1f6315a5dd250c97
timint <info@tim-international.net>
2017-03-28 18:38:45 +0200

    * Fix issue #44 - Search page html

--- public_html/includes/templates/default.catalog/pages/search_results.inc.php
+++ public_html/includes/templates/default.catalog/pages/search_results.inc.php
@@ -1,28 +1,30 @@
+<aside id="sidebar">
+  <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATE . 'views/column_left.inc.php'); ?>
+</aside>
+
 <main id="content">
   {snippet:notices}
 
   <div id="box-search-results" class="box">
-    <nav class="filter" style="float: right;">
+    <div class="btn-group pull-right hidden-xs">
 <?php
-    $separator = false;
-    foreach ($sort_alternatives as $key => $value) {
-      if ($separator) echo ' ';
-      if ($_GET['sort'] == $key) {
-        echo '<span class="button active">'. $value .'</span>';
-      } else {
-        echo '<a class="button" href="'. document::href_ilink(null, array('sort' => $key), true) .'">'. $value .'</a>';
-      }
-      $separator = true;
+  $separator = false;
+  foreach ($sort_alternatives as $key => $value) {
+    if ($_GET['sort'] == $key) {
+      echo '<span class="btn btn-default active">'. $value .'</span>';
+    } else {
+      echo '<a class="btn btn-default" href="'. document::href_ilink(null, array('sort' => $key), true) .'">'. $value .'</a>';
     }
+  }
 ?>
-    </nav>
+    </div>
 
     <h1 class="title"><?php echo $title; ?></h1>
 
     <div class="content">
 
       <?php if ($products) { ?>
-      <ul class="listing-wrapper products">
+      <ul class="products row half-gutter">
         <?php foreach ($products as $product) echo functions::draw_listing_product($product, 'column'); ?>
       </ul>
       <?php } ?>
