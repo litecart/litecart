30082cd47d9171f37ddf7c1cb0d37ca67d2675b1
timint <info@tim-international.net>
2017-04-12 01:01:43 +0200

    * Search engine rewritten

--- public_html/pages/search.inc.php
+++ public_html/pages/search.inc.php
@@ -1,7 +1,7 @@
 <?php
   if (empty($_GET['query'])) $_GET['query'] = '';
   if (empty($_GET['page'])) $_GET['page'] = 1;
-  if (empty($_GET['sort'])) $_GET['sort'] = 'occurrences';
+  if (empty($_GET['sort'])) $_GET['sort'] = 'relevance';
 
   $_GET['query'] = trim($_GET['query']);
 
@@ -17,6 +17,7 @@
     'title' => sprintf(language::translate('title_search_results_for_s', 'Search Results for &quot;%s&quot;'), htmlspecialchars($_GET['query'])),
     'products' => array(),
     'sort_alternatives' => array(
+      'relevance' => language::translate('title_Relevance', 'Relevance'),
       'name' => language::translate('title_name', 'Name'),
       'price' => language::translate('title_price', 'Price'),
       'popularity' => language::translate('title_popularity', 'Popularity'),
@@ -25,41 +26,9 @@
     'pagination' => null,
   );
 
-  if (empty($_GET['query'])) {
-    $_page->snippets['title'] = language::translate('title_all_products', 'All Products');
-  }
-
-  $manufacturers_info_query = database::query(
-    "select m.id
-    from ". DB_TABLE_MANUFACTURERS ." m, ". DB_TABLE_MANUFACTURERS_INFO ." mi
-    where m.status
-    and (mi.manufacturer_id = m.id and mi.language_code = '". database::input(language::$selected['code']) ."')
-    and (
-      m.name like '%". database::input($_GET['query']) ."%'
-      or mi.description like '%". database::input($_GET['query']) ."%'
-    );"
-  );
-
-  $manufacturer_ids = array();
-  while ($manufacturer = database::fetch($manufacturers_info_query)) {
-    $manufacturer_ids[] = (int)$manufacturer['id'];
-  }
-
-  $sql_select_occurrences = "(0
-    + if(p.gtin like '%". database::input($_GET['query']) ."%', 3, 0)
-    + if(p.sku like '%". database::input($_GET['query']) ."%', 3, 0)
-    + if(p.code like '%". database::input($_GET['query']) ."%', 3, 0)
-    + if(p.keywords like '%". database::input($_GET['query']) ."%', 3, 0)
-    + if(pi.name like '%". database::input($_GET['query']) ."%', 3, 0)
-    + if(pi.short_description like '%". database::input($_GET['query']) ."%', 1, 0)
-    + if(pi.description like '%". database::input($_GET['query']) ."%', 1, 0)
-    + if(p.manufacturer_id and p.manufacturer_id in ('". implode("', '", database::input($manufacturer_ids)) ."'), 2, 0)
-  ) as occurrences";
-
-  $sql_price_column = "if(pp.`". database::input(currency::$selected['code']) ."`, pp.`". database::input(currency::$selected['code']) ."` / ". (float)currency::$selected['value'] .", pp.`". database::input(settings::get('store_currency_code')) ."`)";
-
   $query =
-    "select p.*, pi.name, pi.short_description, m.name as manufacturer_name, ". $sql_price_column ." as price, pc.campaign_price, if(pc.campaign_price, pc.campaign_price, if(pc.campaign_price, pc.campaign_price, ". $sql_price_column .")) as final_price, " . $sql_select_occurrences ."
+    "select p.*, pi.name, pi.short_description, m.name as manufacturer_name, pp.price, pc.campaign_price, if(pc.campaign_price, pc.campaign_price, if(pc.campaign_price, pc.campaign_price, pp.price)) as final_price,
+    match(p.code, p.sku, p.gtin, pi.name, pi.short_description, pi.description, m.name) against ('". database::input($_GET['query']) ."' in boolean mode) as relevance
 
     from (
       select id, code, gtin, sku, manufacturer_id, default_category_id, keywords, product_groups, image, tax_class_id, quantity, views, purchases, date_updated, date_created
@@ -70,8 +39,14 @@
     ) p
 
     left join ". DB_TABLE_PRODUCTS_INFO ." pi on (pi.product_id = p.id and pi.language_code = '". language::$selected['code'] ."')
+
     left join ". DB_TABLE_MANUFACTURERS ." m on (m.id = p.manufacturer_id)
-    left join ". DB_TABLE_PRODUCTS_PRICES ." pp on (pp.product_id = p.id)
+
+    left join (
+      select product_id, if(`". database::input(currency::$selected['code']) ."`, `". database::input(currency::$selected['code']) ."` / ". (float)currency::$selected['value'] .", `". database::input(settings::get('store_currency_code')) ."`) as price
+      from ". DB_TABLE_PRODUCTS_PRICES ."
+    ) pp on (pp.product_id = p.id)
+
     left join (
       select product_id, if(`". database::input(currency::$selected['code']) ."`, `". database::input(currency::$selected['code']) ."` / ". (float)currency::$selected['value'] .", `". database::input(settings::get('store_currency_code')) ."`) as campaign_price
       from ". DB_TABLE_PRODUCTS_CAMPAIGNS ."
@@ -79,16 +54,8 @@
       and (year(end_date) < '1971' or end_date >= '". date('Y-m-d H:i:s') ."')
       order by end_date asc
     ) pc on (pc.product_id = p.id)
-    where (
-      p.code like '%". database::input($_GET['query']) ."%'
-      or p.sku like '%". database::input($_GET['query']) ."%'
-      or p.gtin like '%". database::input($_GET['query']) ."%'
-      or p.keywords like '%". database::input($_GET['query']) ."%'
-      ". (!empty($manufacturer_ids) ? "or p.manufacturer_id in (". implode(", ", $manufacturer_ids) .")" : false) ."
-      or pi.name like '%". database::input($_GET['query']) ."%'
-      or pi.short_description like '%". database::input($_GET['query']) ."%'
-      or pi.description like '%". database::input($_GET['query']) ."%'
-    )
+
+    having relevance > 0
 
     order by %sql_sort;
   ";
@@ -103,16 +70,14 @@
     case 'date':
       $query = str_replace("%sql_sort", "date_created desc", $query);
       break;
-    case 'occurrences':
-      $query = str_replace("%sql_sort", "occurrences desc", $query);
-      break;
     case 'rand':
       $query = str_replace("%sql_sort", "rand()", $query);
       break;
     case 'popularity':
-    default:
       $query = str_replace("%sql_sort", "(p.purchases / (datediff(now(), p.date_created)/7)) desc, (p.views / (datediff(now(), p.date_created)/7)) desc", $query);
-      $sql_global_sort = "";
+      break;
+    default:
+      $query = str_replace("%sql_sort", "relevance desc", $query);
       break;
   }
 
