a1031153339ea1d8971ea722aebe530d517b44cb
timint <info@tim-international.net>
2017-04-12 01:30:10 +0200

    * Fallback on default catalog template upon missing view

--- public_html/includes/classes/vmod.inc.php
+++ public_html/includes/classes/vmod.inc.php
@@ -3,6 +3,10 @@
 
     public static function check($file) {
 
+      if (preg_match('#^('. preg_quote(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES, '#') .'[^/]+)/#', $file, $matches)) {
+        if (!file_exists($file)) $file = preg_replace('#^('. preg_quote($matches[1], '#') .')#', FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES . 'default.catalog/', $file);
+      }
+
       if (!class_exists('vqmod', false)) {
         if (is_file(FS_DIR_HTTP_ROOT . WS_DIR_HTTP_HOME . 'vqmod/vqmod.php')) {
           require_once FS_DIR_HTTP_ROOT . WS_DIR_HTTP_HOME . 'vqmod/vqmod.php';
