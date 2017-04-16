3360614766060854b2678ae014426e11f3ccbc9a
timint <info@tim-international.net>
2017-04-17 00:14:41 +0200

    * Footer table to div grid

--- public_html/includes/templates/default.catalog/views/box_site_footer.inc.php
+++ public_html/includes/templates/default.catalog/views/box_site_footer.inc.php
@@ -2,65 +2,61 @@
 
   <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATE . 'views/site_cookie_notice.inc.php'); ?>
 
-  <table>
-    <tr>
-      <td class="categories">
-        <nav>
-          <h3 class="title"><?php echo language::translate('title_categories', 'Categories'); ?></h3>
-          <ul class="list-unstyled">
-            <?php foreach ($categories as $category) echo '<li><a href="'. htmlspecialchars($category['link']) .'">'. $category['name'] .'</a></li>' . PHP_EOL; ?>
-          </ul>
-        </nav>
-      </td>
+  <div class="row">
+    <div class="hidden-xs col-sm-fourths col-md-fifths categories">
+      <h3 class="title"><?php echo language::translate('title_categories', 'Categories'); ?></h3>
+      <ul class="list-unstyled">
+        <?php foreach ($categories as $category) echo '<li><a href="'. htmlspecialchars($category['link']) .'">'. $category['name'] .'</a></li>' . PHP_EOL; ?>
+      </ul>
+    </div>
 
-      <?php if ($manufacturers) { ?>
-      <td class="manufacturers">
-        <nav>
-          <h3 class="title"><?php echo language::translate('title_manufacturers', 'Manufacturers'); ?></h3>
-          <ul class="list-unstyled">
-          <?php foreach ($manufacturers as $manufacturer) echo '<li><a href="'. htmlspecialchars($manufacturer['link']) .'">'. $manufacturer['name'] .'</a></li>' . PHP_EOL; ?>
-          </ul>
-        </nav>
-      </td>
+    <?php if ($manufacturers) { ?>
+    <div class="hidden-xs hidden-sm col-md-fifths manufacturers">
+      <h3 class="title"><?php echo language::translate('title_manufacturers', 'Manufacturers'); ?></h3>
+      <ul class="list-unstyled">
+      <?php foreach ($manufacturers as $manufacturer) echo '<li><a href="'. htmlspecialchars($manufacturer['link']) .'">'. $manufacturer['name'] .'</a></li>' . PHP_EOL; ?>
+      </ul>
+    </div>
+    <?php } ?>
+
+    <div class="col-xs-halfs col-sm-fourths col-md-fifths account">
+      <h3 class="title"><?php echo language::translate('title_account', 'Account'); ?></h3>
+      <ul class="list-unstyled">
+        <li><a href="<?php echo document::ilink('customer_service'); ?>"><?php echo language::translate('title_customer_service', 'Customer Service'); ?></a></li>
+        <li><a href="<?php echo document::href_ilink('regional_settings'); ?>"><?php echo language::translate('title_regional_settings', 'Regional Settings'); ?></a></li>
+        <?php if (empty(customer::$data['id'])) { ?>
+        <li><a href="<?php echo document::href_ilink('create_account'); ?>"><?php echo language::translate('title_create_account', 'Create Account'); ?></a></li>
+        <li><a href="<?php echo document::href_ilink('login'); ?>"><?php echo language::translate('title_login', 'Login'); ?></a></li>
+        <?php } else { ?>
+        <li><a href="<?php echo document::href_ilink('order_history'); ?>"><?php echo language::translate('title_order_history', 'Order History'); ?></a></li>
+        <li><a href="<?php echo document::href_ilink('edit_account'); ?>"><?php echo language::translate('title_edit_account', 'Edit Account'); ?></a></li>
+        <li><a href="<?php echo document::href_ilink('logout'); ?>"><?php echo language::translate('title_logout', 'Logout'); ?></a></li>
+        <?php } ?>
+      </ul>
+    </div>
+
+    <div class="col-xs-halfs col-sm-fourths col-md-fifths information">
+      <h3 class="title"><?php echo language::translate('title_information', 'Information'); ?></h3>
+      <ul class="list-unstyled">
+        <?php foreach ($pages as $page) echo '<li><a href="'. htmlspecialchars($page['link']) .'">'. $page['title'] .'</a></li>' . PHP_EOL; ?>
+      </ul>
+    </div>
+
+    <div class="hidden-xs col-sm-fourths col-md-fifths contact">
+      <h3 class="title"><?php echo language::translate('title_contact', 'Contact'); ?></h3>
+
+      <p><?php echo nl2br(settings::get('store_postal_address')); ?></p>
+
+      <?php if (settings::get('store_phone')) { ?>
+      <p><a href="tel:<?php echo settings::get('store_phone'); ?>"><?php echo functions::draw_fonticon('fa-phone'); ?> <?php echo settings::get('store_phone'); ?></a><p>
       <?php } ?>
 
-      <td class="account">
-        <nav>
-          <h3 class="title"><?php echo language::translate('title_account', 'Account'); ?></h3>
-          <ul class="list-unstyled">
-            <li><a href="<?php echo document::ilink('customer_service'); ?>"><?php echo language::translate('title_customer_service', 'Customer Service'); ?></a></li>
-            <li><a href="<?php echo document::href_ilink('regional_settings'); ?>"><?php echo language::translate('title_regional_settings', 'Regional Settings'); ?></a></li>
-            <?php if (empty(customer::$data['id'])) { ?>
-            <li><a href="<?php echo document::href_ilink('create_account'); ?>"><?php echo language::translate('title_create_account', 'Create Account'); ?></a></li>
-            <li><a href="<?php echo document::href_ilink('login'); ?>"><?php echo language::translate('title_login', 'Login'); ?></a></li>
-            <?php } else { ?>
-            <li><a href="<?php echo document::href_ilink('order_history'); ?>"><?php echo language::translate('title_order_history', 'Order History'); ?></a></li>
-            <li><a href="<?php echo document::href_ilink('edit_account'); ?>"><?php echo language::translate('title_edit_account', 'Edit Account'); ?></a></li>
-            <li><a href="<?php echo document::href_ilink('logout'); ?>"><?php echo language::translate('title_logout', 'Logout'); ?></a></li>
-            <?php } ?>
-          </ul>
-        </nav>
-      </td>
-      <td class="information">
-        <nav>
-          <h3 class="title"><?php echo language::translate('title_information', 'Information'); ?></h3>
-          <ul class="list-unstyled">
-            <?php foreach ($pages as $page) echo '<li><a href="'. htmlspecialchars($page['link']) .'">'. $page['title'] .'</a></li>' . PHP_EOL; ?>
-          </ul>
-        </nav>
-      </td>
-      <td class="contact">
-        <h3 class="title"><?php echo language::translate('title_contact', 'Contact'); ?></h3>
-        <p><?php echo nl2br(settings::get('store_postal_address')); ?></p><br />
-        <p><?php echo settings::get('store_phone'); ?><br />
-          <a href="mailto:<?php echo settings::get('store_email'); ?>"><?php echo settings::get('store_email'); ?></a>
-        </p>
-      </td>
-    </tr>
-  </table>
+      <p><a href="mailto:<?php echo settings::get('store_email'); ?>"><?php echo functions::draw_fonticon('fa-envelope'); ?> <?php echo settings::get('store_email'); ?></a></p>
+    </div>
+  </div>
 </footer>
 
 <div id="copyright" class="twelve-eighty">
   <!-- LiteCart is provided free under license CC BY-ND 4.0 - https://creativecommons.org/licenses/by-nd/4.0/. Removing the link back to litecart.net without written permission is a violation. -->
-  <p>Copyright &copy; <?php echo date('Y'); ?> <?php echo settings::get('store_name'); ?>. All rights reserved &middot; Powered by <a href="http://www.litecart.net" target="_blank" title="Free e-commerce platform">LiteCart®</a></p>
+  <div class="notice">Copyright &copy; <?php echo date('Y'); ?> <?php echo settings::get('store_name'); ?>. All rights reserved &middot; Powered by <a href="http://www.litecart.net" target="_blank" title="Free e-commerce platform">LiteCart®</a></div>
 </div>
