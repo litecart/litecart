049ada41041f6ec8224612bd937fcc75085e86fa
timint <info@tim-international.net>
2017-05-11 03:33:57 +0200

    * Remove postcode_format for United Kingdom as some postcodes failed to validate

--- public_html/install/upgrade_patches/2.0.2.sql
+++ public_html/install/upgrade_patches/2.0.2.sql
@@ -1,1 +1,3 @@
 DELETE FROM `lc_zones` WHERE country_code = 'US' AND code IN ('AS','AF','AA','AC','AE','AM','AP','DC','FM','GU','MH','MP','PW','PR','VI');
+-- --------------------------------------------------------
+UPDATE `lc_countries` SET postcode_format = '' WHERE country_code = 'GB';
