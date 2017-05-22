eb9806c18382241c948381ce47dfdaa73db7d031
timint <info@tim-international.net>
2017-05-22 23:25:46 +0200

    * Adjust query

--- public_html/install/upgrade_patches/2.0.2.sql
+++ public_html/install/upgrade_patches/2.0.2.sql
@@ -2,7 +2,7 @@ DELETE FROM `lc_zones` WHERE country_code = 'US' AND code IN ('AS','AF','AA','AC
 -- --------------------------------------------------------
 UPDATE `lc_countries` SET postcode_format = '[a-zA-Z]{1,2}[0-9][0-9a-zA-Z]? ?[0-9][a-zA-Z]{2}' WHERE country_code = 'GB';
 -- --------------------------------------------------------
-DELETE FROM `lc_settings_groups` WHERE `key` = 'general' LIMIT 1;
+DELETE FROM `lc_settings_groups` WHERE `key` = 'general';
 -- --------------------------------------------------------
 UPDATE `lc_settings` SET setting_group_key = 'store_info' WHERE setting_group_key = 'general';
 -- --------------------------------------------------------
