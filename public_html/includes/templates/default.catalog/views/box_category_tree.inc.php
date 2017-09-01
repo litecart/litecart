894ea9fcbbf4559f4f7ef1c4e68d877158e71c00
timint <info@tim-international.net>
2017-09-01 21:51:09 +0200

    * Fix category tree icon

--- public_html/includes/templates/default.catalog/views/box_category_tree.inc.php
+++ public_html/includes/templates/default.catalog/views/box_category_tree.inc.php
@@ -6,7 +6,7 @@
     function custom_draw_category_tree($categories, $indent=0) {
       foreach ($categories as $category) {
         echo '  <li class="category-'. $category['id'] . (!empty($category['active']) ? ' active' : '') .'">' . PHP_EOL
-           . '    <a href="'. htmlspecialchars($category['link']) .'"><i class="fa fa-'. (empty($category['active']) ? 'plus' : 'minus') .'-square" style="font-size: 0.75em;"></i> '. $category['name'] .'</a>' . PHP_EOL;
+           . '    <a href="'. htmlspecialchars($category['link']) .'"><i class="fa fa-'. (empty($category['opened']) ? 'plus' : 'minus') .'-square" style="font-size: 0.75em;"></i> '. $category['name'] .'</a>' . PHP_EOL;
         if (!empty($category['subcategories'])) {
           echo '<ul class="nav nav-pills nav-stacked">' . PHP_EOL;
           echo PHP_EOL . custom_draw_category_tree($category['subcategories'], $indent+1);
