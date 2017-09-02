09bf9f073ab55202b78f664d99e56577cd318b82
timint <info@tim-international.net>
2017-09-02 17:41:02 +0200

    * Menu rewritten for Bootstrap navbar

--- public_html/includes/templates/default.catalog/views/box_site_menu.inc.php
+++ public_html/includes/templates/default.catalog/views/box_site_menu.inc.php
@@ -4,7 +4,7 @@
       $output = '<li data-type="'. $item['type'] .'" data-id="'. $item['id'] .'">'
               . '  <a href="'. htmlspecialchars($item['link']) .'">'. $item['title'] .'</a>';
       if (!empty($item['subitems'])) {
-        $output .= '  <ul>' . PHP_EOL;
+        $output .= '  <ul class="list-untyled">' . PHP_EOL;
         foreach ($item['subitems'] as $subitem) {
           $output .= custom_draw_site_menu_item($subitem, $indent+1);
         }
@@ -15,10 +15,60 @@
     }
   }
 ?>
-<nav id="site-menu" class="twelve-eighty">
-  <ul>
-    <li class="home"><a href="<?php echo document::href_ilink(''); ?>"><?php echo functions::draw_fonticon('fa-home'); ?></a></li>
-    <?php foreach ($categories as $item) echo custom_draw_site_menu_item($item); ?>
-    <?php foreach ($pages as $item) echo custom_draw_site_menu_item($item); ?>
-  </ul>
-</nav>
+<div id="site-menu" class="twelve-eighty">
+	<nav class="navbar navbar-default">
+
+	  <div class="navbar-header">
+	    <?php echo functions::form_draw_form_begin('search_form', 'get', document::ilink('search'), false, 'class="navbar-form"'); ?>
+	      <?php echo functions::form_draw_search_field('query', true, 'placeholder="'. language::translate('text_search_products', 'Search products') .' …"'); ?>
+	    <?php echo functions::form_draw_form_end(); ?>
+
+	    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#default-menu">
+	      <?php echo functions::draw_fonticon('fa-bars'); ?>
+	    </button>
+	  </div>
+
+	  <div id="default-menu" class="navbar-collapse collapse">
+
+	    <ul class="nav navbar-nav">
+	      <li class="hidden-xs">
+	        <a href="<?php echo document::ilink(''); ?>" class="navbar-brand"><i class="fa fa-home"></i></a>
+	      </li>
+
+        <?php if ($categories) { ?>
+	      <li class="dropdown">
+	        <a href="#" data-toggle="dropdown" class="dropdown-toggle"><?php echo language::translate('title_categories', 'Categories'); ?> <b class="caret"></b></a>
+	        <div class="dropdown-menu" style="width: 250px;">
+	          <ul class="list-unstyled">
+              <?php foreach ($categories as $item) echo custom_draw_site_menu_item($item); ?>
+	          </ul>
+	        </div>
+	      </li>
+        <?php } ?>
+
+        <?php if ($manufacturers) { ?>
+	      <li class="dropdown">
+	        <a href="#" data-toggle="dropdown" class="dropdown-toggle"><?php echo language::translate('title_manufacturers', 'Manufacturers'); ?> <b class="caret"></b></a>
+	        <div class="dropdown-menu" style="width: 250px;">
+	          <ul class="list-unstyled">
+              <?php foreach ($manufacturers as $item) echo custom_draw_site_menu_item($item); ?>
+	          </ul>
+	        </div>
+	      </li>
+        <?php } ?>
+
+        <?php if ($pages) { ?>
+	      <li class="dropdown">
+	        <a href="#" data-toggle="dropdown" class="dropdown-toggle"><?php echo language::translate('title_information', 'Information'); ?><b class="caret"></b></a>
+	        <div class="dropdown-menu" style="width: 640px;">
+	          <ul class="list-unstyled">
+              <?php foreach ($pages as $item) echo custom_draw_site_menu_item($item); ?>
+            </ul>
+	        </div>
+	      </li>
+        <?php } ?>
+
+	    </ul>
+	  </div>
+	</nav>
+</div>
