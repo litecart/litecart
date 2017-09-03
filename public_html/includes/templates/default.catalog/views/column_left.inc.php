e8c65f022594f4a74606a1866e468da053cf9bd6
timint <info@tim-international.net>
2017-09-04 00:16:50 +0200

    - Remove account links from column left

--- public_html/includes/templates/default.catalog/views/column_left.inc.php
+++ public_html/includes/templates/default.catalog/views/column_left.inc.php
@@ -3,10 +3,6 @@
     <div class="toggle">
       <label for="mobile-menu-toggle"><?php echo functions::draw_fonticon('fa-bars'); ?></label>
     </div>
-
-    <?php echo functions::form_draw_form_begin('search_form', 'get', document::ilink('search')); ?>
-      <?php echo functions::form_draw_search_field('query', true, 'placeholder="'. language::translate('text_search_products', 'Search products') .' …"'); ?>
-    <?php echo functions::form_draw_form_end(); ?>
   </div>
 
   <input id="mobile-menu-toggle" type="checkbox" style="display: none;" />
@@ -18,8 +14,6 @@
     <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_filter.inc.php'); ?>
 
     <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_recently_viewed_products.inc.php'); ?>
-
-    <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_account_links.inc.php'); ?>
 
   </div>
 </div>
