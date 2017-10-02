cfaea211372490c9b6a2a581529886ec2e6e8d6c
timint <info@tim-international.net>
2017-10-02 02:57:55 +0200

    * Navigation adjustments

--- public_html/includes/templates/default.catalog/views/box_site_menu.inc.php
+++ public_html/includes/templates/default.catalog/views/box_site_menu.inc.php
@@ -23,14 +23,16 @@
         <?php echo functions::form_draw_search_field('query', true, 'placeholder="'. language::translate('text_search_products', 'Search products') .' &hellip;"'); ?>
       <?php echo functions::form_draw_form_end(); ?>
 
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#default-menu">
-        <?php echo functions::draw_fonticon('fa-bars'); ?>
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#default-menu">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
       </button>
     </div>
 
     <div id="default-menu" class="navbar-collapse collapse">
 
-      <ul class="nav navbar-nav pull-left">
+      <ul class="nav navbar-nav">
         <li class="hidden-xs">
           <a href="<?php echo document::ilink(''); ?>" class="navbar-brand"><i class="fa fa-home"></i></a>
         </li>
@@ -63,8 +65,8 @@
         <?php } ?>
       </ul>
 
-      <ul class="nav navbar-nav pull-right">
-        <li class="dropdown pull-right">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
           <a href="#" data-toggle="dropdown" class="dropdown-toggle"><?php echo functions::draw_fonticon('fa-user'); ?> <?php echo !empty(customer::$data['id']) ? customer::$data['firstname'] : language::translate('title_sign_in', 'Sign In'); ?> <b class="caret"></b></a>
           <ul class="dropdown-menu">
             <?php if (!empty(customer::$data['id'])) { ?>
@@ -73,7 +75,7 @@
               <li><a href="<?php echo document::href_ilink('logout'); ?>"><?php echo language::translate('title_logout', 'Logout'); ?></a></li>
             <?php } else { ?>
               <li>
-                <?php echo functions::form_draw_form_begin('login_form', 'post', document::ilink('login'), false, 'class="navbar-form"'); ?>
+                <?php echo functions::form_draw_form_begin('login_form', 'post', document::ilink('login'), false, 'class="navbar-form" style="min-width: 300px;"'); ?>
                   <?php echo functions::form_draw_hidden_field('redirect_url', !empty($_GET['redirect_url']) ? $_GET['redirect_url'] : document::link()); ?>
 
                   <div class="form-group">
