74fb8c9a1d1b1cb34e1784028768829f134c7ce3
timint <info@tim-international.net>
2017-10-02 02:56:27 +0200

    - Remove collapsable from column left

--- public_html/includes/templates/default.catalog/views/column_left.inc.php
+++ public_html/includes/templates/default.catalog/views/column_left.inc.php
@@ -1,19 +1,9 @@
 <div id="column-left">
-  <div id="navigation">
-    <div class="toggle">
-      <label for="mobile-menu-toggle"><?php echo functions::draw_fonticon('fa-bars'); ?></label>
-    </div>
-  </div>
 
-  <input id="mobile-menu-toggle" type="checkbox" style="display: none;" />
+  <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_category_tree.inc.php'); ?>
 
-  <div class="content">
+  <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_filter.inc.php'); ?>
 
-    <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_category_tree.inc.php'); ?>
+  <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_recently_viewed_products.inc.php'); ?>
 
-    <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_filter.inc.php'); ?>
-
-    <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_recently_viewed_products.inc.php'); ?>
-
-  </div>
 </div>
