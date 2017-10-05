e5437ef8296cb7cdc0a9220b32b27b0edbbd10a3
timint <info@tim-international.net>
2017-10-05 21:38:50 +0200

    * Raise number of characters allowed for postcode (Thanks Johnny for reporting problem with Brazil)

--- public_html/install/upgrade_patches/2.1.sql
+++ public_html/install/upgrade_patches/2.1.sql
@@ -9,3 +9,11 @@ INSERT INTO `lc_settings` (`setting_group_key`, `type`, `title`, `description`, 
 ('email', 'local', 'SMTP Port', 'SMTP port e.g. 25, 465 (SSL/TLS), or 587 (STARTTLS).', 'smtp_port', '25', 'number()', 12, NOW(), NOW()),
 ('email', 'local', 'SMTP Username', 'Username for SMTP authentication.', 'smtp_username', '', 'input()', 13, NOW(), NOW()),
 ('email', 'local', 'SMTP Password', 'Password for SMTP authentication.', 'smtp_password', '', 'input()', 14, NOW(), NOW());
+-- --------------------------------------------------------
+ALTER TABLE `lc_customers`
+CHANGE COLUMN `postcode` `postcode` VARCHAR(16) NOT NULL COLLATE 'utf8_swedish_ci',
+CHANGE COLUMN `shipping_postcode` `shipping_postcode` VARCHAR(16) NOT NULL COLLATE 'utf8_swedish_ci';
+-- --------------------------------------------------------
+ALTER TABLE `lc_orders`
+CHANGE COLUMN `customer_postcode` `customer_postcode` VARCHAR(16) NOT NULL COLLATE 'utf8_swedish_ci',
+CHANGE COLUMN `shipping_postcode` `shipping_postcode` VARCHAR(16) NOT NULL COLLATE 'utf8_swedish_ci';
