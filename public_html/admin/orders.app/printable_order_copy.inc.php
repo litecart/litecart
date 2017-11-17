5db9c52383f4a04363da6dee2eec3fa8694a0216
timint <info@tim-international.net>
2017-11-17 17:26:06 +0100

    - Remove draw_printable_copy() and draw_printable_packing_slip() as no longer necessary

--- public_html/admin/orders.app/printable_order_copy.inc.php
+++ public_html/admin/orders.app/printable_order_copy.inc.php
@@ -5,7 +5,14 @@
 
   $order = new ctrl_order($_GET['order_id']);
 
-  echo $order->draw_printable_copy();
+  $session_language = language::$selected['code'];
+  language::set($order->data['language_code']);
+
+  $_page = new view();
+  $_page->snippets['order'] = $order->data;
+  echo $_page->stitch('pages/printable_order_copy');
+
+  language::set($session_language);
 
   require_once('../includes/app_footer.inc.php');
   exit;
