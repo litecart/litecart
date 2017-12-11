7c4dbe37756b8a5a6de43d8c76b69b41cbf0552b
timint <info@tim-international.net>
2017-12-11 14:38:18 +0100

    * Change col-sm-halfs to col-sm-6

--- public_html/includes/templates/default.catalog/views/listing_category.inc.php
+++ public_html/includes/templates/default.catalog/views/listing_category.inc.php
@@ -1,4 +1,4 @@
-<div class="category category-<?php echo $category_id; ?> col-xs col-sm-halfs col-md-thirds">
+<div class="category category-<?php echo $category_id; ?> col-xs-12 col-sm-6 col-md-4">
   <a class="link shadow hover-light" href="<?php echo htmlspecialchars($link); ?>">
     <img src="<?php echo htmlspecialchars($image['thumbnail']); ?>" alt="" title="<?php echo htmlspecialchars($name); ?>" />
     <div class="caption">
