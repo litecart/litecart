cfd488d849ee4506afa3949d0493fc8a8470447c
timint <info@tim-international.net>
2017-12-11 14:43:44 +0100

    - Remove deprecated css

--- public_html/includes/templates/default.admin/pages/doc.inc.php
+++ public_html/includes/templates/default.admin/pages/doc.inc.php
@@ -1,16 +1,9 @@
 <style>
-<?php $rgb = implode(', ', sscanf($theme['color'], "#%02x%02x%02x")); ?>
 #main {
-  background: #fff; /* Old browsers */
-  background: -moz-linear-gradient(-45deg, rgba(<?php echo $rgb; ?>, 1) 0px, rgba(255,255,255,1) 100px); /* FF3.6+ */
-  background: -webkit-gradient(linear, left top, right bottom, color-stop(0px, rgba(<?php echo $rgb; ?>, 1)), color-stop(100px,rgba(255,255,255,1))); /* Chrome,Safari4+ */
-  background: -webkit-linear-gradient(-45deg, rgba(<?php echo $rgb; ?>, 1) 0px, rgba(255,255,255,1) 100px); /* Chrome10+,Safari5.1+ */
-  background: -o-linear-gradient(-45deg, rgba(<?php echo $rgb; ?>, 1) 0px, rgba(255,255,255,1) 100px); /* Opera 11.10+ */
-  background: -ms-linear-gradient(-45deg, rgba(<?php echo $rgb; ?>, 1) 0px, rgba(255,255,255,1) 100px); /* IE10+ */
-  background: linear-gradient(135deg, rgba(<?php echo $rgb; ?>, 1) 0px, rgba(255,255,255,1) 100px); /* W3C */
+  background: linear-gradient(135deg, rgba(<?php echo implode(', ', sscanf($theme['color'], "#%02x%02x%02x")); ?>, 1) 0px, rgba(255,255,255,1) 100px);
 }
 </style>
 
-<span style="float: right; margin-left: 10px;"><a href="<?php echo htmlspecialchars($help_link); ?>" target="_blank" title="<?php echo language::translate('title_help', 'Help'); ?>"><?php echo functions::draw_fonticon('fa-question-circle', 'style="font-size: 40px; color: #0099cc;"'); ?></a></span>
+<span class="pull-right" style="margin-left: 15px;"><a href="<?php echo htmlspecialchars($help_link); ?>" target="_blank" title="<?php echo language::translate('title_help', 'Help'); ?>"><?php echo functions::draw_fonticon('fa-question-circle', 'style="font-size: 40px; color: #0099cc;"'); ?></a></span>
 
 {snippet:doc}
