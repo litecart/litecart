9b6468204b93e73a9b702facc1c3532ab766cf83
timint <info@tim-international.net>
2018-02-03 21:59:05 +0100

    * Fix follow redirect in HTTP Client

--- public_html/includes/classes/http_client.inc.php
+++ public_html/includes/classes/http_client.inc.php
@@ -1,14 +1,14 @@
 <?php
 
   class http_client {
-    public $follow_redirects = true;
+    public $follow_redirects = false;
     public $timeout = 20;
     public $last_request;
     public $last_response;
 
     public function call($method, $url='', $data=null, $headers=array(), $asynchronous=false) {
 
-    // Backwards compatibility (where first param was URL)
+    // Backwards compatibility (where first param was URL supporting only GET/POST)
       if (strpos($method, '://') !== false) {
         list($url, $data, $headers) = func_get_args();
         $method = !empty($data) ? 'POST' : 'GET';
@@ -93,18 +93,6 @@
           continue;
         }
 
-        if (preg_match('#^Location:\s?(.*)?$#i', $line, $matches)) {
-
-          $redirect_url = !empty($matches[1]) ? trim($matches[1]) : $url;
-
-          if ($this->follow_redirects) {
-            return $this->call($method, $redirect_url, $data, $headers);
-          }
-
-          trigger_error('Destination is pointing to a new destination while follow redirect is disabled ('. $redirect_url .')');
-          return false;
-        }
-
         $response_header .= $line;
       }
 
@@ -116,13 +104,26 @@
       }
 
       preg_match('#HTTP/1\.(1|0)\s(\d{3})#', $response_header, $matches);
+      $status_code = $matches[2];
 
       $this->last_response['timestamp'] = time();
-      $this->last_response['status_code'] = $matches[2];
+      $this->last_response['status_code'] = $status_code;
       $this->last_response['head'] = $response_header . "\r\n";
       $this->last_response['duration'] = round(microtime(true) - $microtime_start, 3);
       $this->last_response['bytes'] = strlen($response_header . "\r\n" . $response_body);
       $this->last_response['body'] = $response_body;
+
+    // Redirect
+      if ($status_code == 301) {
+        if (!$this->follow_redirects) {
+          trigger_error('Destination is redirecting to another destination but follow_redirects is disabled', E_USER_WARNING);
+        } else if (preg_match('#^Location:\s?(.*)?$#im', $line, $matches)) {
+          $redirect_url = !empty($matches[1]) ? trim($matches[1]) : $url;
+          return $this->call($method, $redirect_url, $data, $headers);
+        } else {
+          trigger_error('Destination is redirecting to a null destination', E_USER_WARNING);
+        }
+      }
 
       return $response_body;
     }
