636fa70f36a2fbb7fa7ad45354960aa2d41c23a6
timint <info@tim-international.net>
2018-02-07 22:10:07 +0100

    * Upgrade jQuery 3.3.1

--- public_html/install/upgrade_patches/2.1.1.inc.php
+++ public_html/install/upgrade_patches/2.1.1.inc.php
@@ -1,2 +1,12 @@
 <?php
-  // Nothing to do
+
+// Delete some files
+  $deleted_files = array(
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT .'jquery/jquery-3.2.1.min.js',
+  );
+
+  foreach ($deleted_files as $pattern) {
+    if (!file_delete($pattern)) {
+      die('<span class="error">[Error]</span></p>');
+    }
+  }
