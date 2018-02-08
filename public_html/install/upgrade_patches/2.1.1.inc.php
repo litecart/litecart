d080e37f15f9da2abb75a6fd190c276439ae9dcd
timint <info@tim-international.net>
2018-02-08 21:50:53 +0100

    - Remove panels.less from catalog template (we are not using it)

--- public_html/install/upgrade_patches/2.1.1.inc.php
+++ public_html/install/upgrade_patches/2.1.1.inc.php
@@ -5,6 +5,7 @@
     FS_DIR_HTTP_ROOT . WS_DIR_EXT .'jquery/jquery-3.2.1.min.js',
     FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES .'default.admin/less/framework/navigation.less',
     FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES .'default.catalog/less/framework/navigation.less',
+    FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES .'default.catalog/less/framework/panels.less',
   );
 
   foreach ($deleted_files as $pattern) {
