cfd064fa720996e85819e5348305a8399c128962
timint <info@tim-international.net>
2018-02-08 21:29:06 +0100

    - Remove nav-default, nav-primary variants as we are only using one

--- public_html/includes/templates/default.catalog/views/box_account_links.inc.php
+++ public_html/includes/templates/default.catalog/views/box_account_links.inc.php
@@ -2,7 +2,7 @@
 
   <h2 class="title"><?php echo language::translate('title_account', 'Account'); ?></h2>
 
-  <ul class="nav nav-pills nav-primary nav-stacked">
+  <ul class="nav nav-stacked nav-pills">
     <li><a href="<?php echo document::href_ilink('order_history'); ?>"<?php echo (route::$route['page'] == 'order_history') ? ' class="active"' : ''; ?>><?php echo language::translate('title_order_history', 'Order History'); ?></a></li>
     <li><a href="<?php echo document::href_ilink('edit_account'); ?>"<?php echo (route::$route['page'] == 'edit_account') ? ' class="active"' : ''; ?>><?php echo language::translate('title_edit_account', 'Edit Account'); ?></a></li>
     <li><a href="<?php echo document::href_ilink('logout'); ?>"><?php echo language::translate('title_logout', 'Logout'); ?></a></li>
