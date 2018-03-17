6ed48178172dca2ab1bae3c57f25422432c58c76
timint <info@tim-international.net>
2018-03-17 20:51:05 +0100

    * Honour the page reference object

--- public_html/pages/customer_service.inc.php
+++ public_html/pages/customer_service.inc.php
@@ -17,30 +17,30 @@
 // Custom page
   if (!empty($_GET['page_id'])) {
 
-    $pages_query = database::query(
-      "select p.id, p.status, pi.title, pi.content, pi.head_title, pi.meta_description from ". DB_TABLE_PAGES ." p
-      left join ". DB_TABLE_PAGES_INFO ." pi on (p.id = pi.page_id and pi.language_code = '". language::$selected['code'] ."')
-      where p.id = '". (int)$_GET['page_id'] ."'
-      limit 1;"
-    );
-    $page = database::fetch($pages_query);
+    $page = reference::page($_GET['page_id']);
 
-    if (empty($page['status'])) {
-      notices::add('errors', language::translate('error_page_not_found', 'The requested page could not be found'));
-      http_response_code(404);
-      header('Location: '. document::ilink(''));
-      exit;
+    if (empty($page->id)) {
+      notices::add('errors', language::translate('error_410_gone', 'The requested file is no longer available'));
+      http_response_code(410);
+      header('Refresh: 0; url='. document::ilink(''));
+      die('HTTP Error 410 Gone');
     }
 
-    document::$snippets['title'][] = !empty($page['head_title']) ? $page['head_title'] : $page['title'];
-    document::$snippets['keywords'] = !empty($page['meta_keywords']) ? $page['meta_keywords'] : '';
-    document::$snippets['description'] = !empty($page['meta_description']) ? $page['meta_description'] : '';
+    if (empty($page->status)) {
+      notices::add('errors', language::translate('error_404_not_found', 'The requested file could not be found'));
+      http_response_code(404);
+      header('Refresh: 0; url='. document::ilink(''));
+      die('HTTP Error 404 Not Found');
+    }
 
-    breadcrumbs::add($page['title']);
+    document::$snippets['title'][] = !empty($page->head_title) ? $page->head_title : $page->title;
+    document::$snippets['description'] = !empty($page->meta_description) ? $page->meta_description : '';
+
+    breadcrumbs::add($page->title);
 
     $_page->snippets += array(
-      'title' => $page['title'],
-      'content' => $page['content'],
+      'title' => $page->title,
+      'content' => $page->content,
     );
 
   } else {
--- public_html/pages/information.inc.php
+++ public_html/pages/information.inc.php
@@ -1,38 +1,31 @@
 <?php
 
-// Information page
-  $pages_query = database::query(
-    "select p.id, p.status, pi.title, pi.content, pi.head_title, pi.meta_description from ". DB_TABLE_PAGES ." p
-    left join ". DB_TABLE_PAGES_INFO ." pi on (p.id = pi.page_id and pi.language_code = '". language::$selected['code'] ."')
-    where p.id = '". (int)$_GET['page_id'] ."'
-    limit 1;"
-  );
-  $page = database::fetch($pages_query);
+  $page = reference::page($_GET['page_id']);
 
-  if (empty($page['status'])) {
+  if (empty($page->id)) {
     notices::add('errors', language::translate('error_410_gone', 'The requested file is no longer available'));
     http_response_code(410);
     header('Refresh: 0; url='. document::ilink(''));
     die('HTTP Error 410 Gone');
   }
 
-  if (empty($page['status'])) {
+  if (empty($page->status)) {
     notices::add('errors', language::translate('error_404_not_found', 'The requested file could not be found'));
     http_response_code(404);
     header('Refresh: 0; url='. document::ilink(''));
     die('HTTP Error 404 Not Found');
   }
 
-  document::$snippets['title'][] = !empty($page['head_title']) ? $page['head_title'] : $page['title'];
-  document::$snippets['description'] = !empty($page['meta_description']) ? $page['meta_description'] : '';
+  document::$snippets['title'][] = !empty($page->head_title) ? $page->head_title : $page->title;
+  document::$snippets['description'] = !empty($page->meta_description) ? $page->meta_description : '';
 
-  breadcrumbs::add($page['title']);
+  breadcrumbs::add($page->title);
 
   $_page = new view();
 
   $_page->snippets = array(
-    'title' => $page['title'],
-    'content' => $page['content'],
+    'title' => $page->title,
+    'content' => $page->content,
   );
 
   echo $_page->stitch('pages/information');
