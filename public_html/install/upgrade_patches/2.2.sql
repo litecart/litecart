fb5cffd15f5f94f862dcdb04f826bfff741bedcc
timint <info@tim-international.net>
2018-04-05 00:38:55 +0200

    * Fix upgrade patch

--- public_html/install/upgrade_patches/2.2.sql
+++ public_html/install/upgrade_patches/2.2.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `lc_customers`
 ADD COLUMN `last_ip` VARCHAR(39) NOT NULL AFTER `password_reset_token`,
-ADD COLUMN `last_agent` VARCHAR(256) NOT NULL AFTER `last_ip`
+ADD COLUMN `last_agent` VARCHAR(256) NOT NULL AFTER `last_ip`,
 ADD COLUMN `date_login` DATETIME NOT NULL AFTER `last_ip`;
 -- --------------------------------------------------------
 ALTER TABLE `lc_orders`
