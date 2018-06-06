d2a566826d828a1c8401cf25be40487ad5225d55
timint <info@tim-international.net>
2018-06-06 22:52:47 +0200

    * Reinstate box_categories

--- public_html/pages/categories.inc.php
+++ public_html/pages/categories.inc.php
@@ -5,22 +5,4 @@
   breadcrumbs::add(language::translate('title_categories', 'Categories'));
 
   $_page = new view();
-
-  $categories_cache_id = cache::cache_id('categories', array('language'));
-  if (!$_page->snippets['categories'] = cache::get($categories_cache_id, 'file')) {
-
-    $categories_query = functions::catalog_categories_query();
-    if (database::num_rows($categories_query)) {
-
-      $_page->snippets = array(
-        'categories' => array(),
-      );
-
-      while ($category = database::fetch($categories_query)) {
-        $_page->snippets['categories'][] = $category;
-      }
-    }
-    cache::set($categories_cache_id, 'file', $_page->snippets['categories']);
-  }
-
   echo $_page->stitch('pages/categories');
