f0eb7942c3e8112304f03d52edd0c89639fdb916
timint <info@tim-international.net>
2018-10-05 03:04:43 +0200

    - Remove duplicate notices snippet as already present in ajax.inc.php

--- public_html/includes/templates/default.catalog/pages/product.ajax.inc.php
+++ public_html/includes/templates/default.catalog/pages/product.ajax.inc.php
@@ -1,5 +1,3 @@
-{snippet:notices}
-
 <div id="view-full-page">
   <a href="<?php echo htmlspecialchars($link); ?>"><?php echo language::translate('text_view_full_page', 'View full page'); ?> <?php echo functions::draw_fonticon('fa-external-link'); ?></a>
 </div>
