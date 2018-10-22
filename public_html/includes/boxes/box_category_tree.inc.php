8c94466baabd50d8e7462f5d4cda30b84f40b45e
timint <info@tim-international.net>
2018-10-22 03:37:25 +0200

    * Inline functions to anonymous functions

--- public_html/includes/boxes/box_category_tree.inc.php
+++ public_html/includes/boxes/box_category_tree.inc.php
@@ -17,43 +17,41 @@
       'category_path' => $category_path,
     );
 
-    if (!function_exists('custom_build_category_tree')) {
-      function custom_build_category_tree($parent_id, $level, $category_path, &$output) {
+    $iterator = function($parent_id, $level, $category_path, &$output, &$iterator) {
 
-        $categories_query = database::query(
-          "select c.id, c.parent_id, c.image, ci.name, ci.short_description, c.priority, c.date_updated from ". DB_TABLE_CATEGORIES ." c
-          left join ". DB_TABLE_CATEGORIES_INFO ." ci on (ci.category_id = c.id and ci.language_code = '". database::input(language::$selected['code']) ."')
-          where c.status
-          ". (!empty($parent_id) ? "and c.parent_id = ". (int)$parent_id ."" : "and find_in_set('tree', c.dock)") ."
-          order by c.priority asc, ci.name asc;"
+      $categories_query = database::query(
+        "select c.id, c.parent_id, c.image, ci.name, ci.short_description, c.priority, c.date_updated from ". DB_TABLE_CATEGORIES ." c
+        left join ". DB_TABLE_CATEGORIES_INFO ." ci on (ci.category_id = c.id and ci.language_code = '". database::input(language::$selected['code']) ."')
+        where c.status
+        ". (!empty($parent_id) ? "and c.parent_id = ". (int)$parent_id ."" : "and find_in_set('tree', c.dock)") ."
+        order by c.priority asc, ci.name asc;"
+      );
+
+      while ($category = database::fetch($categories_query)) {
+        $output[$category['id']] = array(
+          'id' => $category['id'],
+          'parent_id' => $category['parent_id'],
+          'name' => $category['name'],
+          'link' => document::ilink('category', array('category_id' => $category['id']), false),
+          'active' => (!empty($_GET['category_id']) && $category['id'] == $_GET['category_id']) ? true : false,
+          'opened' => (!empty($category_path) && in_array($category['id'], $category_path)) ? true : false,
+          'subcategories' => array(),
         );
 
-        while ($category = database::fetch($categories_query)) {
-          $output[$category['id']] = array(
-            'id' => $category['id'],
-            'parent_id' => $category['parent_id'],
-            'name' => $category['name'],
-            'link' => document::ilink('category', array('category_id' => $category['id']), false),
-            'active' => (!empty($_GET['category_id']) && $category['id'] == $_GET['category_id']) ? true : false,
-            'opened' => (!empty($category_path) && in_array($category['id'], $category_path)) ? true : false,
-            'subcategories' => array(),
-          );
-
-          if (in_array($category['id'], $category_path)) {
-            $sub_categories_query = functions::catalog_categories_query($category['id']);
-            if (database::num_rows($sub_categories_query) > 0) {
-              custom_build_category_tree($category['id'], $level+1, $category_path, $output[$category['id']]['subcategories']);
-            }
+        if (in_array($category['id'], $category_path)) {
+          $sub_categories_query = functions::catalog_categories_query($category['id']);
+          if (database::num_rows($sub_categories_query) > 0) {
+            custom_build_category_tree($category['id'], $level+1, $category_path, $output[$category['id']]['subcategories']);
           }
         }
-
-        database::free($categories_query);
-
-        return $output;
       }
-    }
 
-    if ($box_category_tree->snippets['categories'] = custom_build_category_tree(0, 0, $category_path, $box_category_tree->snippets['categories'])) {
+      database::free($categories_query);
+
+      return $output;
+    };
+
+    if ($box_category_tree->snippets['categories'] = $iterator(0, 0, $category_path, $box_category_tree->snippets['categories'], $iterator)) {
       echo $box_category_tree->stitch('views/box_category_tree');
     }
 
--- public_html/includes/boxes/box_customer_service_links.inc.php
+++ public_html/includes/boxes/box_customer_service_links.inc.php
@@ -27,46 +27,44 @@
       'subpages' => array(),
     );
 
-    if (!function_exists('custom_build_custmer_service_links_tree')) {
-      function custom_build_custmer_service_links_tree($parent_id, $level, $current_page_path) {
+    $iterator = function($parent_id, $level, $current_page_path, &$iterator) {
 
-        $pages_query = database::query(
-          "select p.id, p.parent_id, pi.title, p.priority, p.date_updated from ". DB_TABLE_PAGES ." p
-          left join ". DB_TABLE_PAGES_INFO ." pi on (pi.page_id = p.id and pi.language_code = '". database::input(language::$selected['code']) ."')
-          where p.status
-          ". (!empty($parent_id) ? "and p.parent_id = ". (int)$parent_id ."" : "and find_in_set('customer_service', p.dock)") ."
-          order by p.priority asc, pi.title asc;"
+      $pages_query = database::query(
+        "select p.id, p.parent_id, pi.title, p.priority, p.date_updated from ". DB_TABLE_PAGES ." p
+        left join ". DB_TABLE_PAGES_INFO ." pi on (pi.page_id = p.id and pi.language_code = '". database::input(language::$selected['code']) ."')
+        where p.status
+        ". (!empty($parent_id) ? "and p.parent_id = ". (int)$parent_id ."" : "and find_in_set('customer_service', p.dock)") ."
+        order by p.priority asc, pi.title asc;"
+      );
+
+      while ($page = database::fetch($pages_query)) {
+        $output[$page['id']] = array(
+          'id' => $page['id'],
+          'parent_id' => $page['parent_id'],
+          'title' => $page['title'],
+          'link' => document::ilink('customer_service', array('page_id' => $page['id']), false),
+          'active' => (!empty($_GET['page_id']) && $page['id'] == $_GET['page_id']) ? true : false,
+          'opened' => (!empty($current_page_path) && in_array($page['id'], $current_page_path)) ? true : false,
+          'subpages' => array(),
         );
 
-        while ($page = database::fetch($pages_query)) {
-          $output[$page['id']] = array(
-            'id' => $page['id'],
-            'parent_id' => $page['parent_id'],
-            'title' => $page['title'],
-            'link' => document::ilink('customer_service', array('page_id' => $page['id']), false),
-            'active' => (!empty($_GET['page_id']) && $page['id'] == $_GET['page_id']) ? true : false,
-            'opened' => (!empty($current_page_path) && in_array($page['id'], $current_page_path)) ? true : false,
-            'subpages' => array(),
+        if (in_array($page['id'], $current_page_path)) {
+          $sub_pages_query = database::query(
+            "select id from ". DB_TABLE_PAGES ."
+            where parent_id = ". (int)$page['id'] .";"
           );
-
-          if (in_array($page['id'], $current_page_path)) {
-            $sub_pages_query = database::query(
-              "select id from ". DB_TABLE_PAGES ."
-              where parent_id = ". (int)$page['id'] .";"
-            );
-            if (database::num_rows($sub_pages_query) > 0) {
-              $output[$page['id']]['subpages'] = custom_build_custmer_service_links_tree($page['id'], $level+1, $current_page_path);
-            }
+          if (database::num_rows($sub_pages_query) > 0) {
+            $output[$page['id']]['subpages'] = $iterator($page['id'], $level+1, $current_page_path, $iterator);
           }
         }
-
-        database::free($pages_query);
-
-        return $output;
       }
-    }
 
-    if ($box_customer_service_links->snippets['pages'] = custom_build_custmer_service_links_tree(0, 0, $current_page_path, $box_customer_service_links->snippets['pages'])) {
+      database::free($pages_query);
+
+      return $output;
+    };
+
+    if ($box_customer_service_links->snippets['pages'] = $iterator(0, 0, $current_page_path, $box_customer_service_links->snippets['pages'], $iterator)) {
       echo $box_customer_service_links->stitch('views/box_customer_service_links');
     }
 
