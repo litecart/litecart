a87da29c37beb8fb11c6972445a4530efa459c80
timint <info@tim-international.net>
2018-11-02 23:28:03 +0100

    * Minor adjustments

--- public_html/includes/templates/default.catalog/views/box_site_menu.inc.php
+++ public_html/includes/templates/default.catalog/views/box_site_menu.inc.php
@@ -43,7 +43,7 @@
 
       <ul class="nav navbar-nav">
         <li class="hidden-xs">
-          <a href="<?php echo document::ilink(''); ?>" title="<?php echo language::translate('title_home', 'Home'); ?>><?php echo functions::draw_fonticon('fa-home'); ?></a>
+          <a href="<?php echo document::ilink(''); ?>" title="<?php echo language::translate('title_home', 'Home'); ?>"><?php echo functions::draw_fonticon('fa-home'); ?></a>
         </li>
 
         <?php foreach ($categories as $item) echo custom_draw_site_menu_item($item); ?>
