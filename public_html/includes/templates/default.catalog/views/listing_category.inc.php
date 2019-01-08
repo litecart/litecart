f5fd048fffb0b557eb4c3666df61028dde7d452d
timint <info@tim-international.net>
2019-01-08 02:06:22 +0100

    * Replace listing item hover effect with CSS scale

--- public_html/includes/templates/default.catalog/views/listing_category.inc.php
+++ public_html/includes/templates/default.catalog/views/listing_category.inc.php
@@ -1,5 +1,5 @@
 <div class="col-xs-12 col-sm-6 col-md-4">
-  <div class="category hover-light" data-id="<?php echo $category_id; ?>" data-name="<?php echo htmlspecialchars($name); ?>">
+  <div class="category" data-id="<?php echo $category_id; ?>" data-name="<?php echo htmlspecialchars($name); ?>">
     <a class="link" href="<?php echo htmlspecialchars($link); ?>">
       <img class="img-responsive" src="<?php echo document::href_link(WS_DIR_HTTP_HOME . $image['thumbnail']); ?>" alt="" />
       <div class="caption">
