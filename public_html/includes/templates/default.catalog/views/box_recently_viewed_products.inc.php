40f53f55ad2f34a8e1b4f70b798337212dcb7951
timint <info@tim-international.net>
2019-01-09 13:29:25 +0100

    + Add tooltip to Recently Viewed Products

--- public_html/includes/templates/default.catalog/views/box_recently_viewed_products.inc.php
+++ public_html/includes/templates/default.catalog/views/box_recently_viewed_products.inc.php
@@ -6,7 +6,7 @@
 
     <?php foreach ($products as $product) { ?>
     <div class="col-xs-3">
-      <a href="<?php echo htmlspecialchars($product['link']); ?>"<?php echo !empty(document::$settings['product_modal_window']) ? ' data-toggle="lightbox" data-require-window-width="768"' : ''; ?>>
+      <a href="<?php echo htmlspecialchars($product['link']); ?>" title="<?php echo htmlspecialchars($product['name']); ?>" <?php echo !empty(document::$settings['product_modal_window']) ? ' data-toggle="lightbox" data-require-window-width="768"' : ''; ?>>
         <img class="img-thumbnail hover-light" src="<?php echo htmlspecialchars($product['thumbnail']); ?>" alt="" />
         </a>
       </div>
