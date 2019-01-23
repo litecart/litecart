c9b691616dbbdd5de3c2c17f3412e8f760aaa035
timint <info@tim-international.net>
2019-01-23 23:19:48 +0100

    * Add max-width to product modal

--- public_html/includes/templates/default.catalog/views/box_recently_viewed_products.inc.php
+++ public_html/includes/templates/default.catalog/views/box_recently_viewed_products.inc.php
@@ -1,16 +1,16 @@
-<div id="box-recently-viewed-products" class="box hidden-xs">
+<article id="box-recently-viewed-products" class="box hidden-xs">
 
   <h2 class="title"><?php echo language::translate('title_recently_viewed', 'Recently Viewed'); ?></h2>
 
-  <div class="products row half-gutter">
+  <div class="listing row">
 
     <?php foreach ($products as $product) { ?>
     <div class="col-xs-3">
-      <a href="<?php echo htmlspecialchars($product['link']); ?>" title="<?php echo htmlspecialchars($product['name']); ?>" <?php echo !empty(document::$settings['product_modal_window']) ? ' data-toggle="lightbox" data-require-window-width="768"' : ''; ?>>
+      <a href="<?php echo htmlspecialchars($product['link']); ?>" title="<?php echo htmlspecialchars($product['name']); ?>" <?php echo !empty(document::$settings['product_modal_window']) ? ' data-toggle="lightbox" data-require-window-width="768" data-max-width="980"' : ''; ?>>
         <img class="img-thumbnail hover-light" src="<?php echo htmlspecialchars($product['thumbnail']); ?>" alt="" />
         </a>
       </div>
     <?php } ?>
 
   </div>
-</div>
+</article>
