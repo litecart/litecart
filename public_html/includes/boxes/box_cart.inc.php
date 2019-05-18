4370f79662a1443b4711aaca9ef8270b7d458e9f
timint <info@tim-international.net>
2019-05-18 02:03:06 +0200

    * Cart with badge instead of text

--- public_html/includes/boxes/box_cart.inc.php
+++ public_html/includes/boxes/box_cart.inc.php
@@ -3,10 +3,15 @@
 
   $box_cart = new view();
   $box_cart->snippets = array(
-    'items' => cart::$items,
+    'items' => array(),
     'link' => document::ilink('checkout'),
     'num_items' => cart::$total['items'],
   );
+
+  foreach (cart::$items as $key => $item) {
+    $item['thumbnail'] = functions::image_thumbnail(FS_DIR_HTTP_ROOT . WS_DIR_IMAGES . $item['image'], 64, 64, 'FIT_USE_WHITESPACING');
+    $box_cart->snippets['items'][$key] = $item;
+  }
 
   if (!empty(customer::$data['display_prices_including_tax'])) {
     $box_cart->snippets['cart_total'] = currency::format(cart::$total['value'] + cart::$total['tax']);
