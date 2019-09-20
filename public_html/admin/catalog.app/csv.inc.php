7dfaf73b360edffb566237d78ee972ae90d02f90
timint <info@tim-international.net>
2019-09-20 17:56:58 +0200

    * Fix typo

--- public_html/admin/catalog.app/csv.inc.php
+++ public_html/admin/catalog.app/csv.inc.php
@@ -1,5 +1,5 @@
 <?php
-  breadcrumbs::add(language::translate('title_import_export_csv', 'Inport/Export CSV'));
+  breadcrumbs::add(language::translate('title_import_export_csv', 'Import/Export CSV'));
 
   if (isset($_POST['import_categories'])) {
 
