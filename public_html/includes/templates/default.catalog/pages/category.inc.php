4066fb34f8ee6fa013d1c4eb0cd8e500acd0ffbd
timint <info@tim-international.net>
2019-10-18 20:36:53 +0200

    * Fix thumbnail

--- public_html/includes/templates/default.catalog/pages/category.inc.php
+++ public_html/includes/templates/default.catalog/pages/category.inc.php
@@ -30,31 +30,28 @@
     <h1 class="title"><?php echo $h1_title; ?></h1>
 
     <?php if ($_GET['page'] == 1 && count($images) > 1) { ?>
-    <div class="thumbnail">
-      <section id="category-carousel" class="box carousel slide" data-ride="carousel">
-
-        <div class="carousel-inner">
-          <?php foreach ($images as $key => $image) { ?>
-          <div class="item<?php echo (($key == 0) ? ' active' : ''); ?>">
-            <img src="<?php echo document::href_link(WS_DIR_APP . $image); ?>" alt="" style="width: 100%;" /></a>
-          </div>
-          <?php } ?>
+    <section id="category-carousel" class="box carousel slide" data-ride="carousel">
+      <div class="carousel-inner thumbnail">
+        <?php foreach ($images as $key => $image) { ?>
+        <div class="item<?php echo (($key == 0) ? ' active' : ''); ?>">
+          <img src="<?php echo document::href_link(WS_DIR_APP . $image); ?>" alt="" style="width: 100%;" /></a>
         </div>
+        <?php } ?>
+      </div>
 
-        <ol class="carousel-indicators">
-          <?php foreach ($images as $key => $image) { ?>
-          <li data-target="#category-carousel" data-slide-to="<?php echo $key; ?>"<?php echo (($key == 0) ? ' class="active"' : ''); ?>></li>
-          <?php } ?>
-        </ol>
+      <ol class="carousel-indicators">
+        <?php foreach ($images as $key => $image) { ?>
+        <li data-target="#category-carousel" data-slide-to="<?php echo $key; ?>"<?php echo (($key == 0) ? ' class="active"' : ''); ?>></li>
+        <?php } ?>
+      </ol>
 
-        <a class="left carousel-control" href="#category-carousel" data-slide="prev">
-          <span class="icon-prev"><?php echo functions::draw_fonticon('fa-chevron-left'); ?></span>
-        </a>
-        <a class="right carousel-control" href="#category-carousel" data-slide="next">
-          <span class="icon-next"><?php echo functions::draw_fonticon('fa-chevron-right'); ?></span>
-        </a>
-      </section>
-    </div>
+      <a class="left carousel-control" href="#category-carousel" data-slide="prev">
+        <span class="icon-prev"><?php echo functions::draw_fonticon('fa-chevron-left'); ?></span>
+      </a>
+      <a class="right carousel-control" href="#category-carousel" data-slide="next">
+        <span class="icon-next"><?php echo functions::draw_fonticon('fa-chevron-right'); ?></span>
+      </a>
+    </section>
     <?php } ?>
 
     <?php if ($_GET['page'] == 1 && count($images) == 1) { ?>
