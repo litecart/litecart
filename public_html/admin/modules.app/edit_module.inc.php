3609cc02551fded5261f2eb49e31d9150d753e9a
timint <info@tim-international.net>
2019-10-31 14:39:33 +0100

    * Set buttons as action panel

--- public_html/admin/modules.app/edit_module.inc.php
+++ public_html/admin/modules.app/edit_module.inc.php
@@ -128,10 +128,12 @@
         </tbody>
       </table>
 
-      <div class="btn-group">
-        <?php echo functions::form_draw_button('save', language::translate('title_save', 'Save'), 'submit', '', 'save'); ?>
-        <?php echo functions::form_draw_button('cancel', language::translate('title_cancel', 'Cancel'), 'button', 'onclick="history.go(-1)"', 'cancel'); ?>
-        <?php echo functions::form_draw_button('uninstall', language::translate('title_uninstall', 'Uninstall'), 'submit', 'onclick="if (!window.confirm(\''. language::translate('text_are_you_sure', 'Are you sure?') .'\')) return false;"', 'delete'); ?>
+      <div class="panel-action">
+        <div class="btn-group">
+          <?php echo functions::form_draw_button('save', language::translate('title_save', 'Save'), 'submit', '', 'save'); ?>
+          <?php echo functions::form_draw_button('cancel', language::translate('title_cancel', 'Cancel'), 'button', 'onclick="history.go(-1)"', 'cancel'); ?>
+          <?php echo functions::form_draw_button('uninstall', language::translate('title_uninstall', 'Uninstall'), 'submit', 'onclick="if (!window.confirm(\''. language::translate('text_are_you_sure', 'Are you sure?') .'\')) return false;"', 'delete'); ?>
+        </div>
       </div>
 
     <?php echo functions::form_draw_form_end(); ?>
