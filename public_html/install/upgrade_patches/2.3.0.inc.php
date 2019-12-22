63fce3a2037ec9edb460b7164225feb74c842571
timint <info@tim-international.net>
2019-12-22 01:45:38 +0100

    - Remove compatibility with old passwords

--- public_html/install/upgrade_patches/2.3.0.inc.php
+++ public_html/install/upgrade_patches/2.3.0.inc.php
@@ -26,3 +26,17 @@
       die('<span class="error">[Error]</span><br />Could not find: '. $modification['search'] .'</p>');
     }
   }
+
+  $modified_files = array(
+    array(
+      'file'    => FS_DIR_APP . 'includes/config.inc.php',
+      'pattern'  => '#'. preg_quote(PHP_EOL, '#') .'// Password Encryption Salt.*?\);'. preg_quote(PHP_EOL, '#') .'#m',
+      'replace' => '',
+    ),
+  );
+
+  foreach ($modified_files as $modification) {
+    if (!file_modify_regex($modification['file'], $modification['search'], $modification['replace'])) {
+      echo '<span class="error">[Skip]</span><br />Could not find: '. $modification['search'] .'</p>';
+    }
+  }
