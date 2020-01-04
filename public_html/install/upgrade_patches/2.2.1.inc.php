f1e61ae84d1e8ae350833788bf2f2d010f08cecc
timint <info@tim-international.net>
2020-01-04 15:04:47 +0100

    * Append commit Multiple Category Images

--- public_html/install/upgrade_patches/2.2.1.inc.php
+++ public_html/install/upgrade_patches/2.2.1.inc.php
@@ -14,15 +14,16 @@
     having count(*) >= 2;"
   );
 
-  if (!database::num_rows($categories_images_query)) {
+  if (database::num_rows($categories_images_query)) {
+
     $modified_files[] = array(
       'file'    => FS_DIR_APP . 'includes/config.inc.php',
       'search'  => "// Database tables (Add-ons)" . PHP_EOL,
       'replace' => "// Database tables (Add-ons)" . PHP_EOL
                  . "  define('DB_TABLE_CATEGORIES_IMAGES',                 '`'. DB_DATABASE .'`.`'. DB_TABLE_PREFIX . 'categories_images`');" . PHP_EOL,
     );
-  } else {
-    unlink(FS_DIR_APP . 'vqmod/xml/multiple_category_images.xml');
+
+    copy(FS_DIR_APP . 'install/other/multiple_category_images.xml', FS_DIR_APP . 'vqmod/xml/multiple_category_images.xml');
   }
 
 // Modify some files
