b731e81c3a21f30e49ed8a8ac3819daa3a54c5b0
timint <info@tim-international.net>
2020-01-24 03:57:02 +0100

    * Add ability to disable order copy email

--- public_html/install/upgrade_patches/2.2.2.sql
+++ public_html/install/upgrade_patches/2.2.2.sql
@@ -1,3 +1,6 @@
 ALTER TABLE `lc_order_statuses`
 ADD INDEX(`is_archived`),
 ADD INDEX(`is_sale`);
+-- --------------------------------------------------------
+INSERT INTO `lc_settings` (`setting_group_key`, `type`, `title`, `description`, `key`, `value`, `function`, `priority`, `date_updated`, `date_created`) VALUES
+('checkout', 'local', 'Send Order Confirmation', 'The name of your store.', 'send_order_confirmation', '1', 'text()', 11, NOW(), NOW());
