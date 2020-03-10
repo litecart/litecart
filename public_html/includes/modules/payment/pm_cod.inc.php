df08d777c8ac9d8ec5acadbec14f29d677e610ff
timint <info@tim-international.net>
2020-03-10 19:39:27 +0100

    * Fix issue #187 - Geo zone limitation

--- public_html/includes/modules/payment/pm_cod.inc.php
+++ public_html/includes/modules/payment/pm_cod.inc.php
@@ -104,11 +104,11 @@
           'function' => 'order_status()',
         ),
         array(
-          'key' => 'geo_zone_id',
+          'key' => 'geo_zones',
           'default_value' => '',
           'title' => language::translate('title_geo_zone_limitation', 'Geo Zone Limitation'),
           'description' => language::translate('modules:description_geo_zone', 'Limit this module to the selected geo zone. Otherwise leave blank.'),
-          'function' => 'geo_zone()',
+          'function' => 'geo_zones()',
         ),
         array(
           'key' => 'priority',
