13a25de7bf5f68a298ece70a0ec837811b80b7b4
arshidkv12 <arshidkv12@gmail.com>
2020-04-01 13:22:44 +0530

    responsive ui

--- public_html/includes/templates/default.catalog/views/box_checkout_customer.inc.php
+++ public_html/includes/templates/default.catalog/views/box_checkout_customer.inc.php
@@ -14,14 +14,14 @@
     <?php if (settings::get('customer_field_company') || settings::get('customer_field_tax_id')) { ?>
     <div class="row">
       <?php if (settings::get('customer_field_company')) { ?>
-      <div class="form-group col-xs-6">
+      <div class="form-group col-xs-12 col-sm-6">
         <label><?php echo language::translate('title_company', 'Company'); ?> (<?php echo language::translate('text_or_leave_blank', 'Or leave blank'); ?>)</label>
         <?php echo functions::form_draw_text_field('company', true); ?>
       </div>
       <?php } ?>
 
       <?php if (settings::get('customer_field_tax_id')) { ?>
-      <div class="form-group col-xs-6">
+      <div class="form-group col-xs-12 col-sm-6">
         <label><?php echo language::translate('title_tax_id', 'Tax ID'); ?></label>
         <?php echo functions::form_draw_text_field('tax_id', true); ?>
       </div>
@@ -30,60 +30,60 @@
     <?php } ?>
 
     <div class="row">
-      <div class="form-group col-xs-6">
+      <div class="form-group col-xs-12 col-sm-6">
         <label><?php echo language::translate('title_firstname', 'First Name'); ?></label>
         <?php echo functions::form_draw_text_field('firstname', true, 'required="required"'); ?>
       </div>
 
-      <div class="form-group col-xs-6">
+      <div class="form-group col-xs-12 col-sm-6">
         <label><?php echo language::translate('title_lastname', 'Last Name'); ?></label>
         <?php echo functions::form_draw_text_field('lastname', true, 'required="required"'); ?>
       </div>
     </div>
 
     <div class="row">
-      <div class="form-group col-xs-6">
+      <div class="form-group col-xs-12 col-sm-6">
         <label><?php echo language::translate('title_address1', 'Address 1'); ?></label>
         <?php echo functions::form_draw_text_field('address1', true, 'required="required"'); ?>
       </div>
 
-      <div class="form-group col-xs-6">
+      <div class="form-group col-xs-12 col-sm-6">
         <label><?php echo language::translate('title_address2', 'Address 2'); ?></label>
         <?php echo functions::form_draw_text_field('address2', true); ?>
       </div>
     </div>
 
     <div class="row">
-      <div class="form-group col-xs-6">
+      <div class="form-group col-xs-12 col-sm-6">
         <label><?php echo language::translate('title_postcode', 'Postal Code'); ?></label>
         <?php echo functions::form_draw_text_field('postcode', true); ?>
       </div>
 
-      <div class="form-group col-xs-6">
+      <div class="form-group col-xs-12 col-sm-6">
         <label><?php echo language::translate('title_city', 'City'); ?></label>
         <?php echo functions::form_draw_text_field('city', true); ?>
       </div>
     </div>
 
     <div class="row">
-      <div class="form-group col-xs-6">
+      <div class="form-group col-xs-12 col-sm-6">
         <label><?php echo language::translate('title_country', 'Country'); ?></label>
         <?php echo functions::form_draw_countries_list('country_code', true); ?>
       </div>
 
-      <div class="form-group col-xs-6">
+      <div class="form-group col-xs-12 col-sm-6">
         <label><?php echo language::translate('title_zone_state_province', 'Zone/State/Province'); ?></label>
         <?php echo functions::form_draw_zones_list(isset($_POST['country_code']) ? $_POST['country_code'] : '', 'zone_code', true); ?>
       </div>
     </div>
 
     <div class="row">
-      <div class="form-group col-xs-6">
+      <div class="form-group col-xs-12 col-sm-6">
         <label><?php echo language::translate('title_email_address', 'Email Address'); ?></label>
         <?php echo functions::form_draw_email_field('email', true, 'required="required"'. (!empty(customer::$data['id']) ? ' readonly="readonly"' : '')); ?>
       </div>
 
-      <div class="form-group col-xs-6">
+      <div class="form-group col-xs-12 col-sm-6">
         <label><?php echo language::translate('title_phone', 'Phone'); ?></label>
         <?php echo functions::form_draw_phone_field('phone', true, 'required="required"'); ?>
       </div>
@@ -98,7 +98,7 @@
 
       <?php if (settings::get('customer_field_company')) { ?>
       <div class="row">
-        <div class="form-group col-xs-6">
+        <div class="form-group col-xs-12 col-sm-6">
         <label><?php echo language::translate('title_company', 'Company'); ?> (<?php echo language::translate('text_or_leave_blank', 'Or leave blank'); ?>)</label>
           <?php echo functions::form_draw_text_field('shipping_address[company]', true); ?>
         </div>
