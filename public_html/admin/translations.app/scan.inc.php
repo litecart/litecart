9a54ed5164fbeb3f714eaa2d05b6db44c42aad16
timint <info@tim-international.net>
2020-04-08 01:35:24 +0200

    - Remove 1.x compatibility

--- public_html/admin/translations.app/scan.inc.php
+++ public_html/admin/translations.app/scan.inc.php
@@ -23,7 +23,7 @@
       $contents = file_get_contents($file);
 
       $regexp = array(
-        '(?:language->|language::)translate\((?:(?!\$)',
+        'language::translate\((?:(?!\$)',
         '(?:(__CLASS__)?\.)?',
         '(?:[\'"])([^\'"]+)(?:[\'"])',
         '(?:,?\s+(?:[\'"])([^\'"]+)?(?:[\'"]))?',
