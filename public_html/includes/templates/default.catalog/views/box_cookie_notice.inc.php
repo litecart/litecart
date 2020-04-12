9ac2c7b390a2de3a44d45b923f999f3a771b34ac
timint <info@tim-international.net>
2020-04-12 02:49:54 +0200

    * Rename container id

--- public_html/includes/templates/default.catalog/views/box_cookie_notice.inc.php
+++ public_html/includes/templates/default.catalog/views/box_cookie_notice.inc.php
@@ -4,7 +4,7 @@
 
   if (!settings::get('cookie_policy')) return;
 ?>
-<div id="cookies-acceptance" class="text-center">
+<div id="box-cookie-notice" class="text-center">
   <?php echo strtr(language::translate('text_cookie_notice', 'We rely on <a href="%url">cookies</a> to understand how our customers use our website.'), array('%url' => document::href_ilink('information', array('page_id' => settings::get('cookie_policy'))))); ?>
   <div class="btn-group">
     <?php echo functions::form_draw_button('accept_cookies', array('1', language::translate('text_i_accept', 'I accept')), 'button', 'style="font-weight: bold;"'); ?>
@@ -14,14 +14,14 @@
 
 <script>
   $('button[name="accept_cookies"]').click(function(){
-    $('#cookies-acceptance').fadeOut();
+    $('#box-cookie-notice').fadeOut();
     var now = new Date();
     var expires = new Date(now.getTime() + (365 * 24 * 60 * 60 * 1000));
     document.cookie = 'cookies_accepted=1;expires=' + expires.toUTCString();
   });
 
   $('button[name="decline_cookies"]').click(function(){
-    $('#cookies-acceptance').fadeOut();
+    $('#box-cookie-notice').fadeOut();
     var now = new Date();
     var expires = new Date(now.getTime() + (365 * 24 * 60 * 60 * 1000));
     document.cookie = 'cookies_accepted=0;expires=0';
