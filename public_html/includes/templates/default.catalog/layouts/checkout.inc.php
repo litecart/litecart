0ce1882b17fa665d8dd1ba1d5f138cad8a4acb31
timint <info@tim-international.net>
2020-05-02 03:36:38 +0200

    * Display customer service email if phone is not present

--- public_html/includes/templates/default.catalog/layouts/checkout.inc.php
+++ public_html/includes/templates/default.catalog/layouts/checkout.inc.php
@@ -22,7 +22,11 @@
 
   <div class="customer-service hidden-xs">
     <div class="title"><?php echo language::translate('title_customer_service', 'Customer Service'); ?></div>
-    <div class="phone"><?php echo settings::get('store_phone'); ?></div>
+    <?php if (settings::get('store_phone')) { ?>
+    <div class="phone"><?php echo functions::draw_fonticon('fa-phone'); ?> <?php echo settings::get('store_phone'); ?></div>
+    <?php } else { ?>
+    <div class="email"><?php echo functions::draw_fonticon('fa-envelope'); ?> <?php echo settings::get('store_email'); ?></div>
+    <?php } ?>
   </div>
 </header>
 
