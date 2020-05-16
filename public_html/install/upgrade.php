818fb563d6d6fd8b19646ec2563222a8fb0624ac
timint <info@tim-international.net>
2020-05-16 02:44:50 +0200

    * Installer and Upgrade rewritten for PHP CLI support

--- public_html/install/upgrade.php
+++ public_html/install/upgrade.php
@@ -1,16 +1,21 @@
 <?php
   // Automatic upgrade: upgrade.php?upgrade=true&redirect={url}
 
-  ob_start();
+  if (php_sapi_name() == 'cli') {
+    $options = array(
+      'from_version:',
+    );
+    $_REQUEST = getopt(null, $options);
+    $_REQUEST['upgrade'] = true;
+  }
 
-  require_once('../includes/config.inc.php');
+  require_once(__DIR__ . '/../includes/config.inc.php');
   if (!defined('FS_DIR_APP')) define('FS_DIR_APP', FS_DIR_HTTP_ROOT . WS_DIR_HTTP_HOME);
   if (!defined('FS_DIR_ADMIN')) define('FS_DIR_ADMIN', FS_DIR_HTTP_ROOT . WS_DIR_ADMIN);
 
-  require_once('../includes/error_handler.inc.php');
-  require_once('../includes/library/lib_database.inc.php');
-  require_once('includes/header.inc.php');
-  require_once('includes/functions.inc.php');
+  require_once(FS_DIR_APP . 'includes/error_handler.inc.php');
+  require_once(FS_DIR_APP . '/includes/library/lib_database.inc.php');
+  require_once(__DIR__ . '/includes/functions.inc.php');
 
 // Turn on errors
   error_reporting(version_compare(PHP_VERSION, '5.4.0', '<') ? E_ALL | E_STRICT : E_ALL);
@@ -20,11 +25,11 @@
   ini_set('html_errors', 'On');
 
 // Set platform name
-  preg_match('#define\(\'PLATFORM_NAME\', \'([^\']+)\'\);#', file_get_contents('../includes/app_header.inc.php'), $matches);
+  preg_match('#define\(\'PLATFORM_NAME\', \'([^\']+)\'\);#', file_get_contents(FS_DIR_APP . 'includes/app_header.inc.php'), $matches);
   define('PLATFORM_NAME', isset($matches[1]) ? $matches[1] : false);
 
 // Set platform version
-  preg_match('#define\(\'PLATFORM_VERSION\', \'([^\']+)\'\);#', file_get_contents('../includes/app_header.inc.php'), $matches);
+  preg_match('#define\(\'PLATFORM_VERSION\', \'([^\']+)\'\);#', file_get_contents(FS_DIR_APP . 'includes/app_header.inc.php'), $matches);
   define('PLATFORM_VERSION', isset($matches[1]) ? $matches[1] : false);
 
   if (!PLATFORM_VERSION) die('Could not identify target version.');
@@ -38,15 +43,11 @@
 
   if ($platform_database_version = database::fetch($platform_database_version_query)) {
     define('PLATFORM_DATABASE_VERSION', $platform_database_version['value']);
-  } else if (!empty($_GET['from_version'])) {
-    define('PLATFORM_DATABASE_VERSION', $_GET['from_version']);
-  } else {
-    die('Could not identify database version');
   }
 
 // List supported upgrades
   $supported_versions = array('1.0' => '1.0');
-  foreach (glob('upgrade_patches/*') as $file) {
+  foreach (glob(__DIR__ . '/upgrade_patches/*') as $file) {
     if (preg_match('#\/(.*).(inc.php|sql)$#', $file, $matches)) {
       $supported_versions[$matches[1]] = $matches[1];
     }
@@ -55,31 +56,51 @@
     return version_compare($a, $b, '>');
   });
 
-
   if (!empty($_REQUEST['upgrade'])) {
 
-    echo '<h1>Upgrade</h1>' . PHP_EOL;
+    ob_start();
 
-    ### PHP > Check Version #######################################
-
-    echo '<p>Checking PHP version... ';
-
-    if (version_compare(PHP_VERSION, '5.4', '<')) {
-      die('<span class="error">[Error] PHP 5.4+ minimum requirement - Detected '. PHP_VERSION .'</span></p>');
-    } else if (version_compare(PHP_VERSION, '7.1', '<=')) {
-      echo PHP_VERSION .' <span class="ok">[OK]</span><br />'
-         . '<span class="warning">[Warning] PHP 7.3+ recommended - Detected '. PHP_VERSION .' that has reached <a href="https://www.php.net/supported-versions.php" target="_blank">end of life</a></span></span></p>';
-    } else {
-      echo PHP_VERSION .' <span class="ok">[OK]</span></p>' . PHP_EOL;
+    if (php_sapi_name() != 'cli') {
+      require_once(__DIR__ . '/includes/header.inc.php');
     }
 
-    #############################################
+    try {
 
-    foreach ($supported_versions as $version) {
-      if (version_compare(PLATFORM_DATABASE_VERSION, $version, '<')) {
-        if (file_exists('upgrade_patches/'. $version .'.sql')) {
-          echo '<p>Upgrading database to '. $version .'... ' . PHP_EOL;
-          $sql = file_get_contents('upgrade_patches/'. $version .'.sql');
+      echo '<h1>Upgrade</h1>' . PHP_EOL . PHP_EOL;
+
+      ### PHP > Check Version #######################################
+
+      echo '<p>Checking PHP version... ';
+
+      if (version_compare(PHP_VERSION, '5.4', '<')) {
+        throw new Exception(PHP_VERSION .' <span class="error">[Error] PHP 5.4+ minimum requirement</span></p>' . PHP_EOL . PHP_EOL);
+      } else if (version_compare(PHP_VERSION, '7.1', '<=')) {
+        echo PHP_VERSION .' <span class="warning">[Warning] PHP '. PHP_VERSION .' has reached <a href="https://www.php.net/supported-versions.php" target="_blank">end of life</a>.</span></p>' . PHP_EOL . PHP_EOL;
+      } else {
+        echo PHP_VERSION .' <span class="ok">[OK]</span></p>' . PHP_EOL . PHP_EOL;
+      }
+      ### App > Check Version #######################################
+
+      echo '<p>Checking application database version... ';
+
+      if (defined('PLATFORM_DATABASE_VERSION')) {
+        throw new Exception(PLATFORM_DATABASE_VERSION .' <span class="ok">[OK]</span></p>' . PHP_EOL . PHP_EOL);
+      } else if (!empty($_REQUEST['from_version'])) {
+        define('PLATFORM_DATABASE_VERSION', $_REQUEST['from_version']);
+        echo $_REQUEST['from_version'] . '(User Defined) <span class="warning">[OK]</span></p>' . PHP_EOL . PHP_EOL;
+      } else {
+        echo ' <span class="error">[Undetected]</span></p>' . PHP_EOL . PHP_EOL;
+      }
+
+      #############################################
+
+      foreach ($supported_versions as $version) {
+
+        if (version_compare(PLATFORM_DATABASE_VERSION, $version, '>=')) continue;
+
+        if (file_exists(__DIR__ . '/upgrade_patches/'. $version .'.sql')) {
+          echo '<p>Upgrading database to '. $version .'...</p>' . PHP_EOL . PHP_EOL;
+          $sql = file_get_contents(__DIR__ . '/upgrade_patches/'. $version .'.sql');
           $sql = str_replace('`lc_', '`'.DB_TABLE_PREFIX, $sql);
 
           $sql = explode('-- --------------------------------------------------------', $sql);
@@ -90,11 +111,14 @@
               database::query($query);
             }
           }
+
+          unlink(__DIR__ . '/upgrade_patches/'. $version .'.sql');
         }
 
-        if (file_exists('upgrade_patches/'. $version .'.inc.php')) {
-          echo '<p>Upgrading system to '. $version .'...</p>' . PHP_EOL;
-          include('upgrade_patches/'. $version .'.inc.php');
+        if (file_exists(__DIR__ . '/upgrade_patches/'. $version .'.inc.php')) {
+          echo '<p>Upgrading system to '. $version .'...</p>' . PHP_EOL . PHP_EOL;
+          include(__DIR__ . '/upgrade_patches/'. $version .'.inc.php');
+          unlink(__DIR__ . '/upgrade_patches/'. $version .'.inc.php');
         }
 
         echo '<p>Set platform database version...';
@@ -106,88 +130,101 @@
           limit 1;"
         );
 
-        echo ' <strong>'. $version .'</strong></p>' . PHP_EOL;
-      }
-    }
-
-    #############################################
-
-    echo '<p>Preparing CSS files...</p>' . PHP_EOL;
-
-    if (!empty($_REQUEST['development_type']) && $_REQUEST['development_type'] == 'advanced') {
-
-      $files_to_delete = array(
-        '../includes/templates/default.catalog/css/app.css',
-        '../includes/templates/default.catalog/css/checkout.css',
-        '../includes/templates/default.catalog/css/framework.css',
-        '../includes/templates/default.catalog/css/printable.css',
-      );
-
-      foreach ($files_to_delete as $file) {
-        file_delete($file);
+        echo ' <strong>'. $version .'</strong></p>' . PHP_EOL . PHP_EOL;
       }
 
-    } else {
+      #############################################
 
-      $files_to_delete = array(
-        '../includes/templates/default.catalog/css/*.min.css',
-        '../includes/templates/default.catalog/css/*.min.css.map',
-        '../includes/templates/default.catalog/js/*.min.js',
-        '../includes/templates/default.catalog/js/*.min.js.map',
-        '../includes/templates/default.catalog/less/',
-      );
+      echo '<p>Preparing CSS files...</p>' . PHP_EOL . PHP_EOL;
 
-      foreach ($files_to_delete as $file) {
-        file_delete($file);
-      }
+      if (!empty($_REQUEST['development_type']) && $_REQUEST['development_type'] == 'advanced') {
 
-      foreach (glob('../includes/templates/default.catalog/layouts/*.inc.php') as $file) {
-        echo 'Modify '. $file . PHP_EOL;
-        $contents = file_get_contents($file);
-        $search_replace = array(
-          'app.min.css'  => 'app.css',
-          'checkout.min.css'  => 'checkout.css',
-          'framework.min.css' => 'framework.css',
-          'printable.min.css' => 'printable.css',
-          'app.min.js' => 'app.js',
+        $files_to_delete = array(
+          FS_DIR_APP . 'includes/templates/default.catalog/css/app.css',
+          FS_DIR_APP . 'includes/templates/default.catalog/css/checkout.css',
+          FS_DIR_APP . 'includes/templates/default.catalog/css/framework.css',
+          FS_DIR_APP . 'includes/templates/default.catalog/css/printable.css',
         );
-        file_put_contents($file, strtr($contents, $search_replace));
+
+        foreach ($files_to_delete as $file) {
+          file_delete($file);
+        }
+
+      } else {
+
+        $files_to_delete = array(
+          FS_DIR_APP . 'includes/templates/default.catalog/css/*.min.css',
+          FS_DIR_APP . 'includes/templates/default.catalog/css/*.min.css.map',
+          FS_DIR_APP . 'includes/templates/default.catalog/js/*.min.js',
+          FS_DIR_APP . 'includes/templates/default.catalog/js/*.min.js.map',
+          FS_DIR_APP . 'includes/templates/default.catalog/less/',
+        );
+
+        foreach ($files_to_delete as $file) {
+          file_delete($file);
+        }
+
+        foreach (glob(FS_DIR_APP . 'includes/templates/default.catalog/layouts/*.inc.php') as $file) {
+          $contents = file_get_contents($file);
+          $search_replace = array(
+            'app.min.css'  => 'app.css',
+            'checkout.min.css'  => 'checkout.css',
+            'framework.min.css' => 'framework.css',
+            'printable.min.css' => 'printable.css',
+            'app.min.js' => 'app.js',
+          );
+          file_put_contents($file, strtr($contents, $search_replace));
+        }
       }
+
+      echo PHP_EOL;
+
+      #############################################
+
+      echo '<p>Clear cache... ';
+
+      database::query(
+        "update ". DB_TABLE_SETTINGS ."
+        set value = '1'
+        where `key` = 'cache_clear'
+        limit 1;"
+      );
+
+      foreach (glob(FS_DIR_APP . 'vqmod/vqcache/*.php') as $file) {
+        if (is_file($file)) unlink($file);
+      }
+
+      if (is_file($file = FS_DIR_APP . 'vqmod/chekced.cache')) unlink($file);
+      if (is_file($file = FS_DIR_APP . 'vqmod/mods.cache')) unlink($file);
+
+      echo '<span class="ok">[OK]</span></p>' . PHP_EOL . PHP_EOL;
+
+      #############################################
+
+      if (!empty($_REQUEST['redirect'])) {
+        header('Location: '. $_REQUEST['redirect']);
+        exit;
+      }
+
+      echo '<h2>Complete</h2>' . PHP_EOL . PHP_EOL
+         . '<p style="font-weight: bold;">Upgrade complete! Please delete the <strong>~/install/</strong> folder.</p>' . PHP_EOL . PHP_EOL;
+
+    } catch (Exception $e) {
+      echo $e->getMessage();
     }
 
-    #############################################
+    $buffer = ob_get_clean();
 
-    echo '<p>Clear cache... ';
-
-    database::query(
-      "update ". DB_TABLE_SETTINGS ."
-      set value = '1'
-      where `key` = 'cache_clear'
-      limit 1;"
-    );
-
-    foreach (glob(FS_DIR_APP . 'vqmod/vqcache/*.php') as $file) {
-      if (is_file($file)) unlink($file);
+    if (php_sapi_name() == 'cli') {
+      echo strip_tags($buffer);
+    } else {
+      echo $buffer;
+      require('includes/footer.inc.php');
     }
 
-    if (is_file($file = FS_DIR_APP . 'vqmod/chekced.cache')) unlink($file);
-    if (is_file($file = FS_DIR_APP . 'vqmod/mods.cache')) unlink($file);
-
-    echo '<span class="ok">[OK]</span></p>' . PHP_EOL;
-
-    #############################################
-
-    if (!empty($_REQUEST['redirect'])) {
-      header('Location: '. $_REQUEST['redirect']);
-      exit;
-    }
-
-    echo PHP_EOL . '<h2>Complete</h2>' . PHP_EOL
-       . '<p style="font-weight: bold;">Upgrade complete! Please delete the <strong>~/install/</strong> folder.</p>' . PHP_EOL;
-
-    require('includes/footer.inc.php');
     exit;
   }
+
 
 ?>
 <style>
@@ -235,11 +272,22 @@ input[name="development_type"]:checked + div {
 
   <div class="form-group">
     <label>Version</label>
+
+    <?php if (defined('PLATFORM_DATABASE_VERSION')) { ?>
     <ul class="list-inline" style="font-size: 2em;">
       <li><?php echo PLATFORM_DATABASE_VERSION; ?></li>
       <li>→</li>
       <li><?php echo PLATFORM_VERSION; ?></li>
     </ul>
+    <?php } else { ?>
+    <div class="form-group">
+      <label>Select the <?php echo PLATFORM_NAME; ?> version you are upgrading from:</label>
+      <select class="form-control" name="from_version">
+        <option value="">-- Select Version --</option>
+        <?php foreach ($supported_versions as $version) echo '<option value="'. $version .'"'. ((isset($_REQUEST['from_version']) && $_REQUEST['from_version'] == $version) ? 'selected="selected"' : '') .'>'. PLATFORM_NAME .' '. $version .'</option>' . PHP_EOL; ?>
+      </select>
+    </div>
+    <?php } ?>
   </div>
 
   <h3>Development</h3>
