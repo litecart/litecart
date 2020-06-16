c570293578cda47f7908d552e353440faa119e98
timint <info@tim-international.net>
2020-06-16 23:17:05 +0200

    * Update upgrade patch

--- public_html/install/upgrade_patches/2.3.0.inc.php
+++ public_html/install/upgrade_patches/2.3.0.inc.php
@@ -57,36 +57,6 @@
       'search'  => "// Database tables",
       'replace' => "// Database Tables - Backward Compatibility (LiteCart <2.3)",
     ],
-  ];
-
-// See if multiple category images is used
-  $categories_images_query = database::query(
-    "select count(id) from `". DB_PREFIX ."categories_images`
-    group by category_id
-    having count(*) >= 2"
-  );
-
-  if (database::num_rows($categories_images_query)) {
-    $modified_files[] = [
-      'file'    => FS_DIR_APP . 'includes/config.inc.php',
-      'search'  => "// Database tables (Add-ons)" . PHP_EOL,
-      'replace' => "// Database tables (Add-ons)" . PHP_EOL
-                 . "  define('DB_TABLE_CATEGORIES_INFO',                   '`'. DB_DATABASE .'`.`'. DB_PREFIX . 'categories_info`');" . PHP_EOL,
-    ];
-    copy(FS_DIR_APP . 'install/data/other/multiple_category_images.xml', FS_DIR_APP . 'vmods/multiple_category_images.xml');
-  } else {
-    database::query(
-      "drop table `". DB_PREFIX ."categories_images`;"
-    );
-  }
-
-  foreach ($modified_files as $modification) {
-    if (!file_modify($modification['file'], $modification['search'], $modification['replace'])) {
-      die('<span class="error">[Error]</span><br />Could not find: '. $modification['search'] .'</p>');
-    }
-  }
-
-  $modified_files = [
     [
       'file'    => FS_DIR_APP . 'includes/config.inc.php',
       'pattern'  => '#'. preg_quote(PHP_EOL, '#') .'// Password Encryption Salt.*?\);'. preg_quote(PHP_EOL, '#') .'#m',
@@ -95,7 +65,11 @@
   ];
 
   foreach ($modified_files as $modification) {
-    if (!file_modify_regex($modification['file'], $modification['search'], $modification['replace'])) {
-      echo '<span class="error">[Skip]</span><br />Could not find: '. $modification['search'] .'</p>';
+    if (!file_modify($modification['file'], $modification['search'], $modification['replace'])) {
+      die('<span class="error">[Error]</span><br />Could not find: '. $modification['search'] .'</p>');
+    }
+  }
+
+
     }
   }
