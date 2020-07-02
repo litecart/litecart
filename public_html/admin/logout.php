da0e5a9d3a1d53aa9ff595fc5852c035aa3d5a2b
timint <info@tim-international.net>
2020-07-03 01:10:58 +0200

    * Fix remove cookie

--- public_html/admin/logout.php
+++ public_html/admin/logout.php
@@ -6,7 +6,7 @@
 
   user::reset();
 
-  header('Set-Cookie: remember_me=; path='. WS_DIR_APP .'; expires=-1; HttpOnly; SameSite=Strict');
+  header('Set-Cookie: remember_me=; path='. WS_DIR_APP .'; max-age=-1; HttpOnly; SameSite=Strict');
 
   header('Location: ' . document::link(WS_DIR_ADMIN . 'login.php'));
   exit;
