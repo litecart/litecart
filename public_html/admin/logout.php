a1eda08179eb0e734d31e8efc0166fd78f5bcdac
timint <info@tim-international.net>
2020-07-06 21:20:56 +0200

    * Default all cookies to Lax as Strict can cause trouble for customers returning from payment gateways

--- public_html/admin/logout.php
+++ public_html/admin/logout.php
@@ -9,7 +9,7 @@
   session::regenerate_id();
 
   if (!empty($_COOKIE['remember_me'])) {
-    header('Set-Cookie: remember_me=; Path='. WS_DIR_APP .'; Max-Age=-1; HttpOnly; SameSite=Strict', false);
+    header('Set-Cookie: remember_me=; Path='. WS_DIR_APP .'; Max-Age=-1; HttpOnly; SameSite=Lax', false);
   }
 
   header('Location: ' . document::link(WS_DIR_ADMIN . 'login.php'));
--- public_html/pages/logout.inc.php
+++ public_html/pages/logout.inc.php
@@ -8,10 +8,10 @@
   session::regenerate_id();
   session::$data['cart']['uid'] = null;
 
-  header('Set-Cookie: cart[uid]=; Path='. WS_DIR_APP .'; Max-Age=-1; HttpOnly; SameSite=Strict', false);
+  header('Set-Cookie: cart[uid]=; Path='. WS_DIR_APP .'; Max-Age=-1; SameSite=Lax', false);
 
   if (!empty($_COOKIE['customer_remember_me'])) {
-    header('Set-Cookie: customer_remember_me=; Path='. WS_DIR_APP .'; Max-Age=-1; HttpOnly; SameSite=Strict', false);
+    header('Set-Cookie: customer_remember_me=; Path='. WS_DIR_APP .'; Max-Age=-1; HttpOnly; SameSite=Lax', false);
   }
 
   notices::add('success', language::translate('description_logged_out', 'You are now logged out.'));
