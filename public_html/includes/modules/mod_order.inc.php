ee879035ee1bd9b8ebe4c2eb924a525e0aeb3541
timint <info@tim-international.net>
2020-08-28 19:35:35 +0200

    * Fix run() for specific given module

--- public_html/includes/modules/mod_order.inc.php
+++ public_html/includes/modules/mod_order.inc.php
@@ -96,11 +96,17 @@
       }
     }
 
-    public function run($method_name, $module_id) {
+    public function run($method_name, $module_id=null) {
 
       if (empty($this->modules)) return;
 
-      foreach ($this->modules as $module_id => $module) {
+      if (!empty($module_id)) {
+        $module_ids = array($module_id);
+      } else {
+        $module_ids = array_keys($this->modules);
+      }
+
+      foreach ($module_ids as $module_id) {
         if (method_exists($this->modules[$module_id], $method_name)) {
           call_user_func_array(array($this->modules[$module_id], $method_name), array_slice(func_get_args(), 2));
         }
