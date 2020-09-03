0f1dacb9dc8aa5b7a0970cbdfd69ee666455eaa8
Arshid <arshidkv12@gmail.com>
2020-09-03 21:06:56 +0530

    Update wrap_smtp.inc.php

--- public_html/includes/wrappers/wrap_smtp.inc.php
+++ public_html/includes/wrappers/wrap_smtp.inc.php
@@ -1,158 +1,144 @@
 <?php
 
-  class wrap_smtp {
-    private $_socket = null;
-    private $_host = null;
-    private $_port = null;
-    private $_log_handle = '';
+  document::$snippets['title'][] = language::translate('title_storage_encoding', 'Storage Encoding');
 
-    function __construct($host, $port, $username='', $password='') {
+  breadcrumbs::add(language::translate('title_storage_encoding', 'Storage Encoding'));
 
-      if ($port == 465) {
-        $this->_host = "ssl://$host:$port";
-      } else {
-        $this->_host = "tcp://$host:$port";
-      }
-
-      $this->_username = $username;
-      $this->_password = $password;
-
-      $this->_log_handle = fopen(FS_DIR_APP . 'logs/last_smtp.log', 'w');
-    }
-
-    function __destruct() {
-      if (is_resource($this->_socket)) $this->disconnect();
-    }
-
-    public function send($sender, $recipients, $data='') {
-
-      if (!is_array($recipients)) $recipients = array($recipients);
-
-      if (!is_resource($this->_socket)) $this->connect();
-
-      $this->write("MAIL FROM: <$sender>\r\n", 250);
-
-      foreach ($recipients as $recipient) {
-        $this->write("RCPT TO: <$recipient>\r\n", 250);
-      }
-
-      $this->write("DATA\r\n", 354)
-           ->write("$data\r\n")
-           ->write(".\r\n", 250);
-
-      return true;
-    }
-
-    public function connect() {
-
-      $stream_context = $context = stream_context_create(array(
-        'ssl' => array(
-          // set some SSL/TLS specific options
-          'verify_peer' => false,
-          'verify_peer_name' => false,
-          'allow_self_signed' => true
-        ),
-      ));
-
-      fwrite($this->_log_handle, "Connecting to $this->_host ...\r\n");
-      $this->_socket = stream_socket_client($this->_host, $errno, $errstr, 3, STREAM_CLIENT_CONNECT, $stream_context);
-
-      if ($errno) throw new Exception('Could not connect to socket '. $this->_host .': '. $errstr);
-      if (empty($this->_socket)) throw new Exception('Failed opening socket connection to '. $this->_host);
-
-      stream_set_blocking($this->_socket, true);
-      stream_set_timeout($this->_socket, 6);
-
-      $this->read(220)
-           ->write("EHLO {$_SERVER['SERVER_NAME']}\r\n", 250);
-
-      if (preg_match('#250-STARTTLS#', $this->_last_response)) {
-        $this->write("STARTTLS\r\n", 220);
-        if (!stream_socket_enable_crypto($this->_socket, true, STREAM_CRYPTO_METHOD_SSLv23_CLIENT)) {
-        //if (!stream_socket_enable_crypto($this->_socket, true, STREAM_CRYPTO_METHOD_TLS_CLIENT)) {
-          throw new Exception('Could not start TLS encryption');
-        }
-        $this->write("EHLO {$_SERVER['SERVER_NAME']}\r\n", 250);
-      }
-
-      if (!empty($this->_username)) {
-
-        $auths = array();
-        if (preg_match('#250.AUTH (.*?)\R#', $this->_last_response, $matches)) {
-          $auths = explode(' ', $matches[1]);
-        }
-
-        switch(true) {
-
-          case (in_array('DIGEST-MD5', $auths)):
-            $this->write("AUTH DIGEST-MD5\r\n", 334)
-            $realm = base64_decode(substr($this->_last_response, 4));
-            $this->write(base64_encode($this->_username .' '. md5($this->_username .':'. $realm .':'. $this->_password)) . "\r\n", 235);
-            break;
-
-          case (in_array('CRAM-MD5', $auths)):
-            $this->write("AUTH CRAM-MD5\r\n", 334):
-            $challenge = base64_decode(substr($this->_last_response, 4));
-            $this->write(base64_encode($this->_username .' '. hash_hmac('md5', $challenge, $this->_password)) . "\r\n", 235);
-            break;
-
-          case (in_array('LOGIN', $auths)):
-            $this->write("AUTH LOGIN\r\n", 334)
-                 ->write(base64_encode($this->_username) . "\r\n", 334)
-                 ->write(base64_encode($this->_password) . "\r\n", 235);
-            break;
-
-          case (in_array('PLAIN', $auths)):
-            $this->write("AUTH PLAIN\r\n", 334)
-                 ->write(base64_encode("\0" . $this->_username . "\0" . $this->_password) . "\r\n", 235);
-            break;
-
-          default:
-            throw new Exception('No supported authentication methods ('. implode(', ', $auths).')');
-        }
-      }
-
-      return $this;
-    }
-
-    public function disconnect() {
-
-      if (!is_resource($this->_socket)) return;
-
-      $this->write("QUIT\r\n");
-      fwrite($this->_log_handle, "\r\n");
-
-      fclose($this->_socket);
-      fclose($this->_log_handle);
-
-      return $this;
-    }
-
-    public function read($expected_response=null) {
-
-      $response = $buffer = '';
-      while (substr($buffer, 3, 1) != ' ') {
-        if (!$buffer = fgets($this->_socket, 256)) throw new Exception('No response from socket');
-        fwrite($this->_log_handle, "< $buffer");
-        $response .= $buffer;
-      }
-
-      $this->_last_response = $response;
-
-      if (substr($response, 0, 3) != $expected_response) throw new Exception('Unexpected socket response; '. $response);
-
-      return $this;
-    }
-
-    public function write($data, $expected_response=null) {
-
-      fwrite($this->_log_handle, "> $data");
-      $result = fwrite($this->_socket, $data);
-
-      if ($expected_response !== null) {
-        $this->read($expected_response);
-      }
-
-      return $this;
+  $defined_tables = array();
+  foreach (get_defined_constants() as $constant) {
+    if (preg_match('#^`'. preg_quote(DB_DATABASE, '#') .'`\.`(.*)`$#', $constant, $matches)) {
+      $defined_tables[] = $matches[1];
     }
   }
+
+  if (empty($_POST)) $_POST['tables'] = $defined_tables;
+
+  if (isset($_POST['convert'])) {
+
+    try {
+      if (empty($_POST['tables'])) throw new Exception(language::translate('error_must_select_tables', 'You must select tables'));
+      if (empty($_POST['collation']) && empty($_POST['engine'])) throw new Exception(language::translate('error_must_select_action_to_perform', 'You must select an action to perform'));
+
+      foreach ($_POST['tables'] as $table) {
+        if (!in_array($table, $defined_tables)) throw new Exception(strtr(language::translate('error_unknown_defined_table_x', 'Unknown defined table (%table)')), array('%table' => $table));
+      }
+
+      $_POST['collation'] = preg_replace('#[^a-z0-9_]#', '', $_POST['collation']);
+
+      if (!empty($_POST['set_database_default'])) {
+        database::query(
+          "alter database `". DB_DATABASE ."`
+          default character set ". database::input(preg_replace('#^([^_]+).*$#', '$1', $_POST['collation'])) ."
+          collate = ". database::input($_POST['collation']) .";"
+        );
+      }
+
+      if (!empty($_POST['collation'])) {
+        foreach ($_POST['tables'] as $table) {
+          database::query(
+            "alter table `". DB_DATABASE ."`.`". $table ."`
+            convert to character set ". database::input(preg_replace('#^([^_]+).*$#', '$1', $_POST['collation'])) ."
+            collate ". database::input($_POST['collation']) .";"
+          );
+        }
+      }
+
+      if (!empty($_POST['engine'])) {
+        foreach ($_POST['tables'] as $table) {
+          database::query(
+            "alter table `". DB_DATABASE ."`.`". $table ."`
+            engine=". database::input($_POST['engine']) .";"
+          );
+        }
+      }
+
+      notices::add('success', language::translate('success_changes_saved', 'Changes saved'));
+      header('Location: '. document::link());
+      exit;
+
+    } catch (Exception $e) {
+      notices::add('errors', $e->getMessage());
+    }
+  }
+
+// Table Rows
+  $tables = array();
+
+  $tables_query = database::query(
+    "select * from `information_schema`.`TABLES`
+    where TABLE_SCHEMA = '". DB_DATABASE ."';"
+  );
+
+  while ($table = database::fetch($tables_query)) {
+    if (!in_array($table['TABLE_NAME'], $defined_tables)) continue;
+    $tables[] = $table;
+  }
+
+// Number of Rows
+  $num_rows = database::num_rows($tables_query);
+?>
+<div class="panel panel-app">
+  <div class="panel-heading">
+    <?php echo $app_icon; ?> <?php echo language::translate('title_storage_encoding', 'Storage Encoding'); ?>
+  </div>
+
+  <div class="panel-body">
+    <p><?php echo language::translate('description_set_mysql_collation', 'This will recursively convert the charset and collation for all selected database tables and belonging columns.'); ?></p>
+
+    <?php echo functions::form_draw_form_begin('mysql_collation_form', 'post'); ?>
+
+      <h2><?php echo language::translate('title_database_tables', 'Database Tables'); ?></h2>
+
+      <table class="table table-striped table-hover data-table">
+        <thead>
+          <tr>
+            <th><?php echo functions::draw_fonticon('fa-check-square-o fa-fw checkbox-toggle', 'data-toggle="checkbox-toggle"'); ?></th>
+            <th class="main"><?php echo language::translate('title_table', 'Table'); ?></th>
+            <th><?php echo language::translate('title_collation', 'Collation'); ?></th>
+            <th><?php echo language::translate('title_engine', 'Engine'); ?></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <?php foreach ($tables as $table) { ?>
+          <tr>
+            <td><?php echo functions::form_draw_checkbox('tables[]', $table['TABLE_NAME'], true); ?></td>
+            <td><?php echo $table['TABLE_NAME']; ?></td>
+            <td><?php echo $table['TABLE_COLLATION']; ?></td>
+            <td><?php echo $table['ENGINE']; ?></td>
+          </tr>
+          <?php } ?>
+        </tbody>
+
+        <tfoot>
+          <tr>
+            <td colspan="12"><?php echo language::translate('title_tables', 'Tables'); ?>: <?php echo $num_rows; ?></td>
+          </tr>
+        </tfoot>
+      </table>
+
+      <div style="width: 640px;">
+        <div class="form-group">
+          <label><?php echo language::translate('title_collation', 'Collation'); ?></label>
+          <?php echo functions::form_draw_mysql_collations_list('collation'); ?>
+        </div>
+
+        <div class="form-group">
+          <div class="checkbox">
+            <label><?php echo functions::form_draw_checkbox('set_database_default', 'true'); ?> <?php echo language::translate('text_also_set_as_database_default', 'Also set as database default (when new tables are created)'); ?></label>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label><?php echo language::translate('title_engine', 'Engine'); ?></label>
+          <?php echo functions::form_draw_mysql_engines_list('engine'); ?>
+        </div>
+      </div>
+
+      <div class="btn-group">
+        <?php echo functions::form_draw_button('convert', language::translate('title_convert', 'Convert'), 'submit'); ?>
+      </div>
+
+    <?php echo functions::form_draw_form_end(); ?>
+  </div>
+</div>
