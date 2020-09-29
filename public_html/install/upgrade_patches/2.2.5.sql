77b6ad3545771a724eab2acd62bb9d1991e15aea
timint <info@tim-international.net>
2020-09-30 01:15:17 +0200

    * Change decimal columns from 10,4 to 11,4 for database consistency

--- public_html/install/upgrade_patches/2.2.5.sql
+++ public_html/install/upgrade_patches/2.2.5.sql
@@ -1,2 +1,7 @@
 ALTER TABLE `lc_products`
-ADD COLUMN `recommended_price` DECIMAL(11,4) NOT NULL AFTER `purchase_price_currency_code`;
+ADD COLUMN `recommended_price` DECIMAL(11,4) NOT NULL AFTER `purchase_price_currency_code`,
+CHANGE COLUMN `weight` `weight` DECIMAL(11,4) NOT NULL AFTER `quantity_unit_id`,
+CHANGE COLUMN `dim_x` `dim_x` DECIMAL(11,4) NOT NULL AFTER `weight_class`,
+CHANGE COLUMN `dim_y` `dim_y` DECIMAL(11,4) NOT NULL AFTER `dim_x`,
+CHANGE COLUMN `dim_z` `dim_z` DECIMAL(11,4) NOT NULL AFTER `dim_y`,
+CHANGE COLUMN `purchase_price` `purchase_price` DECIMAL(11,4) NOT NULL AFTER `dim_class`;
