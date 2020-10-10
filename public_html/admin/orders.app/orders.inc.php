d505778e62704be21817679b40e26a1d1cfa01d7
timint <info@tim-international.net>
2020-10-11 00:21:52 +0200

    * Always use frontend components for printable order copy and packing slip

--- public_html/admin/orders.app/orders.inc.php
+++ public_html/admin/orders.app/orders.inc.php
@@ -245,8 +245,8 @@ table .fa-star:hover {
             <td class="text-center"><?php echo ($order['order_status_id'] == 0) ? language::translate('title_unprocessed', 'Unprocessed') : $order['order_status_name']; ?></td>
             <td class="text-right"><?php echo language::strftime(language::$selected['format_datetime'], strtotime($order['date_created'])); ?></td>
             <td>
-              <a href="<?php echo document::href_link('', array('app' => 'orders', 'doc' => 'printable_packing_slip', 'order_id' => $order['id'], 'media' => 'print')); ?>" target="_blank" title="<?php echo language::translate('title_packing_slip', 'Packing Slip'); ?>"><?php echo functions::draw_fonticon('fa-file-text-o'); ?></a>
-              <a href="<?php echo document::href_link('', array('app' => 'orders', 'doc' => 'printable_order_copy', 'order_id' => $order['id'], 'media' => 'print')); ?>" target="_blank" title="<?php echo language::translate('title_order_copy', 'Order Copy'); ?>"><?php echo functions::draw_fonticon('fa-print'); ?></a>
+              <a href="<?php echo document::href_link('printable_packing_slip', array('order_id' => $order['id'], 'public_key' => $order['public_key'], 'media' => 'print')); ?>" target="_blank" title="<?php echo language::translate('title_packing_slip', 'Packing Slip'); ?>"><?php echo functions::draw_fonticon('fa-file-text-o'); ?></a>
+              <a href="<?php echo document::href_link('printable_order_copy', array('order_id' => $order['id'], 'public_key' => $order['public_key'], 'media' => 'print')); ?>" target="_blank" title="<?php echo language::translate('title_order_copy', 'Order Copy'); ?>"><?php echo functions::draw_fonticon('fa-print'); ?></a>
               <a href="<?php echo document::href_link('', array('app' => 'orders', 'doc' => 'edit_order', 'order_id' => $order['id'], 'redirect_url' => $_SERVER['REQUEST_URI'])); ?>" title="<?php echo language::translate('title_edit', 'Edit'); ?>"><?php echo functions::draw_fonticon('fa-pencil'); ?></a>
             </td>
           </tr>
---
+++ public_html/install/upgrade_patches/2.2.5.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+// Delete old files
+  $deleted_files = array(
+    FS_DIR_ADMIN . 'orders.app/printable_order_copy.inc.php',
+    FS_DIR_ADMIN . 'orders.app/printable_packing_slip.inc.php',
+    FS_DIR_ADMIN . 'includes/modules/customer/cm_google_maps.php',
+    FS_DIR_APP . 'includes/modules/customer/cm_google_maps.php',
+    FS_DIR_APP . 'includes/modules/customer/cm_local_database.php',
+  );
