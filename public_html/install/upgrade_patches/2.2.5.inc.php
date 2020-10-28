22ef47c34d95ef9fb77392189884fb8bcee5f039
timint <info@tim-international.net>
2020-10-28 02:49:37 +0100

    * Delete some files

--- public_html/install/upgrade_patches/2.2.5.inc.php
+++ public_html/install/upgrade_patches/2.2.5.inc.php
@@ -8,3 +8,9 @@
     FS_DIR_APP . 'includes/modules/customer/cm_google_maps.php',
     FS_DIR_APP . 'includes/modules/customer/cm_local_database.php',
   );
+
+  foreach ($deleted_files as $pattern) {
+    if (!file_delete($pattern)) {
+      echo '<span class="error">[Skip]</span></p>';
+    }
+  }
