f6f83bd097462bec03c0cec76343fafc5010c25c
timint <info@tim-international.net>
2020-11-23 04:06:52 +0100

    * Minor adjustments

--- public_html/backend/apps/catalog/catalog.inc.php
+++ public_html/backend/apps/catalog/catalog.inc.php
@@ -301,11 +301,11 @@
         try {
           $warning = null;
 
-          if ($product['date_valid_from'] > date('Y-m-d H:i:s')) {
+          if (strtotime($product['date_valid_from']) > time()) {
             throw new Exception(strtr(language::translate('text_product_cannot_be_purchased_until_x', 'The product cannot be purchased until %date'), ['%date' => language::strftime(language::$selected['format_date'], strtotime($product['date_valid_from']))]));
           }
 
-          if ($product['date_valid_to'] > '1971' && $product['date_valid_to'] < date('Y-m-d H:i:s')) {
+          if (strtotime($product['date_valid_to']) > '1971' && $product['date_valid_to'] < date('Y-m-d H:i:s')) {
             throw new Exception(strtr(language::translate('text_product_expired_at_x', 'The product expired at %date and can no longer be purchased'), ['%date' => language::strftime(language::$selected['format_date'], strtotime($product['date_valid_to']))]));
           }
 
@@ -448,7 +448,7 @@
             throw new Exception(strtr(language::translate('text_product_cannot_be_purchased_until_x', 'The product cannot be purchased until %date'), ['%date' => language::strftime(language::$selected['format_date'], strtotime($product['date_valid_from']))]));
           }
 
-          if ($product['date_valid_to'] > '1971' && $product['date_valid_to'] < date('Y-m-d H:i:s')) {
+          if (strtotime($product['date_valid_to']) > '1971' && $product['date_valid_to'] < date('Y-m-d H:i:s')) {
             throw new Exception(strtr(language::translate('text_product_expired_at_x', 'The product expired at %date and can no longer be purchased'), ['%date' => language::strftime(language::$selected['format_date'], strtotime($product['date_valid_to']))]));
           }
 
--- public_html/frontend/pages/product.inc.php
+++ public_html/frontend/pages/product.inc.php
@@ -24,7 +24,7 @@
     notices::add('errors', sprintf(language::translate('text_product_cannot_be_purchased_until_s', 'The product cannot be purchased until %s'), language::strftime(language::$selected['format_date'], strtotime($product->date_valid_from))));
   }
 
-  if (substr($product->date_valid_to, 0, 10) != '0000-00-00' && substr($product->date_valid_to, 0, 4) > '1971' && $product->date_valid_to < date('Y-m-d H:i:s')) {
+  if (strtotime($product->date_valid_to) > '1971' && $product->date_valid_to < date('Y-m-d H:i:s')) {
     notices::add('errors', language::translate('text_product_can_no_longer_be_purchased', 'The product can no longer be purchased'));
   }
 
