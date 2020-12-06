3b36562e2ba01ade13c797cfae345cb34aaf928b
timint <info@tim-international.net>
2020-12-06 19:47:26 +0100

    * Cookie notice adjustments

--- public_html/includes/templates/default.catalog/views/box_cookie_notice.inc.php
+++ public_html/includes/templates/default.catalog/views/box_cookie_notice.inc.php
@@ -2,11 +2,16 @@
   if (isset($_COOKIE['cookies_accepted']) && $_COOKIE['cookies_accepted'] != '') return;
   if (!settings::get('cookie_policy')) return;
 ?>
-<div id="box-cookie-notice" class="text-center">
-  <?php echo strtr(language::translate('text_cookie_notice', 'We rely on <a href="%url">cookies</a> to understand how our customers use our website.'), array('%url' => document::href_ilink('information', array('page_id' => settings::get('cookie_policy'))))); ?>
-  <div class="btn-group">
-    <?php echo functions::form_draw_button('accept_cookies', array('1', language::translate('text_i_accept', 'I accept')), 'button', 'style="font-weight: bold;"'); ?>
-    <?php echo functions::form_draw_button('decline_cookies', array('0', language::translate('text_i_decline', 'I decline')), 'button'); ?>
+<div id="box-cookie-notice">
+  <div class="twelve-eighty text-center">
+    <div class="notice">
+      <?php echo strtr(language::translate('text_cookie_notice', 'We rely on <a href="%url">cookies</a> for storing your shopping cart and regional settings between visits and to understand how our customers use our website.'), array('%url' => document::href_ilink('information', array('page_id' => settings::get('cookie_policy'))))); ?>
+    </div>
+
+    <div class="btn-group">
+      <?php echo functions::form_draw_button('accept_cookies', array('1', language::translate('text_i_accept', 'I accept')), 'button', 'style="font-weight: bold;"'); ?>
+      <?php echo functions::form_draw_button('decline_cookies', array('0', language::translate('text_i_decline', 'I decline')), 'button'); ?>
+    </div>
   </div>
 </div>
 
