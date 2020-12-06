dd15ca1487ca28a321a1d19bfd112ec9c66254fb
timint <info@tim-international.net>
2020-12-06 01:12:15 +0100

    - Remove deprecated file

--- public_html/includes/functions/func_reference.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-  function reference_in_geo_zone($geo_zone_id, $country_code, $zone_code='') {
-    //trigger_error(__METHOD__." is deprecated. Use instead reference::country('$country_code')->in_geo_zones('$zone_code')", E_USER_DEPRECATED);
-    return reference::country($country_code)->in_geo_zone($zone_code, $geo_zone_id);
-  }
