ca72b98394522009cd27c323443e7f91e11c7ee1
timint <info@tim-international.net>
2020-12-18 21:46:02 +0100

    * Make the reference object instantiating support a varying amount of construct parameters

--- public_html/includes/library/lib_reference.inc.php
+++ public_html/includes/library/lib_reference.inc.php
@@ -34,11 +34,8 @@
           $class_name = 'ref_'.$resource;
 
           //self::$_cache[$resource][$checksum] = new $class_name(...$arguments); // As of PHP 5.6
-          self::$_cache[$resource][$checksum] = new $class_name(
-            isset($arguments[0]) ? $arguments[0] : null,
-            isset($arguments[1]) ? $arguments[1] : null,
-            isset($arguments[2]) ? $arguments[2] : null
-          );
+          $reflect = new ReflectionClass($class_name);
+          self::$_cache[$resource][$checksum] = $reflect->newInstanceArgs($arguments);
 
           call_user_func_array(array(self::$_cache[$resource][$checksum], '__construct'), $arguments);
 
