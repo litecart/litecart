10aa64eee8cfb6e559902f4d7421f59da60cd602
timint <info@tim-international.net>
2020-12-26 22:28:37 +0100

    * English language correction

--- public_html/includes/functions/func_form.inc.php
+++ public_html/includes/functions/func_form.inc.php
@@ -368,7 +368,7 @@
     if (is_bool($parameters)) {
       $args = func_get_args();
       if ($parameters === true) {
-        trigger_error('The 4th parameter $multiple in form_draw_select_field() has been deprecated. Use instead form_draw_select_multiple_field()', E_USER_DEPRECATED);
+        trigger_error('The 4th parameter $multiple in form_draw_select_field() has been deprecated. Use form_draw_select_multiple_field()', E_USER_DEPRECATED);
         return form_draw_select_multiple_field($args[0], $args[1], $args[2], isset($args[4]) ? $args[4] : '');
       } else {
         trigger_error('The 4th parameter $multiple in form_draw_select_field() has been deprecated', E_USER_DEPRECATED);
--- public_html/includes/functions/func_reference.inc.php
+++ public_html/includes/functions/func_reference.inc.php
@@ -1,41 +1,41 @@
 <?php
 
   function reference_in_geo_zone($geo_zone_id, $country_code, $zone_code='') {
-    trigger_error(__METHOD__." is deprecated. Use instead reference::country($country_code)->in_geo_zone($zone_code)", E_USER_DEPRECATED);
+    trigger_error(__METHOD__." is deprecated. Use reference::country($country_code)->in_geo_zone($zone_code)", E_USER_DEPRECATED);
     return reference::country($country_code)->in_geo_zone($zone_code, $geo_zone_id);
   }
 
   function reference_get_manufacturer_name($manufacturer_id) {
-    trigger_error(__METHOD__." is deprecated. Use instead reference::manufacturer($manufacturer_id)->name", E_USER_DEPRECATED);
+    trigger_error(__METHOD__." is deprecated. Use reference::manufacturer($manufacturer_id)->name", E_USER_DEPRECATED);
     return reference::manufacturer($manufacturer_id)->name;
   }
 
   function reference_get_country_name($country_code) {
-    trigger_error(__METHOD__." is deprecated. Use instead reference::country($country_code)->name", E_USER_DEPRECATED);
+    trigger_error(__METHOD__." is deprecated. Use reference::country($country_code)->name", E_USER_DEPRECATED);
     return reference::country($country_code)->name;
   }
 
   function reference_get_zone_name($country_code, $zone_code) {
-    trigger_error(__METHOD__." is deprecated. Use instead reference::country($country_code)->zones[$zone_code]['name']", E_USER_DEPRECATED);
+    trigger_error(__METHOD__." is deprecated. Use reference::country($country_code)->zones[$zone_code]['name']", E_USER_DEPRECATED);
     return isset(reference::country($country_code)->zones[$zone_code]) ? reference::country($country_code)->zones[$zone_code]['name'] : null;
   }
 
   function reference_country_num_zones($country_code) {
-    trigger_error(__METHOD__." is deprecated. Use instead count(reference::country($country_code)->zones)", E_USER_DEPRECATED);
+    trigger_error(__METHOD__." is deprecated. Use count(reference::country($country_code)->zones)", E_USER_DEPRECATED);
     return count(reference::country($country_code)->zones);
   }
 
   function reference_verify_zone_code($country_code, $zone_code) {
-    trigger_error(__METHOD__." is deprecated. Use instead isset(reference::country($country_code)->zones[$zone_code])", E_USER_DEPRECATED);
+    trigger_error(__METHOD__." is deprecated. Use isset(reference::country($country_code)->zones[$zone_code])", E_USER_DEPRECATED);
     return isset(reference::country($country_code)->zones[$zone_code]);
   }
 
   function reference_get_postcode_required($country_code) {
-    trigger_error(__METHOD__." is deprecated. Use instead reference::country($country_code)->postcode_format", E_USER_DEPRECATED);
+    trigger_error(__METHOD__." is deprecated. Use reference::country($country_code)->postcode_format", E_USER_DEPRECATED);
     return !empty(reference::country($country_code)->postcode_format);
   }
 
   function reference_get_phone_country_code($country_code) {
-    trigger_error(__METHOD__." is deprecated. Use instead reference::country($country_code)->phone_code", E_USER_DEPRECATED);
+    trigger_error(__METHOD__." is deprecated. Use reference::country($country_code)->phone_code", E_USER_DEPRECATED);
     return '+'. reference::country($country_code)->phone_code;
   }
