fa5b39baf6e6c66522e6e86c29a31278c7aed21c
timint <info@tim-international.net>
2021-01-01 04:16:20 +0100

    * Bump version

--- public_html/includes/app_header.inc.php
+++ public_html/includes/app_header.inc.php
@@ -1,6 +1,6 @@
 <?php
   define('PLATFORM_NAME', 'LiteCart');
-  define('PLATFORM_VERSION', '2.2.6');
+  define('PLATFORM_VERSION', '2.2.6.1');
 
   if (!file_exists(__DIR__ . '/config.inc.php')) {
     header('Location: ./install/');
--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,6 @@
 <?php
 /*!
- * LiteCart® 2.2.6
+ * LiteCart® 2.2.6.1
  *
  * Online Catalog and Shopping Cart Platform
  *
