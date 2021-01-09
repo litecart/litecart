451acf150ecfad16d6d432abcdce70b0100acfa3
timint <info@tim-international.net>
2021-01-09 21:24:54 +0100

    * Make snippet wrappers customizable, and minor adjustments

--- public_html/includes/entities/ent_view.inc.php
+++ public_html/includes/entities/ent_view.inc.php
@@ -1,7 +1,8 @@
 <?php
   class ent_view {
-    public $snippets = [];
     public $html = '';
+    public $snippets = [];
+    public $wrapper = ['{{', '}}'];
 
     public function stitch($view=null, $cleanup=false) {
 
@@ -19,8 +20,10 @@
           if (!is_file($file)) $file = vmod::check(FS_DIR_APP . 'frontend/templates/default/'. $view .'.inc.php');
         }
 
+      // Load view
         $fn = function(){
           ob_start();
+          $view_file = preg_replace('#^'. preg_quote(FS_DIR_APP, '#') .'#', '', func_get_arg(0));
           extract(func_get_arg(1));
           include vmod::check(func_get_arg(0));
           return ob_get_clean();
@@ -32,24 +35,26 @@
 
       if (empty($this->html)) return;
 
-      if (!empty($this->snippets)) {
-
-        $search_replace = [];
-        foreach (array_keys($this->snippets) as $key) {
-          if (!is_string($this->snippets[$key])) continue;
-          $search_replace['#{{\s*'. preg_quote($key, '#') .'\s*}}#'] = addcslashes($this->snippets[$key], '\\$');
-        }
-
-        foreach (array_keys($search_replace) as $key) {
-          $search_replace[$key] = preg_replace(array_keys($search_replace), array_values($search_replace), $search_replace[$key]);
-        }
-
-        $this->html = preg_replace(array_keys($search_replace), array_values($search_replace), $this->html);
+    // Create stack of patterns and replacements
+      $stack = [];
+      foreach (array_keys($this->snippets) as $key) {
+        if (!is_string($this->snippets[$key])) continue;
+        $pattern = '#'. preg_quote($this->wrapper[0], '#') .'\s*'. preg_quote($key, '#') .'\s*'. preg_quote($this->wrapper[1], '#') .'#';
+        $replace = addcslashes($this->snippets[$key], '\\$');
+        $stack[$pattern] = $replace;
       }
+
+    // Replace any patterns in stack to prevent being left out
+      foreach (array_keys($stack) as $key) {
+        $stack[$key] = preg_replace(array_keys($stack), array_values($stack), $stack[$key]);
+      }
+
+    // Perform replacing
+      $this->html = preg_replace(array_keys($stack), array_values($stack), $this->html);
 
     // Clean orphan snippets
       if ($cleanup) {
-        $this->html = preg_replace('#\{\{.*?\}\}#', '', $this->html);
+        $this->html = preg_replace('#'. preg_quote($this->wrapper[0], '#') .'.*?'. preg_quote($this->wrapper[1], '#') .'#', '', $this->html);
       }
 
       return $this->html;
