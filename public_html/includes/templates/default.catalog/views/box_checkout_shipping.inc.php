8414ca3f5338f3ee3ee64298a532b33f3fda1e4b
timint <info@tim-international.net>
2021-01-28 03:47:28 +0100

    * Mobile adjustments

--- public_html/includes/templates/default.catalog/views/box_checkout_shipping.inc.php
+++ public_html/includes/templates/default.catalog/views/box_checkout_shipping.inc.php
@@ -7,11 +7,11 @@
     <label class="option btn btn-default btn-block<?php echo (!empty($selected['id']) && $selected['id'] == $module['id'].':'.$option['id']) ? ' active' : ''; ?><?php echo !empty($option['error']) ? ' disabled' : ''; ?>">
       <?php echo functions::form_draw_radio_button('shipping[option_id]', $module['id'].':'.$option['id'], !empty($selected['id']) ? $selected['id'] : '', 'style="display: none;"' . (!empty($option['error']) ? ' disabled="disabled"' : '')); ?>
       <div class="header row" style="margin: 0;">
-        <div class="col-sm-3 thumbnail" style="margin: 0;">
+        <div class="col-xs-3 thumbnail" style="margin: 0;">
           <img src="<?php echo document::href_link(WS_DIR_APP . functions::image_thumbnail(FS_DIR_APP . $option['icon'], 140, 60, 'FIT_ONLY_BIGGER_USE_WHITESPACING')); ?>" />
         </div>
 
-        <div class="col-sm-9 text-left" style="padding-bottom: 0;">
+        <div class="col-xs-9 text-left" style="padding-bottom: 0;">
           <div class="title"><?php echo $module['title']; ?></div>
           <div class="name"><?php echo $option['name']; ?></div>
           <div class="price"><?php echo (empty($option['error']) && (float)$option['cost'] != 0) ? '+ ' . currency::format(tax::get_price($option['cost'], $option['tax_class_id'])) : language::translate('text_no_fee', 'No fee'); ?></div>
