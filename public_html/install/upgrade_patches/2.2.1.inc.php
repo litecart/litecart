c5ed762b9702a222dfd66f2113c25bdcd34a6b79
timint <info@tim-international.net>
2021-02-01 20:02:39 +0100

    - Remove multiple_category_images vqmod

--- public_html/install/upgrade_patches/2.2.1.inc.php
+++ public_html/install/upgrade_patches/2.2.1.inc.php
@@ -26,15 +26,12 @@
   );
 
   if (database::num_rows($categories_images_query)) {
-
     $modified_files[] = array(
       'file'    => FS_DIR_APP . 'includes/config.inc.php',
       'search'  => "// Database tables (Add-ons)" . PHP_EOL,
       'replace' => "// Database tables (Add-ons)" . PHP_EOL
                  . "  define('DB_TABLE_CATEGORIES_IMAGES',                 '`'. DB_DATABASE .'`.`'. DB_TABLE_PREFIX . 'categories_images`');" . PHP_EOL,
     );
-
-    copy(FS_DIR_APP . 'install/data/other/multiple_category_images.xml', FS_DIR_APP . 'vqmod/xml/multiple_category_images.xml');
   }
 
 // Modify some files
