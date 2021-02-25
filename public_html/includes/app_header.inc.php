beec05637ea0fbeb9281f5172c7d6e91dc7a70bd
timint <info@tim-international.net>
2021-02-25 20:18:29 +0100

    * Bump version

--- public_html/includes/app_header.inc.php
+++ public_html/includes/app_header.inc.php
@@ -1,6 +1,6 @@
 <?php
   define('PLATFORM_NAME', 'LiteCart');
-  define('PLATFORM_VERSION', '2.2.8');
+  define('PLATFORM_VERSION', '2.2.9');
 
   if (!file_exists(__DIR__ . '/config.inc.php')) {
     header('Location: ./install/');
