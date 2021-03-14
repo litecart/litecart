1a3519fe20b16177177c94c8550ba6c5f39f1005
timint <info@tim-international.net>
2021-03-14 03:43:05 +0100

    * List all products from all subcategories descending from category

--- public_html/frontend/pages/category.inc.php
+++ public_html/frontend/pages/category.inc.php
@@ -79,7 +79,7 @@
 
   // Products
     $products_query = functions::catalog_products_query([
-      'categories' => [$category->id],
+      'categories' => [$category->id] + array_keys($category->descendants),
       'brands' => !empty($_GET['brands']) ? $_GET['brands'] : null,
       'attributes' => !empty($_GET['attributes']) ? $_GET['attributes'] : null,
       'product_name' => !empty($_GET['product_name']) ? $_GET['product_name'] : null,
