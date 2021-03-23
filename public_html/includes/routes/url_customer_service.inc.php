37924a02369b8ba7e6d321144162560b670235e6
timint <info@tim-international.net>
2021-03-23 15:15:04 +0100

    * Fix regexp group

--- public_html/includes/routes/url_customer_service.inc.php
+++ public_html/includes/routes/url_customer_service.inc.php
@@ -11,7 +11,7 @@
 
       return array(
         array(
-          'pattern' => '#^'. implode('|', array_filter($titles)) .'$#',
+          'pattern' => '#^('. implode('|', array_filter($titles)) .')$#',
           'page' => 'customer_service',
           'params' => '',
           'options' => array(
