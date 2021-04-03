e149963e99f776a58f65293707c42eedbccd5415
timint <info@tim-international.net>
2021-04-04 00:25:49 +0200

    * Fix checkbox

--- public_html/backend/template/pages/login.inc.php
+++ public_html/backend/template/pages/login.inc.php
@@ -98,8 +98,10 @@ body {
           <?php echo functions::form_draw_password_field('password', '', 'placeholder="'. language::translate('title_password', 'Password') .'"'); ?>
         </div>
 
-        <div class="form-group checkbox">
-          <label><?php echo functions::form_draw_checkbox('remember_me', '1'); ?> <?php echo language::translate('title_remember_me', 'Remember Me'); ?></label>
+        <div class="form-group">
+          <label class="checkbox">
+            <?php echo functions::form_draw_checkbox('remember_me', '1'); ?> <?php echo language::translate('title_remember_me', 'Remember Me'); ?>
+          </label>
         </div>
       </div>
 
