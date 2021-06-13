46259ca5b4eb83a486976416493ba671924b9dec
timint <info@tim-international.net>
2021-06-14 00:51:57 +0200

    * Rename cart to shopping cart

--- public_html/frontend/boxes/box_shopping_cart.inc.php
+++ public_html/frontend/boxes/box_shopping_cart.inc.php
@@ -1,8 +1,8 @@
 <?php
   if (settings::get('catalog_only_mode')) return;
 
-  $box_cart = new ent_view();
-  $box_cart->snippets = [
+  $box_shopping_cart = new ent_view();
+  $box_shopping_cart->snippets = [
     'items' => [],
     'link' => document::ilink('shopping_cart'),
     'num_items' => cart::$total['items'],
@@ -11,13 +11,13 @@
 
   foreach (cart::$items as $key => $item) {
     $item['thumbnail'] = functions::image_thumbnail(FS_DIR_STORAGE . 'images/' . $item['image'], 64, 64, 'FIT_USE_WHITESPACING');
-    $box_cart->snippets['items'][$key] = $item;
+    $box_shopping_cart->snippets['items'][$key] = $item;
   }
 
   if (!empty(customer::$data['display_prices_including_tax'])) {
-    $box_cart->snippets['cart_total'] = currency::format(cart::$total['value'] + cart::$total['tax']);
+    $box_shopping_cart->snippets['cart_total'] = currency::format(cart::$total['value'] + cart::$total['tax']);
   } else {
-    $box_cart->snippets['cart_total'] = currency::format(cart::$total['value']);
+    $box_shopping_cart->snippets['cart_total'] = currency::format(cart::$total['value']);
   }
 
-  echo $box_cart->stitch('views/box_cart.inc.php');
+  echo $box_shopping_cart->stitch('views/box_shopping_cart.inc.php');
