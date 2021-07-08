bf0a08be9c595e21a95201d1de2a60fe7bb2f926
timint <info@tim-international.net>
2021-07-08 18:06:53 +0200

    * Update number of products in listing due to new 1440px standard

---
+++ public_html/install/upgrade_patches/2.3.0.sql
@@ -0,0 +1,3 @@
+UPDATE `lc_settings` SET `value` = '5' WHERE `key` IN ('box_also_purchased_products_num_items', 'box_campaign_products_num_items', 'box_latest_products_num_items', 'box_popular_products_num_items', 'box_similar_products_num_items') AND `value` = '4';
+-- --------------------------------------------------------
+UPDATE `lc_settings` SET `value` = '10' WHERE `key` IN ('box_also_purchased_products_num_items', 'box_campaign_products_num_items', 'box_latest_products_num_items', 'box_popular_products_num_items', 'box_similar_products_num_items') AND `value` = '8';
