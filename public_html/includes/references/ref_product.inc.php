06545e4d1f3c04b3436723faa571be5012570214
T. Almroth <info@tim-international.net>
2021-07-19 21:48:21 +0200

    ! Fix tax not calculated correctly when not a campaign

--- public_html/includes/references/ref_product.inc.php
+++ public_html/includes/references/ref_product.inc.php
@@ -188,6 +188,12 @@
 
           break;
 
+        case 'final_price':
+
+          $this->_data['final_price'] = (!empty($this->campaign['price']) && $this->campaign['price'] != 0) ? $this->campaign['price'] : $this->price;
+
+          break;
+
         case 'images':
 
           $this->_data['images'] = array();
@@ -498,7 +504,7 @@
 
         case 'tax':
 
-          $this->_data['tax'] = tax::get_tax(!empty($this->campaign) ? $this->campaign['price'] : $this->price, $this->tax_class_id);
+          $this->_data['tax'] = tax::get_tax((!empty($this->campaign['price']) && $this->campaign['price'] != 0) ? $this->campaign['price'] : $this->price, $this->tax_class_id);
 
           break;
 
