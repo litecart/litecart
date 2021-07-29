b6e49aad95ace18483b7aec56ed0953e801b7f1c
timint <info@tim-international.net>
2021-07-29 21:00:00 +0200

    * Restore cache for apps

--- public_html/includes/functions/func_admin.inc.php
+++ public_html/includes/functions/func_admin.inc.php
@@ -2,16 +2,19 @@
 
   function admin_get_apps() {
 
+    $apps_cache_token = cache::token('backend_apps', ['language']);
+    if (!$apps = cache::get($apps_cache_token)) {
+
       $apps = [];
 
       foreach (glob(FS_DIR_APP . 'backend/apps/*', GLOB_ONLYDIR) as $directory) {
         if (!$app_config = require vmod::check($directory . '/config.inc.php')) continue;
 
-        $code = basename($directory);
-        $apps[$code] = array_merge(['code' => $code, 'directory' => rtrim($directory, '/') . '/'], $app_config);
+        $id = basename($directory);
+        $apps[$id] = array_merge(['id' => $id, 'directory' => rtrim($directory, '/') . '/'], $app_config);
       }
 
-      usort($apps, function($a, $b) use ($apps) {
+      uasort($apps, function($a, $b) use ($apps) {
 
         if (!isset($a['priority'])) $a['priority'] = 0;
         if (!isset($b['priority'])) $b['priority'] = 0;
@@ -22,22 +25,28 @@
 
         return ($a['priority'] < $b['priority']) ? -1 : 1;
       });
+
+      cache::set($apps_cache_token, $apps);
+    }
 
     return $apps;
   }
 
   function admin_get_widgets() {
 
+    $widgets_cache_token = cache::token('backend_widgets', ['language']);
+    if (!$widgets = cache::get($widgets_cache_token)) {
+
       $widgets = [];
 
       foreach (glob(FS_DIR_APP . 'backend/widgets/*', GLOB_ONLYDIR) as $directory) {
         if (!$widget_config = require vmod::check($directory . '/config.inc.php')) return;
 
-        $code = basename($directory);
-        $widgets[$code] = array_merge(['code' => $code, 'directory' => rtrim($directory, '/') . '/'], $widget_config);
+        $id = basename($directory);
+        $widgets[$id] = array_merge(['id' => $id, 'directory' => rtrim($directory, '/') . '/'], $widget_config);
       }
 
-      usort($widgets, function($a, $b) use ($widgets) {
+      uasort($widgets, function($a, $b) use ($widgets) {
 
         if (!isset($a['priority'])) $a['priority'] = 0;
         if (!isset($b['priority'])) $b['priority'] = 0;
@@ -48,6 +57,9 @@
 
         return ($a['priority'] < $b['priority']) ? -1 : 1;
       });
+
+      cache::set($widgets_cache_token, $widgets);
+    }
 
     return $widgets;
   }
