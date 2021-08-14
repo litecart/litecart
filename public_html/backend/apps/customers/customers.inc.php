958e7658f9e01379eb78a3872de1fa57b1bfeef8
timint <info@tim-international.net>
2021-08-14 22:16:41 +0200

    + Ability to batch delete customers

--- public_html/backend/apps/customers/customers.inc.php
+++ public_html/backend/apps/customers/customers.inc.php
@@ -26,6 +26,20 @@
     }
   }
 
+  if (!empty($_POST['delete'])) {
+
+    if (!empty($_POST['customers'])) {
+      foreach ($_POST['customers'] as $customer_id) {
+        $customer = new ent_customer($customer_id);
+        $customer->delete();
+      }
+    }
+
+    notices::add('success', language::translate('success_changes_saved', 'Changes saved'));
+    header('Location: '. document::link());
+    exit;
+  }
+
 // Table Rows
   $customers = [];
 
@@ -136,10 +150,15 @@
       </tfoot>
     </table>
 
-    <div class="btn-group">
-      <?php echo functions::form_draw_button('enable', language::translate('title_enable', 'Enable'), 'submit', '', 'on'); ?>
-      <?php echo functions::form_draw_button('disable', language::translate('title_disable', 'Disable'), 'submit', '', 'off'); ?>
-    </div>
+    <ul class="list-inline">
+      <li>
+      <div class="btn-group">
+        <?php echo functions::form_draw_button('enable', language::translate('title_enable', 'Enable'), 'submit', '', 'on'); ?>
+        <?php echo functions::form_draw_button('disable', language::translate('title_disable', 'Disable'), 'submit', '', 'off'); ?>
+      </div>
+      </li>
+      <li><?php echo functions::form_draw_button('delete', language::translate('title_delete', 'Delete'), 'submit', 'class="btn btn-danger" onclick="if (!confirm(\''. language::translate('text_are_you_sure', 'Are you sure?') .'\')) return false;"', 'delete'); ?></li>
+    </ul>
 
   <?php echo functions::form_draw_form_end(); ?>
 
