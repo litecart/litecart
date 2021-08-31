4099bcaf677563696a0c91b23ce2c911a33d5391
timint <info@tim-international.net>
2021-08-31 16:20:32 +0200

    * Minor adjustments

--- public_html/frontend/templates/default/views/box_newsletter_subscribe.inc.php
+++ public_html/frontend/templates/default/views/box_newsletter_subscribe.inc.php
@@ -1,6 +1,6 @@
 <style>
 #box-newsletter-subscribe {
-  padding: 2rem;
+  padding: calc(var(--gutter-size) * 2);
   background: #f9f9f9;
 }
 #box-newsletter-subscribe .row > div:last-child {
@@ -19,7 +19,7 @@
       <div class="col-sm-9 col-md-10">
         <?php echo functions::form_draw_form_begin('newsletter_subscribe_form', 'post'); ?>
 
-          <h2><?php echo language::translate('box-newsletter-subscribe:title', 'Subscribe to our newsletter!'); ?></h2>
+          <h2 class="mt-0"><?php echo language::translate('box-newsletter-subscribe:title', 'Subscribe to our newsletter!'); ?></h2>
 
           <p><?php echo language::translate('box_newsletter_subscribe:description', 'Get the latest news and offers straight to your inbox. Sign up now.'); ?></p>
 
