b29b1568a64813f1e5a5197929ee63923e8a9260
timint <info@tim-international.net>
2021-10-06 00:09:09 +0200

    * Minor adjustments

--- public_html/backend/apps/appearance/logotype.inc.php
+++ public_html/backend/apps/appearance/logotype.inc.php
@@ -29,7 +29,7 @@
         throw new Exception(language::translate('error_failed_uploading_image', 'The uploaded image failed saving to disk. Make sure permissions are set.'));
       }
 
-      notices::add('success', language::translate('success_logotype_saved', 'Changes saved successfully. Your browser may still show the old logotype due to cache.'));
+      notices::add('success', language::translate('success_changes_saved_refresh_cache', 'Changes saved successfully. If you don\'t see any changes, try <a href="https://www.google.com/search?q=how+to+hard+refresh+a+web+page" target="_blank">hard refreshing</a> the page or clear browser cache.'));
       header('Location: '. document::ilink());
       exit;
 
@@ -46,21 +46,18 @@
   </div>
 
   <div class="card-body">
-    <?php echo functions::form_draw_form_begin('logotype_form', 'post', false, true, 'style="max-width: 320px;"'); ?>
+    <?php echo functions::form_draw_form_begin('logotype_form', 'post', false, true); ?>
 
       <div class="thumbnail" style="padding: 1em; display: inline-block;  margin-top: 1em;">
-        <img src="<?php echo document::href_rlink(FS_DIR_STORAGE . functions::image_thumbnail(FS_DIR_STORAGE . 'images/logotype.png', 500, 500, 'FIT_ONLY_BIGGER')); ?>" alt="" />
+        <img src="<?php echo document::href_rlink(FS_DIR_STORAGE . functions::image_thumbnail(FS_DIR_STORAGE . 'images/logotype.png', 480, 480, 'FIT_ONLY_BIGGER')); ?>" alt="" />
       </div>
 
-      <div class="form-group">
+      <div class="form-group" style="max-width: 480px;">
         <label><?php echo language::translate('title_new_image', 'New Image'); ?></label>
-        <?php echo functions::form_draw_file_field('image', 'accept="image/*"'); ?>
-      </div>
-
-      <div class="card-action">
-        <?php echo functions::form_draw_button('save', language::translate('title_save', 'Save'), 'submit', '', 'save'); ?>
-        <?php echo (isset($pages->data['id'])) ? functions::form_draw_button('delete', language::translate('title_delete', 'Delete'), 'submit', 'formnovalidate class="btn btn-danger" onclick="if (!window.confirm(\''. language::translate('text_are_you_sure', 'Are you sure?') .'\')) return false;"', 'delete') : false; ?>
-        <?php echo functions::form_draw_button('cancel', language::translate('title_cancel', 'Cancel'), 'button', 'onclick="history.go(-1);"', 'cancel'); ?>
+        <div class="input-group">
+          <?php echo functions::form_draw_file_field('image', 'accept="image/*"'); ?>
+          <?php echo functions::form_draw_button('upload', language::translate('title_upload', 'Upload'), 'submit'); ?>
+        </div>
       </div>
 
     <?php echo functions::form_draw_form_end(); ?>
