1c01a0bf2c1e1902c0c16bf6a4e4f9e852a21402
timint <info@tim-international.net>
2021-10-10 02:11:16 +0200

    + Webmanifest

---
+++ public_html/backend/pages/webmanifest.json.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+  $manifest = [
+    'name' => settings::get('site_name'),
+    'start_url' => document::ilink(''),
+    'display' => 'standalone',
+    'background_color' => '#e6e8ec',
+    'icons' => [
+      [
+        'src' => document::link(WS_DIR_STORAGE . 'images/favicon.ico'),
+        'sizes' => '32x32 48x48 64x64 96x96',
+      ],
+      [
+        'src' => document::link(WS_DIR_STORAGE . 'images/favicon-128x128.png'),
+        'sizes' => '128x128',
+      ],
+      [
+        'src' => document::link(WS_DIR_STORAGE . 'images/favicon-192x192.png'),
+        'sizes' => '192x192',
+      ],
+      [
+        'src' => document::link(WS_DIR_STORAGE . 'images/favicon-256x256.png'),
+        'sizes' => '256x256',
+      ]
+    ],
+  ];
+
+  header('Content-Type: application/manifest+json; charset=utf-8');
+  echo json_encode($manifest, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+  exit;
