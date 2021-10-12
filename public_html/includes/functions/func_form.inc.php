14711d58fc2dab1dc96b5ac1e8d16fb1c45dad06
timint <info@tim-international.net>
2021-10-12 15:10:26 +0200

    * Fix some styling

--- public_html/includes/functions/func_form.inc.php
+++ public_html/includes/functions/func_form.inc.php
@@ -861,10 +861,10 @@
           }
         }
 
-        $html .= '<li class="list-item" style="display: flex;">'. PHP_EOL
+        $html .= '<li class="list-item" style="display: flex; align-items: center;">'. PHP_EOL
                . '  ' . form_draw_hidden_field($name, $category['id'], 'data-name="'. htmlspecialchars($category['name']) .'"') . PHP_EOL
                . '  <div style="flex-grow: 1;">' . functions::draw_fonticon('fa-folder', 'style="color: #cccc66;"') .' '. implode(' &gt; ', $path) .'</div>'. PHP_EOL
-               . '  <button class="remove btn btn-default btn-sm" type="button">'. language::translate('title_remove', 'Remove') .'</button>' . PHP_EOL
+               . '  <div><button class="remove btn btn-default btn-sm" type="button">'. language::translate('title_remove', 'Remove') .'</button></div>' . PHP_EOL
                .'</li>';
       }
     }
