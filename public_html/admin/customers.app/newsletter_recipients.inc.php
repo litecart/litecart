0f52eefc3967c4ff65671d7caae04b77719bdd85
timint <info@tim-international.net>
2021-10-13 16:13:20 +0200

    * Fix icon

--- public_html/admin/customers.app/newsletter_recipients.inc.php
+++ public_html/admin/customers.app/newsletter_recipients.inc.php
@@ -139,7 +139,7 @@
             <td><?php echo $recipient['email']; ?></td>
             <td><?php echo $recipient['client_ip']; ?></td>
             <td class="text-end"><?php echo language::strftime(language::$selected['format_datetime'], strtotime($recipient['date_created'])); ?></td>
-            <td class="text-end"><a href="<?php echo document::href_ilink('customers/edit_recipient', ['recipient_id' => $recipient['id']]); ?>" title="<?php echo language::translate('title_edit', 'Edit'); ?>"><?php echo functions::draw_fonticon('edit'); ?></a></td>
+            <td class="text-end"><a href="<?php echo document::href_ilink('customers/edit_recipient', ['recipient_id' => $recipient['id']]); ?>" title="<?php echo language::translate('title_edit', 'Edit'); ?>"><?php echo functions::draw_fonticon('fa-pencil'); ?></a></td>
           </tr>
           <?php } ?>
         </tbody>
