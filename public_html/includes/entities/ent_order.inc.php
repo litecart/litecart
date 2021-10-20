61b783c840705b6297ca79a87b715c6d8ebfabce
timint <info@tim-international.net>
2021-10-20 02:56:42 +0200

    + Default incoterm

--- public_html/includes/entities/ent_order.inc.php
+++ public_html/includes/entities/ent_order.inc.php
@@ -52,6 +52,7 @@
         'currency_code' => currency::$selected['code'],
         'currency_value' => currency::$selected['value'],
         'language_code' => language::$selected['code'],
+        'incoterm' => settings::get('default_incoterm'),
         'items' => [],
         'order_total' => [],
         'comments' => [],
