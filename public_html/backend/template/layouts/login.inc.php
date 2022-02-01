ab48b13b17b9f6e907c149f2c1f48b21ae7c1bd5
timint <info@tim-international.net>
2022-02-01 05:32:27 +0100

    - Remove browser update.js (Soo last decade)

--- public_html/backend/template/layouts/login.inc.php
+++ public_html/backend/template/layouts/login.inc.php
@@ -17,15 +17,6 @@
 {{foot_tags}}
 <script src="<?php echo document::href_rlink(FS_DIR_TEMPLATE . 'js/app.min.js'); ?>"></script>
 {{javascript}}
-<script>
-  var $buoop = {c:2};
-  function $buo_f(){
-    var e = document.createElement("script");
-    e.src = "https://browser-update.org/update.js";
-    document.body.appendChild(e);
-  };
-  try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
-  catch(e){window.attachEvent("onload", $buo_f)}
-</script>
+
 </body>
 </html>
