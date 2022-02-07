65ced4edcbfc48ade51ac5fd0ca0d13e4acacd87
timint <info@tim-international.net>
2022-02-07 17:18:21 +0100

    * Set order status change comment in store language
    (cherry picked from commit 5b595164ddba23343b6b1d21be0c91ebd54e1d53)

--- public_html/includes/entities/ent_order.inc.php
+++ public_html/includes/entities/ent_order.inc.php
@@ -195,7 +195,7 @@
       if (!empty($this->previous['id']) && ($this->data['order_status_id'] != $this->previous['order_status_id'])) {
         $this->data['comments'][] = [
           'author' => 'system',
-          'text' => strtr(language::translate('text_user_changed_order_status_to_new_status', 'Order status changed to %new_status by %username'), [
+          'text' => strtr(language::translate('text_user_changed_order_status_to_new_status', 'Order status changed to %new_status by %username', settings::get('store_language_code')), [
             '%username' => !empty(user::$data['username']) ? user::$data['username'] : 'system',
             '%new_status' => reference::order_status($this->data['order_status_id'])->name,
           ]),
