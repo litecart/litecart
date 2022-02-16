d3a670d050540dcebaf3fdbaa787c2b549b3eec6
timint <info@tim-international.net>
2022-02-16 20:09:56 +0100

    * Convert decimal columns to float as a decimal value is returned as string by mysqli

--- public_html/install/upgrade_patches/2.4.0.inc.php
+++ public_html/install/upgrade_patches/2.4.0.inc.php
@@ -1,3 +1,15 @@
 <?php
 
-  // Do nothing
+  $columns_query = database::query(
+    "SELECT * FROM `information_schema`.COLUMNS
+    WHERE TABLE_SCHEMA = '". DB_DATABASE ."'
+    AND TABLE_NAME like '". DB_TABLE_PREFIX ."%'
+    AND DATA_TYPE = 'double';"
+  );
+
+  while ($column = database::fetch($columns_query)) {
+    database::query(
+      "ALTER TABLE ". $column['TABLE_NAME'] ."
+      CHANGE COLUMN `". $column['COLUMN_NAME'] ."` `". $column['COLUMN_NAME'] ."` ". strtr($column['COLUMN_TYPE'], ['double' => 'float']) ." ". (($column['IS_NULLABLE'] == 'YES') ? "NULL" : "NOT NULL") ." ". ($column['COLUMN_DEFAULT'] ? "DEFAULT ". $column['COLUMN_DEFAULT'] : "") .";"
+    );
+  }
