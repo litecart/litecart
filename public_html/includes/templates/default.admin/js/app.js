005f6d0353da4662e27d2a4876761a0b0d361923
timint <info@tim-international.net>
2022-03-03 16:58:20 +0100

    * Fix script path

--- public_html/includes/templates/default.admin/js/app.js
+++ public_html/includes/templates/default.admin/js/app.js
@@ -181,7 +181,7 @@
 // Keep-alive
   var keepAlive = setInterval(function(){
     $.get({
-      url: window._env.platform.url + 'ajax/cart.json',
+      url: window._env.platform.path + 'ajax/cart.json',
       cache: false
     });
   }, 60000);
