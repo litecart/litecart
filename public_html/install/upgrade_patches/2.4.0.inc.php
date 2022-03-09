08765a76af564bb0976a383defc738cca1d76ecb
timint <info@tim-international.net>
2022-03-09 22:55:49 +0100

    * Fix upgrade patch

--- public_html/install/upgrade_patches/2.4.0.inc.php
+++ public_html/install/upgrade_patches/2.4.0.inc.php
@@ -4,12 +4,12 @@
     "SELECT * FROM `information_schema`.COLUMNS
     WHERE TABLE_SCHEMA = '". DB_DATABASE ."'
     AND TABLE_NAME like '". DB_TABLE_PREFIX ."%'
-    AND DATA_TYPE = 'double';"
+    AND DATA_TYPE = 'decimal';"
   );
 
   while ($column = database::fetch($columns_query)) {
     database::query(
       "ALTER TABLE ". $column['TABLE_NAME'] ."
-      CHANGE COLUMN `". $column['COLUMN_NAME'] ."` `". $column['COLUMN_NAME'] ."` ". strtr($column['COLUMN_TYPE'], ['double' => 'float']) ." ". (($column['IS_NULLABLE'] == 'YES') ? "NULL" : "NOT NULL") ." ". ($column['COLUMN_DEFAULT'] ? "DEFAULT ". $column['COLUMN_DEFAULT'] : "") .";"
+      CHANGE COLUMN `". $column['COLUMN_NAME'] ."` `". $column['COLUMN_NAME'] ."` ". strtr($column['COLUMN_TYPE'], ['decimal' => 'float']) ." ". (($column['IS_NULLABLE'] == 'YES') ? "NULL" : "NOT NULL") ." ". ($column['COLUMN_DEFAULT'] ? "DEFAULT ". $column['COLUMN_DEFAULT'] : "") .";"
     );
   }
