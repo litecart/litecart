f4f7d0df1a34971e91c00b79e50c072222f25c70
timint <info@tim-international.net>
2022-03-14 23:56:31 +0100

    * Fix

--- public_html/includes/functions.inc.php
+++ public_html/includes/functions.inc.php
@@ -3,8 +3,8 @@
   define('SQL_DATE', 'Y-m-d');
   define('SQL_DATETIME', 'Y-m-d H:i:s');
 
-// Returns value for variable or falls back to a substituting value on empty(). Similar to $var ?? $fallback
-  function fallback(mixed &$var, mixed $fallback=null):mixed {
-    if (!empty($var)) return $var;
+// Returns value for variable or falls back to a substituting value on nil(). Similar to $var ?? $fallback
+  function fallback(&$var, $fallback=null) {
+    if (!nil($var)) return $var;
     return $fallback;
   }
