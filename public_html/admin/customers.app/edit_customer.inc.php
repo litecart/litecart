16cc28cd6206758773259f8247985b103ea71232
timint <info@tim-international.net>
2022-04-02 04:00:31 +0200

    + Sign in as customer

--- public_html/admin/customers.app/edit_customer.inc.php
+++ public_html/admin/customers.app/edit_customer.inc.php
@@ -17,6 +17,29 @@
   breadcrumbs::add(language::translate('title_customers', 'Customers'), document::link(WS_DIR_ADMIN, ['doc' => 'customers'], ['app']));
   breadcrumbs::add(!empty($customer->data['id']) ? language::translate('title_edit_customer', 'Edit Customer') : language::translate('title_add_new_customer', 'Add New Customer'));
 
+  if (isset($_POST['sign_in'])) {
+
+    try {
+
+      customer::load($_GET['customer_id']);
+
+      session::$data['security.timestamp'] = time();
+      session::regenerate_id();
+
+      notices::add('success', strtr(language::translate('success_logged_in_as_user', 'You are now logged in as %firstname %lastname.'), [
+        '%email' => customer::$data['email'],
+        '%firstname' => customer::$data['firstname'],
+        '%lastname' => customer::$data['lastname'],
+      ]));
+
+      header('Location: '. document::ilink(''));
+      exit;
+
+    } catch (Exception $e) {
+      notices::add('errors', $e->getMessage());
+    }
+  }
+
   if (isset($_POST['save'])) {
 
     try {
@@ -136,6 +159,13 @@
               <?php echo functions::form_draw_text_field('code', true); ?>
             </div>
           </div>
+
+          <?php if (!empty($customer->data['id'])) { ?>
+          <div class="form-group col-md-6">
+            <label></label>
+            <?php echo functions::form_draw_button('sign_in', ['true' => language::translate('text_sign_in_as_customer', 'Sign in as customer')], 'submit'); ?>
+          </div>
+          <?php } ?>
 
           <div class="row">
             <div class="form-group col-md-6">
