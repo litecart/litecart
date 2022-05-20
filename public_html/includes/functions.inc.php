88cb7baf3ae8c3ce1574262cddb0cfa10ff1aa18
timint <info@tim-international.net>
2022-05-20 20:10:02 +0200

    + Add function nil()

--- public_html/includes/functions.inc.php
+++ public_html/includes/functions.inc.php
@@ -3,6 +3,14 @@
   define('SQL_DATE', 'Y-m-d');
   define('SQL_DATETIME', 'Y-m-d H:i:s');
 
+// Checks if variable is not set, null, (bool)false, (int)0, (float)0.00, (string)"", (string)"0", (string)"0.00", or (array)[].
+  function nil(&$var) {
+    if (is_array($var)) { // Recursive
+      foreach ($var as $node) {
+        if (!nil($node)) return !1;
+      }
+    }
+
 // Returns value for variable or falls back to a substituting value on nil(). Similar to $var ?? $fallback
   function fallback(&$var, $fallback=null) {
     if (!nil($var)) return $var;
