3b2dc65ce8044fa0d74c62008dd3feed2b2fa01e
timint <info@tim-international.net>
2022-06-13 19:18:54 +0200

    * Simplify code

--- public_html/includes/functions/func_general.inc.php
+++ public_html/includes/functions/func_general.inc.php
@@ -11,16 +11,6 @@
   }
 
   function general_order_public_checksum($order_id) {
-
     trigger_error(__METHOD__.'() is deprecated. Use reference::order(id)->public_key', E_USER_DEPRECATED);
-
-    $query = database::query(
-      "select public_key from ". DB_TABLE_PREFIX ."orders
-      where id = ". (int)$order_id ."
-      limit 1;"
-    );
-
-    if ($order = database::fetch($query)) {
-      return $order['public_key'];
-    }
+    return reference::order($order_id)->public_key;
   }
