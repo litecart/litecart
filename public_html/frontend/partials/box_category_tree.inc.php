a08218a128be93d7f9343426227d93895d6b1889
timint <info@tim-international.net>
2022-06-27 16:57:15 +0200

    * Category tree adjustments

--- public_html/frontend/partials/box_category_tree.inc.php
+++ public_html/frontend/partials/box_category_tree.inc.php
@@ -4,21 +4,22 @@
   if (cache::capture($box_category_tree_cache_token)) {
 
     if (!empty($_GET['category_id'])) {
-      $category_path = array_keys(reference::category($_GET['category_id'])->path);
-      $parent_id = $category_path[0];
+      $main_category = array_values(reference::category($_GET['category_id'])->path)[0];
+      $trail = array_keys(reference::category($_GET['category_id'])->path);
     } else {
-      $category_path = [];
-      $parent_id = 0;
+      $main_category = false;
+      $trail = [];
     }
 
     $box_category_tree = new ent_view(FS_DIR_TEMPLATE . 'partials/box_category_tree.inc.php');
     $box_category_tree->snippets = [
-      'title' => $parent_id ? reference::category($parent_id)->name : language::translate('title_categories', 'Categories'),
+      'title' => $main_category ? $main_category->name : language::translate('title_categories', 'Categories'),
+      'main_category' => $main_category ? $main_category->id : 0,
       'categories' => [],
-      'category_path' => $category_path,
+      'trail' => $trail,
     ];
 
-    $iterator = function($parent_id, $level) use (&$iterator, &$category_path) {
+    $iterator = function($parent_id) use (&$iterator, &$trail) {
 
       $tree = [];
 
@@ -32,7 +33,7 @@
           'name' => $category['name'],
           'link' => document::ilink('category', ['category_id' => $category['id']], false),
           'active' => (!empty($_GET['category_id']) && $category['id'] == $_GET['category_id']) ? true : false,
-          'opened' => (!empty($category_path) && in_array($category['id'], $category_path)) ? true : false,
+          'opened' => (!empty($trail) && in_array($category['id'], $trail)) ? true : false,
           'subcategories' => [],
         ];
 
@@ -40,10 +41,10 @@
           $tree[$category['id']]['num_products'] = reference::category($category['id'])->num_products;
         }
 
-        if (in_array($category['id'], $category_path)) {
+        if (in_array($category['id'], $trail)) {
           $sub_categories_query = functions::catalog_categories_query($category['id']);
           if (database::num_rows($sub_categories_query)) {
-            $tree[$category['id']]['subcategories'] = $iterator($category['id'], $level+1);
+            $tree[$category['id']]['subcategories'] = $iterator($category['id']);
           }
         }
       }
@@ -51,9 +52,9 @@
       return $tree;
     };
 
-    if ($box_category_tree->snippets['categories'] = $iterator(0, 0)) {
-      echo $box_category_tree;
-    }
+    $box_category_tree->snippets['categories'] = $iterator($main_category ? $main_category->id : 0);
+
+    echo $box_category_tree;
 
     cache::end_capture($box_category_tree_cache_token);
   }
--- public_html/frontend/templates/default/partials/box_category_tree.inc.php
+++ public_html/frontend/templates/default/partials/box_category_tree.inc.php
@@ -1,11 +1,12 @@
 <?php
-  $draw_branch = function($category, &$category_path) use (&$draw_branch) {
+  $draw_branch = function($category) use (&$draw_branch, &$trail) {
     echo '<li class="category-'. $category['id'] . (!empty($category['opened']) ? ' opened' : '') . (!empty($category['active']) ? ' active' : '') .'">' . PHP_EOL
        . '  <a href="'. functions::escape_html($category['link']) .'">'. functions::draw_fonticon(!empty($category['opened']) ? 'fa-angle-down fa-fw float-end' : 'fa-angle-right fa-fw float-end') .' '. $category['name'] . ((settings::get('category_tree_product_count') && $category['num_products']) ? ' <small class="float-end">('. $category['num_products'] .')</small>' : '') .'</a>' . PHP_EOL;
+
     if (!empty($category['subcategories'])) {
       echo '  <ul class="nav nav-stacked nav-pills">' . PHP_EOL;
       foreach ($category['subcategories'] as $subcategory) {
-        echo PHP_EOL . $draw_branch($subcategory, $category_path);
+        echo PHP_EOL . $draw_branch($subcategory);
       }
       echo '  </ul>' . PHP_EOL;
     }
@@ -17,6 +18,6 @@
 
   <ul class="nav nav-stacked nav-pills">
     <li><a href="<?php echo document::href_ilink('categories'); ?>"><?php echo functions::draw_fonticon('fa-angle-left fa-fw'); ?> <?php echo language::translate('title_back', 'Back'); ?></a></li>
-    <?php foreach ($categories as $category) $draw_branch($category, $category_path); ?>
+    <?php foreach ($categories as $category) $draw_branch($category); ?>
   </ul>
 </section>
