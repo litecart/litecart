ed20ea9e51949eb794ed7db2b5b7ebcec32b6dd1
timint <info@tim-international.net>
2022-09-04 23:46:45 +0200

    * Only pass committed lines to PHP lint

--- .git-hooks/pre-commit.d/php_lint_checker.php
+++ .git-hooks/pre-commit.d/php_lint_checker.php
@@ -5,11 +5,19 @@
      . 'Processing PHP lint checker' . PHP_EOL;
 
   $tracked_files = preg_split('#(\r\n?|\n)#', shell_exec('git ls-files'), -1, PREG_SPLIT_NO_EMPTY);
-  $commmitted_files = preg_split('#(\r\n?|\n)#', shell_exec('git diff --cached --name-only 2>&1'), -1, PREG_SPLIT_NO_EMPTY);
+  $committed_files = preg_split('#(\r\n?|\n)#', shell_exec('git diff --cached --name-only 2>&1'), -1, PREG_SPLIT_NO_EMPTY);
 
-  foreach ($commmitted_files as $file) {
+  foreach ($committed_files as $file) {
     if (!in_array($file, $tracked_files)) continue;
     if (!preg_match('#\.php$#', $file)) continue;
-    system('php -l '. $file, $result_code);
+
+      $tmp_file = tempnam(sys_get_temp_dir(), '_blob');
+      shell_exec('git cat-file blob :'. $file .' > '. $tmp_file);
+      $blob = file_get_contents($tmp_file);
+
+      system('php -l '. $file, $result_code);
+
+      unlink($tmp_file);
+
     if (!empty($result_code)) exit($result_code);
   }
