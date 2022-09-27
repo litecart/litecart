e25a9c8265bd258e83cf9c2fb5ba8f00651445d5
timint <info@tim-international.net>
2022-09-27 04:48:43 +0200

    + Add script sources for vMod editor

---
+++ public_html/admin/vmods.app/sources.inc.php
@@ -0,0 +1,38 @@
+<?php
+
+  try {
+
+    if (empty($_GET['pattern'])) {
+      throw new Exception('Missing file');
+    }
+
+    $results = [];
+
+    $skip_list = [
+      '#.*(?<!\.inc\.php)$#',
+      '#^assets/#',
+      '#^index.php$#',
+      '#^includes/app_header.inc.php$#',
+      '#^includes/library/nod_vmod.inc.php$#',
+      '#^install/#',
+    ];
+
+    $files = functions::file_search(FS_DIR_APP . $_GET['pattern'], GLOB_BRACE);
+
+    foreach ($files as $file) {
+      $relative_path = preg_replace('#^'. preg_quote(FS_DIR_APP, '#') .'#', '', $file);
+
+      foreach ($skip_list as $pattern) {
+        if (preg_match($pattern, $relative_path)) continue 2;
+      }
+
+      $results[$relative_path] = file_get_contents($file);
+    }
+
+  } catch (Exception $e) {
+    $results = [];
+  }
+
+  header('Content-Type: application/json');
+  echo json_encode($results, JSON_UNESCAPED_SLASHES);
+  exit;
