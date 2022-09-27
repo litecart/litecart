e0f2c27eb0778858ab8aebc6beca9699dbec4ed6
timint <info@tim-international.net>
2022-09-27 04:47:46 +0200

    * Fix tabs

--- public_html/includes/templates/default.admin/js/app.js
+++ public_html/includes/templates/default.admin/js/app.js
@@ -69,57 +69,30 @@
     }
   });
 
-
 // Tabs (data-toggle="tab")
-+function($) {
-  'use strict';
-  $.fn.Tabs = function(){
-    this.each(function(){
+  $('.nav-tabs').each(function(){
+    if (!$(this).find('.active').length) {
+      $(this).find('[data-toggle="tab"]:first').addClass('active');
+    }
 
-      let self = this;
-
-      this.$element = $(this);
-
-      this.$element.find('[data-toggle="tab"]').each(function(){
-        let $link = $(this);
-
-        $link.on('select', function(){
-          self.$element.find('.active').removeClass('active');
-
-          if ($link.hasClass('nav-link')) {
-            $link.addClass('active');
-          }
-
-          $link.closest('.nav-item').addClass('active');
-
-          $($link.attr('href')).show().siblings().hide();
-        });
-
-        $link.on('click', function(e) {
-          e.preventDefault();
-          history.replaceState(null, null, this.hash);
-          $link.trigger('select');
-        });
-      });
-
-      if (!this.$element.find('.active').length) {
-        this.$element.find('[data-toggle="tab"]').first().select();
-      } else {
-        this.$element.find('[data-toggle="tab"].active').select();
-      }
+    $(this).on('select', '[data-toggle="tab"]', function() {
+      $(this).siblings().removeClass('active');
+      $(this).addClass('active');
+      $($(this).attr('href')).show().siblings().hide();
     });
-  }
 
-  $('.nav-tabs').Tabs();
+    $(this).on('click', '[data-toggle="tab"]', function(e) {
+      e.preventDefault();
+      $(this).trigger('select');
+      history.replaceState({}, '', location.toString().replace(/#.*$/, '') + $(this).attr('href'));
+    });
 
-  if (document.location.hash && document.location.hash.match(/^#tab-/)) {
-    $('[data-toggle="tab"][href="' + document.location.hash +'"]').trigger('select');
-  }
-
-  $(document).on('ajaxcomplete', function(){
-    $('.nav-tabs').Tabs();
+    $(this).find('.active').trigger('select');
   });
-}(jQuery);
+
+  if (document.location.hash != '') {
+    $('a[data-toggle="tab"][href="' + document.location.hash + '"]').click();
+  }
 
 // Toggle Buttons (data-toggle="buttons")
   $('body').on('click', '[data-toggle="buttons"] :checkbox', function(){
