31396304c19f2c1d4fbda2cfe505652ef6d94ec5
timint <info@tim-international.net>
2022-10-02 01:04:56 +0200

    + Add some error documents

---
+++ public_html/includes/templates/default.admin/pages/error_document.inc.php
@@ -0,0 +1,22 @@
+<style>
+.code {
+  font-size: 64px;
+  font-weight: bold;
+}
+.title {
+  font-size: 48px;
+}
+.description {
+  font-size: 24px;
+}
+</style>
+
+<main id="main">
+  {snippet:notices}
+
+  <article class="box text-center">
+    <div class="code">HTTP {snippet:code}</div>
+    <div class="title">{snippet:title}</div>
+    <p class="description">{snippet:description}</p>
+  </article>
+</main>
