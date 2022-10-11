48382c7fbcb65fb53dc79938963280312b996dcb
timint <info@tim-international.net>
2022-10-11 19:51:54 +0200

    * Adjust error logging

--- public_html/includes/library/lib_stats.inc.php
+++ public_html/includes/library/lib_stats.inc.php
@@ -36,8 +36,11 @@
       $page_parse_time = microtime(true) - SCRIPT_TIMESTAMP_START;
 
       if ($page_parse_time > 5) {
-        notices::add('warnings', sprintf(language::translate('text_long_execution_time', 'We apologize for the inconvenience that the server seems temporary overloaded right now.'), number_format($page_parse_time, 1, ',', ' ')));
-        error_log('Warning: Long page execution time '. number_format($page_parse_time, 3, ',', ' ') .' s - '. document::link(), 3, FS_DIR_APP . 'logs/performance.log');
+        if ($page_parse_time > 10) {
+          notices::add('warnings', sprintf(language::translate('text_long_execution_time', 'We apologize for the inconvenience that the server seems temporary overloaded right now.'), number_format($page_parse_time, 1, ',', ' ')));
+        }
+        $log_message = '['. date('Y-m-d H:i:s e').'] Long page execution time '. number_format($page_parse_time, 3, ',', ' ') .' s - '. document::link() . PHP_EOL . PHP_EOL;
+        file_put_contents(FS_DIR_APP . 'logs/performance.log', $log_message, FILE_APPEND);
       }
     }
 
