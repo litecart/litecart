84744c0a4054b521a135d77476e896f1a39dd6ba
timint <info@tim-international.net>
2022-12-16 00:47:31 +0100

    * Fix notice background for default notices

--- public_html/includes/templates/default.admin/views/notices.inc.php
+++ public_html/includes/templates/default.admin/views/notices.inc.php
@@ -10,7 +10,7 @@
           echo '<div class="alert alert-warning"><a href="#" class="close" data-dismiss="alert">&times;</a>' . functions::draw_fonticon('fa-exclamation-triangle') . ' ' . $notice .'</div>' . PHP_EOL;
           break;
         case 'notices':
-          echo '<div class="alert alert-info"><a href="#" class="close" data-dismiss="alert">&times;</a>' . functions::draw_fonticon('fa-info-circle') . ' ' . $notice .'</div>' . PHP_EOL;
+          echo '<div class="alert alert-default"><a href="#" class="close" data-dismiss="alert">&times;</a>' . functions::draw_fonticon('fa-info-circle') . ' ' . $notice .'</div>' . PHP_EOL;
           break;
         case 'success':
           echo '<div class="alert alert-success"><a href="#" class="close" data-dismiss="alert">&times;</a>' .functions::draw_fonticon('fa-check-circle') . ' ' . $notice .'</div>' . PHP_EOL;
