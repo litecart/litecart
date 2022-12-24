3eaa313a9ca2a67f1fcff3defbbe780849e4337b
Arshid <arshidkv12@gmail.com>
2022-12-24 12:17:09 +0530

    Update lib_form.inc.php

--- public_html/includes/library/lib_form.inc.php
+++ public_html/includes/library/lib_form.inc.php
@@ -14,16 +14,17 @@
         }
 
         if ($unicoded_content) {
-          function utf8_decode_recursive($input) {
-            $return = [];
-            foreach ($input as $key => $val) {
-              if (is_array($val)) $return[$key] = utf8_decode_recursive($val);
-              else $return[$key] = utf8_decode($val);
-            }
-            return $return;
-          }
           $_POST = utf8_decode_recursive($_POST);
         }
       }
     }
   }
+
+function utf8_decode_recursive($input) {
+    $return = [];
+    foreach ($input as $key => $val) {
+      if (is_array($val)) $return[$key] = utf8_decode_recursive($val);
+      else $return[$key] = utf8_decode($val);
+    }
+    return $return;
+}
