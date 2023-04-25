a1d9457f97c497b3a010b7c8e9812e561257a0cd
timint <info@tim-international.net>
2023-04-25 20:17:11 +0200

    + Show categories on the start page

--- public_html/includes/templates/default.catalog/pages/index.inc.php
+++ public_html/includes/templates/default.catalog/pages/index.inc.php
@@ -4,6 +4,8 @@
 
     <?php include vmod::check(FS_DIR_APP . 'includes/boxes/box_slides.inc.php'); ?>
 
+    <?php include vmod::check(FS_DIR_APP . 'includes/boxes/box_categories.inc.php'); ?>
+
     <?php include vmod::check(FS_DIR_APP . 'includes/boxes/box_campaign_products.inc.php'); ?>
 
     <?php include vmod::check(FS_DIR_APP . 'includes/boxes/box_popular_products.inc.php'); ?>
