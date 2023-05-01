44310ab552c6b2031400751510813249fd5356cb
timint <info@tim-international.net>
2023-05-01 16:59:20 +0200

    * Convert all time zone for all timestamps

---
+++ public_html/install/upgrade_patches/2.6.0.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+  // Get store timezone
+  $setting_query = database::query(
+    "SELECT * FROM ". DB_TABLE_PREFIX ."settings
+    WHERE `key` = 'store_timezone'
+    LIMIT 1;"
+  );
+
+  if ($timezone = database::fetch($setting_query, 'value')) {
+
+    $datetime = new \DateTime('now', new \DateTimezone($timezone));
+
+  // Get all timestamp columns in database
+    $columns_query = database::query(
+      "SELECT * FROM information_schema.COLUMNS
+      WHERE TABLE_SCHEMA = '". database::input(DB_DATABASE) ."'
+      AND COLUMN_TYPE = 'timestamp';"
+    );
+
+    while ($column = database::fetch($columns_query)) {
+
+    // Convert timestamps for column
+      database::query(
+        "UPDATE ". $column['TABLE_NAME'] ."
+        SET `". $column['COLUMN_NAME'] ."` = CONVERT_TZ(`". $column['COLUMN_NAME'] ."`, @@GLOBAL.time_zone, '". $timezone->format('P') ."');"
+      );
+    }
+  }
