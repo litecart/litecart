debf773e1a5b22aa3cdca9fca6c6d1c759c59fe4
timint <info@tim-international.net>
2023-05-01 13:31:35 +0200

    * Fix merge

--- public_html/includes/templates/default.catalog/views/listing_category.inc.php
+++ public_html/includes/templates/default.catalog/views/listing_category.inc.php
@@ -3,11 +3,7 @@
     <img class="img-responsive hidden-xs hidden-sm" src="<?php echo document::href_rlink(FS_DIR_STORAGE . $image['thumbnail']); ?>" style="aspect-ratio: <?php echo $image['ratio']; ?>;" alt="" />
 
     <div class="caption">
-<<<<<<< HEAD
       <div class="name"><?php echo $name; ?></div>
-=======
-      <h3 class="name"><?php echo $name; ?></h3>
->>>>>>> 9dfdc20218adb032ffbb80c70c93f6b5878d214a
       <div class="short-description"><?php echo $short_description; ?></div>
     </div>
   </a>
