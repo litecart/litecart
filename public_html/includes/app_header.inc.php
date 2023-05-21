bc0d235a0fcfa81c4bd2dfae18f74a38261061ff
timint <info@tim-international.net>
2023-05-21 13:16:17 +0200

    * Bump version

--- public_html/includes/app_header.inc.php
+++ public_html/includes/app_header.inc.php
@@ -1,6 +1,6 @@
 <?php
   define('PLATFORM_NAME', 'LiteCart');
-  define('PLATFORM_VERSION', '2.5.3');
+  define('PLATFORM_VERSION', '2.5.4');
   define('SCRIPT_TIMESTAMP_START', microtime(true));
 
 // Start redirecting output to the output buffer
