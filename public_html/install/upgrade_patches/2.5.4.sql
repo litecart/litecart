910e883b4a69be30b7969e8e6b9d9ebe376cb567
timint <info@tim-international.net>
2023-06-07 13:30:39 +0200

    * Fix upgrade patch

--- public_html/install/upgrade_patches/2.5.4.sql
+++ public_html/install/upgrade_patches/2.5.4.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `lc_order_statuses`
-CHANGE COLUMN `state` ENUM('','created','on_hold','ready','delayed','processing','completed','dispatched','in_transit','delivered','returning','returned','cancelled','fraud','other') NOT NULL DEFAULT '' AFTER `id`;
+CHANGE COLUMN `state` `state` ENUM('','created','on_hold','ready','delayed','processing','completed','dispatched','in_transit','delivered','returning','returned','cancelled','fraud','other') NOT NULL DEFAULT '' AFTER `id`;
 -- --------------------------------------------------------
 UPDATE `lc_order_statuses`
 SET `state` = 'completed'
