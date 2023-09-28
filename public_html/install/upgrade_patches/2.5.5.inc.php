59a347f85337971764d725ef8188e837e4ddeaf4
timint <info@tim-international.net>
2023-09-28 15:53:01 +0200

    - Remove orphan files

---
+++ public_html/install/upgrade_patches/2.5.5.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+// Delete some files
+  perform_action('delete', [
+    FS_DIR_APP . 'includes/boxes/box_region.inc.php',
+    FS_DIR_APP . 'includes/templates/default.catalog/views/box_region.inc.php',
+  ]);
