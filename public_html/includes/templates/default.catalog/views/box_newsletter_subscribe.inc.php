257332aa4e8905c4a8316d68c17d2fba294564eb
timint <info@tim-international.net>
2023-10-14 16:57:13 +0200

    + Add CAPTCHA and a dedicated page for newsletter subscriptions

--- public_html/includes/templates/default.catalog/views/box_newsletter_subscribe.inc.php
+++ public_html/includes/templates/default.catalog/views/box_newsletter_subscribe.inc.php
@@ -1,32 +1,30 @@
-<style>
-#box-newsletter-subscribe .form-group {
-  max-width: 400px;
-  margin: 0 auto;
-}
-</style>
-
 <section id="box-newsletter-subscribe" class="card text-center">
 
-  <div class="card-header">
-    <h2 class="card-title" style="margin-top: 0;"><?php echo language::translate('box_newsletter_subscribe:title', 'Subscribe to our newsletter!'); ?></h2>
-  </div>
-
   <div class="card-body">
-    <?php echo functions::form_draw_form_begin('newsletter_subscribe_form', 'post'); ?>
 
-      <p><?php echo language::translate('box_newsletter_subscribe:description', 'Get the latest news and offers straight to your inbox. Subscribe now.'); ?></p>
+    <h2><?php echo language::translate('box_newsletter_subscribe:title', 'Subscribe to our newsletter!'); ?></h2>
 
-      <?php if ($privacy_policy_link) { ?>
-      <p><?php echo strtr(language::translate('description_subscribing_to_newsletter_accepts_privacy_policy', 'By subscribing to our newsletter you are accepting our <a href="%link">privacy policy</a>.'), ['%link' => functions::escape_html($privacy_policy_link)]); ?></p>
-      <?php } ?>
+    <p><?php echo language::translate('box_newsletter_subscribe:description', 'Get the latest news and offers straight to your inbox. Subscribe now.'); ?></p>
 
-      <div class="form-group">
+    <?php echo functions::form_draw_form_begin('newsletter_subscribe_form', 'post', document::ilink('newsletter')); ?>
+
+      <div class="form-group" style="max-width: 400px; margin: 0 auto;">
         <div class="input-group">
           <?php echo functions::form_draw_text_field('email', true, 'placeholder="your@email.com" required'); ?>
-          <?php echo functions::form_draw_button('subscribe', language::translate('title_subscripbe', 'Subscribe')); ?>
+          <?php echo functions::form_draw_button('subscribe', language::translate('title_subscribe', 'Subscribe')); ?>
         </div>
       </div>
 
     <?php echo functions::form_draw_form_end(); ?>
   </div>
 </section>
+
+<script>
+  $('form[name="newsletter_subscribe_form"]').submit(function(e){
+    e.preventDefault();
+    $.featherlight('<?php echo document::ilink('newsletter'); ?>?email='+ $(this).find('input[name="email"]').val() +' #box-newsletter-subscribe', {
+      "seamless": true,
+      "width": "640px"
+    });
+  })
+</script>
---
+++ public_html/pages/newsletter.inc.php
@@ -0,0 +1,99 @@
+<?php
+
+  if (!empty($_POST['subscribe'])) {
+
+    try {
+
+      if (empty($_POST['email'])) throw new Exception(language::translate('error_missing_email', 'You must provide an email address'));
+
+      if (settings::get('captcha_enabled')) {
+        $captcha = functions::captcha_get('newsletter_subscribe');
+        if (!$captcha || empty($_POST['captcha']) || $captcha != $_POST['captcha']) {
+          throw new Exception(language::translate('error_invalid_captcha', 'Invalid CAPTCHA given'));
+        }
+      }
+
+      $_POST['email'] = strtolower($_POST['email']);
+
+    // Collect scraps
+      if (empty(customer::$data['id'])) {
+        customer::$data = array_replace(customer::$data, array_intersect_key(array_filter(array_diff_key($_POST, array_flip(['id']))), customer::$data));
+      }
+
+      database::query(
+        "insert ignore into ". DB_TABLE_PREFIX ."newsletter_recipients
+        (email, firstname, lastname, client_ip, date_created)
+        values ('". database::input(mb_strtolower($_POST['email'])) ."', '". (!empty($_POST['firstname']) ? database::input($_POST['firstname']) : '') ."', '". (!empty($_POST['lastname']) ? database::input($_POST['lastname']) : '') ."', '". database::input($_SERVER['REMOTE_ADDR']) ."', '". date('Y-m-d H:i:s') ."');"
+      );
+
+      $aliases = [
+        '%ipaddress' => $_SERVER['REMOTE_ADDR'],
+        '%hostname' => gethostbyaddr($_SERVER['REMOTE_ADDR']),
+        '%datetime' => language::strftime(language::$selected['format_datetime']),
+        '%store_name' => settings::get('store_name'),
+        '%store_link' => document::ilink(),
+        '%unsubscribe_link' => document::ilink('newsletter', ['email' => $_POST['email']]),
+      ];
+
+      $message = strtr(
+        language::translate('email_body:newsletter_subscription_confirmation', "This is a confirmation that we have recieved your request to subscribe to our newsletter.\r\n\r\nIf this was not you, click the link to unsubscribe: %unsubscribe_link") . "\r\n\r\n" .
+        language::translate('title_ip_address', 'IP Address') . ': %ipaddress' . "\r\n" .
+        language::translate('title_date', 'Date') . ': %datetime',
+      $aliases);
+
+      $email = new ent_email();
+      $email->add_recipient($_POST['email'])
+          ->set_subject(language::translate('email_subject:newsletter_subscription_confirmation', 'Confirmation of newsletter subscription'))
+          ->add_body($message)
+          ->send();
+
+      notices::add('success', language::translate('success_subscribed_to_newsletter', 'Thank you for subscribing to our newsletter'));
+      header('Location: '. $_SERVER['REQUEST_URI']);
+      exit;
+
+    } catch (Exception $e) {
+      notices::add('errors', $e->getMessage());
+    }
+  }
+
+  if (!empty($_POST['unsubscribe'])) {
+
+    try {
+
+      if (empty($_POST['email'])) throw new Exception(language::translate('error_missing_email', 'You must provide an email address'));
+
+      $_POST['email'] = strtolower($_POST['email']);
+
+    // Collect scraps
+      if (empty(customer::$data['id'])) {
+        customer::$data = array_replace(customer::$data, array_intersect_key(array_filter(array_diff_key($_POST, array_flip(['id']))), customer::$data));
+      }
+
+      database::query(
+        "delete from ". DB_TABLE_PREFIX ."newsletter_recipients
+        where email like '". addcslashes(database::input($_POST['email']), '%_') ."';"
+      );
+
+      notices::add('success', language::translate('success_unsubscribed_from_newsletter', 'You have been unsubscribed from the newsletter'));
+      header('Location: '. $_SERVER['REQUEST_URI']);
+      exit;
+
+    } catch (Exception $e) {
+      notices::add('errors', $e->getMessage());
+    }
+  }
+
+  $_page = new ent_view();
+
+  $_page->snippets = [
+    'consent' => null,
+  ];
+
+  if ($privacy_policy_id = settings::get('privacy_policy')) {
+    $aliases = [
+      '%privacy_policy_link' => document::href_ilink('information', ['page_id' => $privacy_policy_id]),
+    ];
+    $_page->snippets['consent'] = strtr(language::translate('consent:privacy_policy', 'I have read the <a href="%privacy_policy_link" target="_blank">Privacy Policy</a> and I consent.'), $aliases);
+  }
+
+  echo $_page->stitch('pages/newsletter');
