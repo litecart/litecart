4ef61fdb2939000f97da792cce34a04e6e6b9c3d
timint <info@tim-international.net>
2023-10-14 23:57:22 +0200

    * Append commit author id

--- public_html/install/upgrade_patches/2.6.0.sql
+++ public_html/install/upgrade_patches/2.6.0.sql
@@ -1,2 +1,5 @@
+ALTER TABLE `lc_orders_comments`
+ADD COLUMN `author_id` INT(11) NOT NULL DEFAULT '0' AFTER `order_id`;
+-- --------------------------------------------------------
 UPDATE `lc_pages`
 SET dock = CAST(REGEXP_REPLACE(dock, ',.*$', '') AS CHAR);
