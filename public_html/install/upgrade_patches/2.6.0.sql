8feb89c1e325928611e346ef522ca79f69cdd825
timint <info@tim-international.net>
2023-10-19 01:48:49 +0200

    + Add support for Lazy Loading images

--- public_html/install/upgrade_patches/2.6.0.sql
+++ public_html/install/upgrade_patches/2.6.0.sql
@@ -3,3 +3,6 @@ ADD COLUMN `author_id` INT(11) NOT NULL DEFAULT '0' AFTER `order_id`;
 -- --------------------------------------------------------
 UPDATE `lc_pages`
 SET dock = CAST(REGEXP_REPLACE(dock, ',.*$', '') AS CHAR);
+-- --------------------------------------------------------
+INSERT INTO `lc_settings`
+(`setting_group_key`, `key`, `title`, `description`, `value`, `function`, `priority`, `date_updated`, `date_created`)
