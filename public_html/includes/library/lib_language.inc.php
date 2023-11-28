2b5e3c2ddf1fea72f19b0cfc9f0fd7cb42e318d6
timint <info@tim-international.net>
2023-11-28 21:52:14 +0100

    * Fix PHP scalar error

--- public_html/includes/library/lib_language.inc.php
+++ public_html/includes/library/lib_language.inc.php
@@ -11,8 +11,17 @@
 
     // Bind selected language to session
       if (preg_match('#^'. preg_quote(WS_DIR_APP . BACKEND_ALIAS, '#') .'/#', $_SERVER['REQUEST_URI'])) {
-        if (!isset(session::$data['backend']['language'])) session::$data['backend']['language'] = [];
+
+        if (!isset(session::$data['backend'])) {
+          session::$data['backend'] = [];
+        }
+
+        if (!isset(session::$data['backend']['language'])) {
+          session::$data['backend']['language'] = [];
+        }
+
         self::$selected = &session::$data['backend']['language'];
+
       } else {
         if (!isset(session::$data['language'])) session::$data['language'] = [];
         self::$selected = &session::$data['language'];
