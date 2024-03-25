ef9b23862eaca491b39a0ee274bd107315e2f791
timint <info@tim-international.net>
2024-03-25 02:27:54 +0100

    + Shared carousel view component

---
+++ public_html/frontend/templates/default/partials/carousel.inc.php
@@ -0,0 +1,19 @@
+<div class="carousel slide" data-ride="carousel">
+
+  <div class="carousel-inner">
+    <?php foreach ($items as $key => $item) echo '<div class="item'. (($key == 0) ? ' active' : '') .'">'. $item. '</div>'; ?>
+  </div>
+
+  <?php if (count($items) > 1) { ?>
+  <ol class="carousel-indicators">
+    <?php foreach ($items as $key => $item) echo '<li data-target="#box-leaderboard" data-slide-to="'. $key .'"'. (($key == 0) ? ' class="active"' : '') .'></li>'; ?>
+  </ol>
+
+  <a class="left carousel-control" href="#" data-slide="prev">
+    <span class="icon-prev"><?php echo functions::draw_fonticon('fa-chevron-left'); ?></span>
+  </a>
+  <a class="right carousel-control" href="#" data-slide="next">
+    <span class="icon-next"><?php echo functions::draw_fonticon('fa-chevron-right'); ?></span>
+  </a>
+  <?php } ?>
+  </div>
