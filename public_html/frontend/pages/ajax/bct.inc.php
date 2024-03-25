c1ae2f9d03ddc9a3735172b3df85600021dfb0d9
timint <info@tim-international.net>
2024-03-25 02:27:15 +0100

    + Banner click tracker

---
+++ public_html/frontend/pages/ajax/bct.inc.php
@@ -0,0 +1,20 @@
+<?php
+
+  try {
+
+    if (empty($_POST['banner_id'])) {
+      throw new Exception('Missing banner_id');
+    }
+
+    database::query(
+      "update ". DB_TABLE_PREFIX ."banners
+      set total_clicks = total_clicks + 1
+      where status
+      and id = ". (int)$_POST['banner_id'] ."
+      limit 1;"
+    );
+
+  } catch (Exception $e) {
+    http_response_code(500);
+    die('Error: '. $e->getMessage());
+  }
