6c174d71b72d79f34d17d2d60235db81b781e58f
timint <info@tim-international.net>
2024-03-28 19:37:07 +0100

    * Allow access to manifest.json during Development or Maintenance Mode

--- public_html/index.php
+++ public_html/index.php
@@ -18,10 +18,12 @@
 // Development Mode
   if (settings::get('development_mode')) {
     if (empty(user::$data['id']) && !preg_match('#^'. preg_quote(WS_DIR_ADMIN, '#') .'#', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
-      http_response_code(403);
-      include vmod::check(FS_DIR_APP . 'pages/development_mode.inc.php');
-      require_once vmod::check(FS_DIR_APP . 'includes/app_footer.inc.php');
-      exit;
+      if (!in_array(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), [WS_DIR_APP . 'manifest.json'])) {
+        http_response_code(403);
+        include vmod::check(FS_DIR_APP . 'pages/development_mode.inc.php');
+        require_once vmod::check(FS_DIR_APP . 'includes/app_footer.inc.php');
+        exit;
+      }
     }
   }
 
@@ -34,10 +36,12 @@
         '%link' => document::href_ilink('maintenance_mode'),
       ]), 'maintenance_mode');
     } else {
-      http_response_code(503);
-      include vmod::check(FS_DIR_APP . 'pages/maintenance_mode.inc.php');
-      require_once vmod::check(FS_DIR_APP . 'includes/app_footer.inc.php');
-      exit;
+      if (!in_array(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), [WS_DIR_APP . 'manifest.json'])) {
+        http_response_code(503);
+        include vmod::check(FS_DIR_APP . 'pages/maintenance_mode.inc.php');
+        require_once vmod::check(FS_DIR_APP . 'includes/app_footer.inc.php');
+        exit;
+      }
     }
   }
 
