999e1cf88ddeba9443b161385c7a29de783d5a2e
timint <info@tim-international.net>
2024-07-14 18:12:23 +0200

    + Add mappings for more deprecated functions

--- public_html/includes/nodes/nod_functions.inc.php
+++ public_html/includes/nodes/nod_functions.inc.php
@@ -6,21 +6,24 @@
 
     // Renamed functions
       $search_replace = [
+        '#^form_draw_currency_field$#' => 'form_select_currency',
         '#^form_draw_customer_field$#' => 'form_select_customer',
         '#^form_draw_product_field$#' => 'form_select_product',
         '#^form_draw_users_list$#' => 'form_select_administrator',
+        '#^form_draw_select_field$#' => 'form_select',
         '#^form_draw_weight_classes_list$#' => 'form_select_weight_unit',
         '#^form_draw_length_classes_list$#' => 'form_select_length_unit',
         '#^form_draw_volume_classes_list$#' => 'form_select_volume_unit',
         '#^form_draw_order_status_list$#' => 'form_select_order_status',
-        '#^form_draw_(.*?)ies_list$#' => 'form_select_$y',
-        '#^form_draw_(.*?)ses_list$#' => 'form_select_$s',
+        '#^form_draw_(.*?)ies_list$#' => 'form_select_$1y',
+        '#^form_draw_(.*?)ses_list$#' => 'form_select_$1s',
         '#^form_draw_(.*?)s_list$#' => 'form_select_$1',
-        '#^form_draw_form_(.*?)$#' => 'form_$2',
-        '#^form_draw_(.*?)_field$#' => 'form_input_$2',
+        '#^form_draw_form_(.*?)$#' => 'form_$1',
+        '#^form_draw_(.*?)_field$#' => 'form_input_$1',
         '#^form_draw_checkbox$#' => 'form_checkbox',
         '#^form_draw_radio_button$#' => 'form_radio_button',
         '#^form_draw_textarea$#' => 'form_input_textarea',
+        '#^form_draw_button$#' => 'form_button',
       ];
 
       foreach ($search_replace as $search => $replace) {
