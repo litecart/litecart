d16cdda55398b933b8dca8db190d56f144f4b324
timint <info@tim-international.net>
2024-07-26 01:02:18 +0200

    + Add missing aspect ratio 9:16

--- public_html/install/upgrade_patches/2.5.6.sql
+++ public_html/install/upgrade_patches/2.5.6.sql
@@ -2,3 +2,7 @@ UPDATE `lc_modules`
 SET `settings` = REPLACE(settings, 'report_frequency', 'frequency')
 WHERE `key` = 'job_error_reporter'
 LIMIT 1;
+-- --------------------------------------------------------
+UPDATE `lc_settings`
+SET `function` = 'select("1:1","2:3","3:2","3:4","4:3","16:9","9:16")'
+WHERE `key` LIKE '%_image_ratio';
