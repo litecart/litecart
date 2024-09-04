b849c9a2932e867123c50a1ebd0452ffa0e52dc9
timint <info@tim-international.net>
2024-09-04 21:54:53 +0200

    * Fix user.date_expire_sessions

--- public_html/install/upgrade_patches/2.5.6.sql
+++ public_html/install/upgrade_patches/2.5.6.sql
@@ -6,3 +6,6 @@ LIMIT 1;
 UPDATE `lc_settings`
 SET `function` = 'select("1:1","2:3","3:2","3:4","4:3","16:9","9:16")'
 WHERE `key` LIKE '%_image_ratio';
+-- --------------------------------------------------------
+ALTER TABLE `lc_users`
+ADD COLUMN IF NOT EXISTS `date_expire_sessions` TIMESTAMP NULL AFTER `date_login`;
