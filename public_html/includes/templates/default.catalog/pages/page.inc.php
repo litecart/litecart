b3df6f1ce86bbf2d33c4239529de26dc7c9f45f3
timint <info@tim-international.net>
2024-09-10 23:20:43 +0200

    + Add missing files

---
+++ public_html/includes/templates/default.catalog/pages/page.inc.php
@@ -0,0 +1,11 @@
+<div class="fourteen-forty">
+  <main id="content">
+    {snippet:breadcrumbs}
+    {snippet:notices}
+
+    <section id="box-page">
+      <?php echo $content; ?>
+    </section>
+
+  </main>
+</div>
