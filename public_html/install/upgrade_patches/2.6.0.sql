6a3c6cd122ae703262e4eb949c8c0e74f1f1f090
timint <info@tim-international.net>
2024-10-03 19:12:04 +0200

    * Set lazy loading enabled by default (because why not)

--- public_html/install/upgrade_patches/2.6.0.sql
+++ public_html/install/upgrade_patches/2.6.0.sql
@@ -15,7 +15,7 @@ SET dock = CAST(REGEXP_REPLACE(dock, ',.*$', '') AS CHAR);
 -- -----
 INSERT INTO `lc_settings`
 (`setting_group_key`, `type`, `key`, `title`, `description`, `value`, `function`, `priority`, `date_updated`, `date_created`) VALUES
-('images', 'global', 'image_lazyload', 'Image Lazy Loading', 'Tells the browser to load images on the pages first when they are scrolled down to.', '0', 'toggle("e/d")', 45, NOW(), NOW()),
+('images', 'global', 'image_lazyload', 'Image Lazy Loading', 'Tells the browser to load images on the pages first when they are scrolled down to.', '1', 'toggle("e/d")', 45, NOW(), NOW()),
 ('images', 'global', 'avif_enabled', 'AVIF Enabled', 'Use AVIF images if supported by the browser.', '0', 'toggle("e/d")', 44, NOW(), NOW());
 -- -----
 INSERT INTO `lc_settings` (`setting_group_key`, `type`, `title`, `description`, `key`, `value`, `function`, `priority`, `date_updated`, `date_created`) VALUES
