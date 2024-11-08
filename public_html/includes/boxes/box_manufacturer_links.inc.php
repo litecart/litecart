4f1b491f5c5d084468345fc093cacb5d5c64a0ca
timint <info@tim-international.net>
2024-11-08 12:08:33 +0100

    * Fix commit

--- public_html/includes/boxes/box_manufacturer_links.inc.php
+++ public_html/includes/boxes/box_manufacturer_links.inc.php
@@ -1,21 +1,18 @@
 <?php
 
   $box_manufacturer_links = new ent_view();
-  
+
   $box_manufacturer_links_cache_token = cache::token('box_manufacturer_links', ['language'], 'file');
   if (!$box_manufacturer_links->snippets['manufacturers'] = cache::get($box_manufacturer_links_cache_token)) {
 
     $box_manufacturer_links->snippets['manufacturers'] = [];
-      
+
     $manufacturers_query = database::query(
       "select a.id, a.name, a.date_created from ". DB_TABLE_PREFIX ."manufacturers a
       left join ". DB_TABLE_PREFIX ."manufacturers_info ai on (a.id = ai.manufacturer_id and ai.language_code = '". database::input(language::$selected['code']) ."')
       where status
       order by a.name;"
     );
-
-
-
 
     while ($manufacturer = database::fetch($manufacturers_query)) {
       $box_manufacturer_links->snippets['manufacturers'][] = [
@@ -31,7 +28,7 @@
   }
 
   if (!empty($_GET['manufacturer_id'])) {
-    foreach ($box_manufacturers_links->snippets['manufacturers'] as $key => $manufacturer) {
+    foreach ($box_manufacturer_links->snippets['manufacturers'] as $key => $manufacturer) {
       if ($manufacturer['id'] == $_GET['manufacturer_id']) {
         $box_manufacturer_links->snippets['manufacturers'][$key]['active'] = true;
         break;
