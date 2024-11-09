2d5213a4e9b14bdf29b15573cbc94db94cf53df4
timint <info@tim-international.net>
2024-11-09 13:24:51 +0100

    * Update error handler

--- public_html/includes/error_handler.inc.php
+++ public_html/includes/error_handler.inc.php
@@ -6,27 +6,28 @@
 
     $errfile = preg_replace('#^'. preg_quote(FS_DIR_APP, '#') .'#', '~/', str_replace('\\', '/', $errfile));
 
-    switch($errno) {
+    $output = [];
 
+    switch ($errno) {
       case E_STRICT:
-        $output = "<strong>Strict:</strong> ". htmlspecialchars($errstr) ." in <strong>$errfile</strong> on line <strong>$errline</strong><br>" . PHP_EOL;
+        $output[] = "<strong>Strict:</strong> ". htmlspecialchars($errstr) ." in <strong>$errfile</strong> on line <strong>$errline</strong>";
         break;
 
       case E_NOTICE:
       case E_USER_NOTICE:
-        $output = "<strong>Notice:</strong> ". htmlspecialchars($errstr) ." in <strong>$errfile</strong> on line <strong>$errline</strong><br>" . PHP_EOL;
+        $output[] = "<strong>Notice:</strong> ". htmlspecialchars($errstr) ." in <strong>$errfile</strong> on line <strong>$errline</strong>";
         break;
 
       case E_WARNING:
       case E_USER_WARNING:
       case E_COMPILE_WARNING:
       case E_RECOVERABLE_ERROR:
-        $output = "<strong>Warning:</strong> ". htmlspecialchars($errstr) ." in <strong>$errfile</strong> on line <strong>$errline</strong><br>" . PHP_EOL;
+        $output[] = "<strong>Warning:</strong> ". htmlspecialchars($errstr) ." in <strong>$errfile</strong> on line <strong>$errline</strong>";
         break;
 
       case E_DEPRECATED:
       case E_USER_DEPRECATED:
-        $output = "<strong>Deprecated:</strong> ". htmlspecialchars($errstr) ." in <strong>$errfile</strong> on line <strong>$errline</strong><br>" . PHP_EOL;
+        $output[] = "<strong>Deprecated:</strong> ". htmlspecialchars($errstr) ." in <strong>$errfile</strong> on line <strong>$errline</strong>";
         break;
 
       case E_PARSE:
@@ -34,45 +35,62 @@
       case E_CORE_ERROR:
       case E_COMPILE_ERROR:
       case E_USER_ERROR:
-        $output = "<strong>Fatal error:</strong> ". htmlspecialchars($errstr) ." in <strong>$errfile</strong> on line <strong>$errline</strong><br>" . PHP_EOL;
+        $output[] = "<strong>Fatal error:</strong> ". htmlspecialchars($errstr) ." in <strong>$errfile</strong> on line <strong>$errline</strong>";
         break;
 
       default:
-        $output = "<strong>Fatal error:</strong> ". htmlspecialchars($errstr) ." in <strong>$errfile</strong> on line <strong>$errline</strong><br>" . PHP_EOL;
+        $output[] = "<strong>Fatal error:</strong> ". htmlspecialchars($errstr) ." in <strong>$errfile</strong> on line <strong>$errline</strong>";
         break;
     }
 
-    $backtrace_output = '';
-    $backtraces = array_slice(debug_backtrace(), 2);
+    if ($backtraces = debug_backtrace()) {
 
-    if (!empty($backtraces)) {
+    // Remove self from backtrace
+      array_shift($backtraces);
+
+    // Extract trace from exception_handler
+      if (!empty($backtraces[0]['function']) && $backtraces[0]['function'] == 'exception_handler') {
+        $backtraces = array_slice($backtraces[0]['args'][0]->getTrace(), 1);
+      }
+
       foreach ($backtraces as $backtrace) {
         if (empty($backtrace['file'])) continue;
         $backtrace['file'] = preg_replace('#^'. preg_quote(FS_DIR_APP, '#') .'#', '~/', str_replace('\\', '/', $backtrace['file']));
-        $backtrace_output .= " ← <strong>{$backtrace['file']}</strong> on line <strong>{$backtrace['line']}</strong> in <strong>{$backtrace['function']}()</strong><br>" . PHP_EOL;
+        $output[] = " → <strong>$backtrace[file]</strong> on line <strong>$backtrace[line]</strong> in <strong>$backtrace[function]()</strong>";
       }
     }
 
+  // Display errors
     if (filter_var(ini_get('display_errors'), FILTER_VALIDATE_BOOLEAN)) {
-      if (!filter_var(ini_get('html_errors'), FILTER_VALIDATE_BOOLEAN) || $_SERVER['SERVER_SOFTWARE'] == 'CLI') {
-        $stripped_output = strip_tags($output . (isset($_GET['debug']) ? $backtrace_output : ''));
-        $stripped_output = html_entity_decode($stripped_output);
-        echo $stripped_output;
+      if (filter_var(ini_get('html_errors'), FILTER_VALIDATE_BOOLEAN) && $_SERVER['SERVER_SOFTWARE'] != 'CLI') {
+        echo isset($_GET['debug']) ? implode('<br>'.PHP_EOL, $output) : $output[0]; // HTML
       } else {
-        echo $output . (isset($_GET['debug']) ? $backtrace_output : '');
+        echo html_entity_decode(strip_tags(
+          isset($_GET['debug']) ? implode('<br>'.PHP_EOL, $output) : $output[0] // Plain text
+        ));
       }
     }
 
     if (filter_var(ini_get('log_errors'), FILTER_VALIDATE_BOOLEAN)) {
-      error_log(implode(PHP_EOL, array_filter([
-        strip_tags($output . $backtrace_output),
+
+      if ($_SERVER['SERVER_SOFTWARE'] == 'CLI') {
+        $output[] = 'Command: '. implode(' ', $GLOBALS['argv']);
+      } else {
+        $output = array_merge($output, array_filter([
         'Request: '. $_SERVER['REQUEST_METHOD'] .' '. $_SERVER['REQUEST_URI'] .' '. $_SERVER['SERVER_PROTOCOL'],
         'Host: '. $_SERVER['HTTP_HOST'],
         'Client: '. $_SERVER['REMOTE_ADDR'] .' ('. gethostbyaddr($_SERVER['REMOTE_ADDR']) .')',
         'User Agent: '. $_SERVER['HTTP_USER_AGENT'],
-        'Elapsed Time: '. number_format((microtime(true) - SCRIPT_START_TIME) * 1000, 0, '.', ' ') .' ms',
         !empty($_SERVER['HTTP_REFERER']) ? 'Referer: '. $_SERVER['HTTP_REFERER'] : '',
-      ])));
+        ]));
+      }
+      
+      $output[] = 'Elapsed Time: '. number_format((microtime(true) - SCRIPT_TIMESTAMP_START) * 1000, 0, '.', ' ') .' ms';
+      $output[] = 'Platform: '. PLATFORM_NAME .'/'. PLATFORM_VERSION;
+
+      error_log(html_entity_decode(strip_tags(
+      	implode(PHP_EOL, $output))) . PHP_EOL
+      );
     }
 
     if (in_array($errno, [E_PARSE, E_ERROR, E_COMPILE_ERROR, E_CORE_ERROR, E_USER_ERROR])) {
