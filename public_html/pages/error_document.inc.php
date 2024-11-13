8306fb836912aa4e312f4282488dfbd447122b64
timint <info@tim-international.net>
2024-11-13 13:43:05 +0100

    * Minor adjustments to error document

--- public_html/pages/error_document.inc.php
+++ public_html/pages/error_document.inc.php
@@ -1,51 +1,62 @@
 <?php
 
+  header('X-Robots-Tag: noindex');
+  document::$snippets['head_tags']['noindex'] = '<meta name="robots" content="noindex">';
+
   document::$layout = 'default';
 
   if (!empty($_GET['code'])) {
     http_response_code((int)$_GET['code']);
   }
 
-	if (preg_match('#\.(avif|gif|jpg|png|webp)$#', route::$request)) {
+	if (preg_match('#\.(a?png|avif|gif|jpe?g|webp)$#', route::$request)) {
     echo file_get_contents('images/no_image.png');
     exit;
   }
 
-  $_page = new ent_view();
-  $_page->snippets['code'] = http_response_code();
-
   switch (http_response_code()) {
 
     case 400:
-      $_page->snippets['title'] = 'Bad Request';
-      $_page->snippets['description'] = language::translate('error_400_bad_request', 'The server cannot or will not process the request due to a client error.');
+      $code = 400;
+      $title = 'Bad Request';
+      $description = language::translate('error_400_bad_request', 'The server cannot or will not process the request due to a client error.');
       break;
 
     case 401:
-      $_page->snippets['title'] = 'Unauthorized';
-      $_page->snippets['description'] = language::translate('error_401_unauthorized', 'You are not authorized to view the requested file.');
+      $code = 401;
+      $title = 'Unauthorized';
+      $description = language::translate('error_401_unauthorized', 'You are not authorized to view the requested page.');
       break;
 
     case 403:
-      $_page->snippets['title'] = 'Forbidden';
-      $_page->snippets['description'] = language::translate('error_403_forbidden', 'Access to the requested file is forbidden.');
+      $code = 403;
+      $title = 'Forbidden';
+      $description = language::translate('error_403_forbidden', 'Access to the requested page is forbidden.');
       break;
 
     case 404:
-      $_page->snippets['title'] = 'Not Found';
-      $_page->snippets['description'] = language::translate('error_404_not_found', 'The requested file could not be found.');
+      $code = 404;
+      $title = 'Not Found';
+      $description = language::translate('error_404_not_found', 'The requested page could not be found.');
       break;
 
     case 410:
-      $_page->snippets['title'] = 'Gone';
-      $_page->snippets['description'] = language::translate('error_410_gone', 'The requested page is no longer available.');
+      $code = 410;
+      $title = 'Gone';
+      $description = language::translate('error_410_gone', 'The requested page is no longer available.');
       break;
 
     default:
-      http_response_code(500);
-      $_page->snippets['title'] = 'Internal Server Error';
-      $_page->snippets['description'] = language::translate('error_500_internal_server_error', 'Oops! That was not meant to happen. We don\'t know how to handle your request.');
+      http_response_code($code = 500);
+      $title = 'Internal Server Error';
+      $description = language::translate('error_500_internal_server_error', 'Oops! That was not meant to happen. We don\'t know how to handle your request.');
       break;
   }
 
+  $_page = new ent_view();
+  $_page->snippets = [
+    'code' => $code,
+    'title' => $title,
+    'description' => $description,
+  ];
   echo $_page->stitch('pages/error_document');
