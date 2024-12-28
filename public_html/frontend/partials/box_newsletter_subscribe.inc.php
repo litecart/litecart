d82e59347c4cce0cfcbf12a8f4c8dcc45beb9be1
timint <info@tim-international.net>
2024-12-28 23:34:08 +0100

    * Hide newsletter subscribe form if customer is already signed up

--- public_html/frontend/partials/box_newsletter_subscribe.inc.php
+++ public_html/frontend/partials/box_newsletter_subscribe.inc.php
@@ -7,6 +7,14 @@
 	 *   ~/frontend/templates/default/partials/box_newsletter_subscribe.inc.php
 	 */
 
+	if (customer::$data['email'] && database::query(
+		"select id from ". DB_TABLE_PREFIX ."newsletter_recipients
+		where lower(email) = '". database::input(strtolower(customer::$data['email'])) ."'
+		limit 1;"
+	)->num_rows) {
+		return;
+	}
+
 	$box_newsletter_subscribe = new ent_view('app://frontend/templates/'.settings::get('template').'/partials/box_newsletter_subscribe.inc.php');
 
 	echo $box_newsletter_subscribe->render();
