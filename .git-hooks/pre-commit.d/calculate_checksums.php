bc28750e855a20f7fb8a16c8d210fa1c2c1a57bb
timint <info@tim-international.net>
2025-04-10 01:18:11 +0200

    * Checksum updates

--- .git-hooks/pre-commit.d/calculate_checksums.php
+++ .git-hooks/pre-commit.d/calculate_checksums.php
@@ -2,7 +2,7 @@
 
 	$checksums_file = 'public_html/install/checksums.md5';
 	$tracked_files = preg_split('#(\r\n?|\n)#', shell_exec('git ls-files'), -1, PREG_SPLIT_NO_EMPTY);
-	$committed_files = preg_split('#(\r\n?|\n)#', shell_exec('git diff --cached --name-only 2>&1'), -1, PREG_SPLIT_NO_EMPTY);
+	$committed_files = preg_split('#(\r\n?|\n)#', shell_exec('git diff --cached --name-only 2>&1') ?: '', -1, PREG_SPLIT_NO_EMPTY);
 
 	// Create list of files for checksums
 	$checksums = [];
@@ -12,7 +12,7 @@
 		$checksums[preg_replace('#^public_html/#', '', $file)] = str_repeat('0', 32);
 	}
 
-	// Reinsert checksums
+	// Reinsert previous checksums
 	if (is_file($checksums_file)) {
 		$checksum_files = preg_split('#(\r\n?|\n)#', file_get_contents($checksums_file), -1, PREG_SPLIT_NO_EMPTY);
 		foreach ($checksum_files as $line) {
@@ -41,10 +41,9 @@
 			$blob = file_get_contents($tmp_file);
 
 			$checksum = md5(preg_replace('#(\r\n?|\n)#', "\n", $blob));
-			if ($checksum == $checksums[$short_file]) {
-				//echo 'Checksum for '. $short_file .' is unchanged' . PHP_EOL;
-			} else {
-				echo 'Checksum for '. $short_file .' has been updated' . PHP_EOL;
+			if ($checksum != $checksums[$short_file]) {
+				$checksums[$short_file] = $checksum;
+				echo 'Updating '. $short_file . PHP_EOL;
 			}
 
 			unlink($tmp_file);
