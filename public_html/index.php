a0c463be22088cc37775ad22cfc12c019286df9a
timint <info@tim-international.net>
2025-04-25 11:25:05 +0200

    + Add CLI support for main script to poke background job engine

--- public_html/index.php
+++ public_html/index.php
@@ -14,6 +14,38 @@
 
 	require_once 'includes/app_header.inc.php';
 
+	// Process a CLI request
+	if ($_SERVER['SERVER_SOFTWARE'] == 'CLI') {
+
+		if (!isset($argv[1]) || (in_array($argv[1], ['help', '-h', '--help', '/?']))) {
+			echo implode(PHP_EOL, [
+				'',
+				PLATFORM_NAME .'® '. PLATFORM_VERSION,
+				'Copyright (c) '. date('Y') .' LiteCart AB',
+				'https://www.litecart.net/',
+				'Usage: php '. basename(__FILE__) .' [command]',
+				'',
+				'Command:',
+				'  push_jobs          Run the background jobs',
+				'',
+			]);
+			exit;
+		}
+
+		switch ($argv[1]) {
+
+			case 'push_jobs':
+				// Run the background jobs
+				require_once 'app://frontend/pages/push_jobs.inc.php';
+				exit;
+
+			default:
+				echo 'Unknown command: '. $argv[1] . PHP_EOL;
+				echo 'Run "php '. basename(__FILE__) .' help" for a list of commands.' . PHP_EOL;
+				exit(1);
+		}
+	}
+
 	// Recognize some destinations
 	route::load('app://frontend/routes/url_*.inc.php');
 	route::load('app://backend/routes/url_*.inc.php');
@@ -38,7 +70,8 @@
 	// Run operations before processing the route
 	event::fire('before_capture');
 
-	// Process the route and capture the content
+	// Process the route
 	route::process();
 
+	// Run operations after processing the route
 	require_once 'app://includes/app_footer.inc.php';
