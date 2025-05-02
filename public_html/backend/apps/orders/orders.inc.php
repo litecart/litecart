82445b386016c7296f761e6e70961624fa2c9309
bbkt <bbkt@s22.us>
2025-05-02 21:27:12 +0000

    orders.inc.php edited online with Bitbucket

--- public_html/backend/apps/orders/orders.inc.php
+++ public_html/backend/apps/orders/orders.inc.php
@@ -413,11 +413,11 @@ table .icon-star-o:hover {
 					<td><?php echo !empty($order['starred']) ? functions::draw_fonticon('icon-star', 'style="color: #f2b01e;"') : functions::draw_fonticon('icon-star-o', 'style="color: #ccc;"'); ?></td>
 					<td>
 						<a class="link" href="<?php echo document::href_ilink(__APP__.'/edit_order', ['order_id' => $order['id'], 'redirect_url' => $_SERVER['REQUEST_URI']]); ?>">
-							<?php echo functions::draw_fonticon($order['customer_company'] ? 'icon-building' : 'icon-user', 'style="opacity: .5;"'); ?>
-							<?php echo $order['customer_company'] ?: $order['customer_firstname'] .' '. $order['customer_lastname']; ?><?php if (!$order['customer_id']) echo ' <em>('. language::translate('title_guest', 'Guest') .')</em>'; ?>
+							<?php echo functions::draw_fonticon($order['billing_company'] ? 'icon-building' : 'icon-user', 'style="opacity: .5;"'); ?>
+							<?php echo $order['billing_company'] ?: $order['billing_firstname'] .' '. $order['billing_lastname']; ?><?php if (!$order['customer_id']) echo ' <em>('. language::translate('title_guest', 'Guest') .')</em>'; ?>
 						</a>
 					</td>
-					<td><?php echo $order['customer_country_code']; ?></td>
+					<td><?php echo $order['billing_country_code']; ?></td>
 					<td><?php echo $order['payment_option_name']; ?></td>
 					<td class="text-center"><?php if (!is_null($order['sufficient_stock'])) echo $order['sufficient_stock'] ? functions::draw_fonticon('icon-check', 'style="color: #88cc44;"') : functions::draw_fonticon('icon-times', 'style="color: #ff6644;"'); ?></td>
 					<td class="text-end"><?php echo currency::format($order['total'], false, $order['currency_code'], $order['currency_value']); ?></td>
