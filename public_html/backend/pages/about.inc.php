76954d0cde6a536ea0c6a065d96539bf5ac856d4
timint <info@tim-international.net>
2025-07-02 21:25:50 +0200

    * Skip using separate views for backend content

--- public_html/backend/pages/about.inc.php
+++ public_html/backend/pages/about.inc.php
@@ -86,6 +86,48 @@
 		}
 	}
 
+	$machine = [
+
+			'name' => php_uname('n'),
+			'architecture' => php_uname('m'),
+			'os' => [
+				'name' => php_uname('s') .' '. php_uname('r'),
+				'version' => php_uname('v'),
+			],
+			'ip_address' => $_SERVER['SERVER_ADDR'],
+			'hostname' => gethostbyaddr($_SERVER['SERVER_ADDR']),
+			'cpu_usage' => fallback($cpu_usage, ''),
+			'memory_usage' => fallback($memory_usage, ''),
+			'uptime' =>  fallback($uptime, ''),
+	];
+
+	$web_server = [
+		'name' => fallback($_SERVER['SERVER_SOFTWARE'], ''),
+		'sapi' => php_sapi_name(),
+		'current_user' => get_current_user(),
+		'loaded_modules' => function_exists('apache_get_modules') ? apache_get_modules() : [],
+	];
+
+	$php = [
+		'version' => PHP_VERSION .' ('. ((PHP_INT_SIZE === 8) ? '64-bit' : '32-bit') .')',
+		'whoami' => (function_exists('exec') && !in_array('exec', preg_split('#\s*,\s*#', ini_get('disabled_functions')))) ? exec('whoami') : '',
+		'loaded_extensions' => (function_exists('get_loaded_extensions') && !in_array('get_loaded_extensions', preg_split('#\s*,\s*#', ini_get('disabled_functions')))) ? get_loaded_extensions() : [],
+		'disabled_functions' => ini_get('disabled_functions') ?  preg_split('#\s*,\s*#', ini_get('disabled_functions')) : [],
+		'memory_limit' => ini_get('memory_limit'),
+		'ini' => ini_get_all(null, false),
+	];
+
+	sort($php['loaded_extensions'], SORT_NATURAL);
+	sort($php['disabled_functions'], SORT_NATURAL);
+
+	$database = [
+			'name' => database::server_info(),
+			'library' => mysqli_get_client_info(),
+			'hostname' => DB_SERVER,
+			'user' => DB_USERNAME,
+			'database' => DB_DATABASE,
+	];
+
 	// Errors
 	$errors = [];
 
@@ -141,48 +183,338 @@
 		ini_set('memory_limit', $iniatial_memory_limit); // Restore limit
 	}
 
-	// Render view
-	$_page = new ent_view('app://backend/template/pages/about.inc.php');
+?>
+<style>
+.tabs {
+	background: transparent;
+	border-bottom: none;
+}
+.tabs .nav-link:not(.active) {
+	background: #eee;
+}
+.tab-content {
+	margin: 0;
+	background: none;
+}
+#box-about {
+	position: relative;
+	overflow: hidden;
+}
+#box-about table + table {
+	margin-top: 2em;
+}
+#box-about tbody th {
+	text-align: left;
+}
+#box-about tr > *:first-child {
+	width: 250px;
+}
+#box-about meter {
+	width: 500px;
+}
+#box-about .credits-wrapper {
+	position: absolute;
+	top: 2em;
+	inset-inline-end: 2em;
+	width: 400px;
+	height: 600px;
+	padding: 2em;
+	background: #fff;
+	border: 1px solid var(--default-border-color);
+	box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
+	border-radius: var(--border-radius);
+	overflow: hidden;
+	transform: translateY(0%);
+	z-index: 999;
+}
+#box-error-log tr.critical {
+	background: #c002;
+}
+#box-error-log td {
+	white-space: wrap !important;
+	cursor: default;
+}
+</style>
 
-	$_page->snippets = [
-		'machine' => [
-			'name' => php_uname('n'),
-			'architecture' => php_uname('m'),
-			'os' => [
-				'name' => php_uname('s') .' '. php_uname('r'),
-				'version' => php_uname('v'),
-			],
-			'ip_address' => $_SERVER['SERVER_ADDR'],
-			'hostname' => gethostbyaddr($_SERVER['SERVER_ADDR']),
-			'cpu_usage' => fallback($cpu_usage, ''),
-			'memory_usage' => fallback($memory_usage, ''),
-			'uptime' =>  fallback($uptime, ''),
-		],
-		'web_server' => [
-			'name' => fallback($_SERVER['SERVER_SOFTWARE'], ''),
-			'sapi' => php_sapi_name(),
-			'current_user' => get_current_user(),
-			'loaded_modules' => function_exists('apache_get_modules') ? apache_get_modules() : [],
-		],
-		'php' => [
-			'version' => PHP_VERSION .' ('. ((PHP_INT_SIZE === 8) ? '64-bit' : '32-bit') .')',
-			'whoami' => (function_exists('exec') && !in_array('exec', preg_split('#\s*,\s*#', ini_get('disabled_functions')))) ? exec('whoami') : '',
-			'loaded_extensions' => (function_exists('get_loaded_extensions') && !in_array('get_loaded_extensions', preg_split('#\s*,\s*#', ini_get('disabled_functions')))) ? get_loaded_extensions() : [],
-			'disabled_functions' => ini_get('disabled_functions') ?  preg_split('#\s*,\s*#', ini_get('disabled_functions')) : [],
-			'memory_limit' => ini_get('memory_limit'),
-			'ini' => ini_get_all(null, false),
-		],
-		'database' => [
-			'name' => database::server_info(),
-			'library' => mysqli_get_client_info(),
-			'hostname' => DB_SERVER,
-			'user' => DB_USERNAME,
-			'database' => DB_DATABASE,
-		],
-		'errors' => $errors,
-	];
+<nav class="tabs">
+	<a class="tab-item active" data-toggle="tab" href="#tab-system"><?php echo t('title_system', 'System'); ?></a>
+	<a class="tab-item" data-toggle="tab" href="#tab-php-ini"><?php echo t('title_php_ini', 'PHP.ini'); ?></a>
+	<a class="tab-item" data-toggle="tab" href="#tab-errors"><?php echo t('title_error_log', 'Error Log'); ?></a>
+</nav>
 
-	sort($_page->snippets['php']['loaded_extensions'], SORT_NATURAL);
-	sort($_page->snippets['php']['disabled_functions'], SORT_NATURAL);
+<div class="tab-contents">
+	<div id="tab-system" class="tab-content active">
+		<div id="box-about" class="card">
+			<div class="card-header">
+				<div class="card-title">
+					<?php echo PLATFORM_NAME; ?> <?php echo PLATFORM_VERSION; ?>
+				</div>
+			</div>
 
-	echo $_page;
+			<table class="table data-table">
+				<thead>
+					<tr>
+						<th colspan="2">Application</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th>System Path</th>
+						<td><?php echo FS_DIR_APP; ?></td>
+					</tr>
+					<tr>
+						<th>Document Root</th>
+						<td><?php echo DOCUMENT_ROOT; ?></td>
+					</tr>
+					<tr>
+						<th>Web Path</th>
+						<td><?php echo WS_DIR_APP; ?></td>
+					</tr>
+				</tbody>
+			</table>
+
+			<table class="table data-table">
+				<thead>
+					<tr>
+						<th colspan="2">Machine</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th>Name</th>
+						<td><?php echo $machine['name']; ?></td>
+					</tr>
+					<tr>
+						<th>IP Address</th>
+						<td><?php echo $machine['ip_address']; ?></td>
+					</tr>
+					<tr>
+						<th>Hostname</th>
+						<td><?php echo $machine['hostname']; ?></td>
+					</tr>
+					<tr>
+						<th>Operating System</th>
+						<td><?php echo $machine['os']['name']; ?></td>
+					</tr>
+					<tr>
+						<th>Operating System Version</th>
+						<td><?php echo $machine['os']['version']; ?></td>
+					</tr>
+					<tr>
+						<th>System Architecture</th>
+						<td><?php echo $machine['architecture']; ?></td>
+					</tr>
+					<tr>
+						<th>CPU Usage</th>
+						<td><?php echo !empty($machine['cpu_usage']) ? '<meter class="memory-usage" value="'. (float)$machine['cpu_usage'] .'" max="100" min="0" high="30" low="10" optimum="5"></meter>' : '<em>n/a</em>'; ?></td>
+					</tr>
+					<tr>
+						<th>Memory Usage</th>
+						<td><?php echo !empty($machine['memory_usage']) ? '<meter class="memory-usage" value="'. (float)$machine['memory_usage'] .'" max="100" min="0" high="30" low="10" optimum="5"></meter>' : '<em>n/a</em>'; ?></td>
+					</tr>
+					<tr>
+						<th>Uptime</th>
+						<td><?php echo fallback($uptime, '<em>n/a</em>'); ?></td>
+					</tr>
+				</tbody>
+			</table>
+
+			<table class="table data-table">
+				<thead>
+					<tr>
+						<th colspan="2">Web Server</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th>Daemon</th>
+						<td><?php echo fallback($web_server['name'], '<em>Unknown</em>'); ?></td>
+					</tr>
+					<tr>
+						<th>SAPI</th>
+						<td><?php echo $web_server['sapi']; ?></td>
+					</tr>
+					<tr>
+						<th>Current User</th>
+						<td><?php echo $web_server['current_user']; ?></td>
+					</tr>
+					<tr>
+						<th>Enabled Modules</th>
+						<td><?php echo !empty($web_server['loaded_modules']) ? implode(', ', $web_server['loaded_modules']) : '<em>n/a</em>'; ?></td>
+					</tr>
+				</tbody>
+			</table>
+
+			<table class="table data-table">
+				<thead>
+					<tr>
+						<th colspan="2">PHP</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th>Version</th>
+						<td><?php echo $php['version']; ?></td>
+					</tr>
+					<tr>
+						<th>Whoami</th>
+						<td><?php echo fallback($php['whoami'], '<em>Unknown</em>'); ?></td>
+					</tr>
+					<tr>
+						<th>PHP Extensions</th>
+						<td style="columns: 100px auto;"><div><?php echo !empty($php['loaded_extensions']) ? implode('</div><div>', $php['loaded_extensions']) : '<em>None</em>'; ?></div></td>
+					</tr>
+					<tr>
+						<th>Disabled PHP Functions</th>
+						<td><?php echo !empty($php['disabled_functions']) ? implode(', ', $php['disabled_functions']) : '<em>None</em>'; ?></td>
+					</tr>
+					<tr>
+						<th>Memory Limit</th>
+						<td><?php echo fallback($php['memory_limit'], '<em>n/a</em>'); ?></td>
+					</tr>
+				</tbody>
+			</table>
+
+			<table class="table data-table">
+				<thead>
+					<tr>
+						<th colspan="2">Database</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th>Server Name</th>
+						<td><?php echo $database['name']; ?></td>
+					</tr>
+					<tr>
+						<th>Client Library</th>
+						<td><?php echo $database['library']; ?></td>
+					</tr>
+					<tr>
+						<th>Hostname</th>
+						<td><?php echo $database['hostname']; ?></td>
+					</tr>
+					<tr>
+						<th>User</th>
+						<td><?php echo $database['user']; ?></td>
+					</tr>
+					<tr>
+						<th>Database</th>
+						<td><?php echo $database['database']; ?></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+	<div id="tab-php-ini" class="tab-content">
+		<div class="card">
+			<div class="card-header">
+				<div class="card-title">
+					<?php echo t('title_php_configuration', 'PHP Configuration'); ?>
+				</div>
+				<div>&nbsp;</div>
+			</div>
+
+			<div class="card-action">
+				<?php echo functions::form_input_search('filter', true, 'placeholder="'. t('title_filter', 'Filter') .'"'); ?>
+			</div>
+
+			<table id="php-config" class="table data-table">
+				<thead>
+					<tr>
+						<th><?php echo t('title_setting', 'Setting'); ?></th>
+						<th><?php echo t('title_value', 'Value'); ?></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach ($php['ini'] as $key => $value) { ?>
+					<tr>
+						<td><tt><?php echo functions::escape_html($key); ?></tt></td>
+						<td><?php echo functions::escape_html($value); ?></td>
+					</tr>
+					<?php } ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+	<div id="tab-errors" class="tab-content">
+		<div id="box-error-log" class="card">
+			<div class="card-header">
+				<div class="card-title">
+					<?php echo t('title_error_log', 'Error Log'); ?>
+				</div>
+			</div>
+
+			<?php echo functions::form_begin('errors_form', 'post'); ?>
+
+			<table class="table data-table">
+				<thead>
+					<tr>
+						<th><?php echo functions::draw_fonticon('icon-square-check checkbox-toggle', 'data-toggle="checkbox-toggle"'); ?></th>
+						<th class="main"><?php echo t('title_error', 'Error'); ?></th>
+						<th><?php echo t('title_occurrences', 'Occurrences'); ?></th>
+						<th><?php echo t('title_last_occurrence', 'Last Occurrence'); ?></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach ($errors as $error) { ?>
+					<tr>
+						<td><?php echo functions::form_checkbox('errors[]', $error['error'], !empty($_POST['parse']) ? 'disabled' : ''); ?></td>
+						<td style="white-space: normal;"><?php echo functions::escape_html($error['error']); ?><br>
+							<div class="backtrace">
+								<?php echo nl2br(functions::escape_html($error['backtrace'])); ?>
+							</div>
+						</td>
+						<td class="text-center"><?php echo $error['occurrences']; ?></td>
+						<td><?php echo functions::datetime_when($error['last_occurrence']); ?></td>
+					</tr>
+					<?php } ?>
+				</tbody>
+			</table>
+
+			<div class="card-body">
+				<div id="actions">
+					<?php echo functions::form_button_predefined('delete'); ?>
+				</div>
+			</div>
+
+			<?php echo functions::form_end(); ?>
+		</div>
+	</div>
+</div>
+
+<script>
+	<?php if (!empty($machine['cpu_usage']) || !empty($machine['memory_usage'])) { ?>
+	setInterval(function() {
+		$.ajax({
+			cache: false,
+			dataType: 'html',
+			success: function(result) {
+				var $cpu_usage = $('meter.cpu-usage', result);
+				var $memory_usage = $('meter.memory-usage', result);
+				$('meter.cpu-usage').replaceWith($cpu_usage)
+				$('meter.memory-usage').replaceWith($memory_usage)
+			},
+		});
+	}, 3000);
+	<?php } ?>
+
+	// Filter
+	$('input[name="filter"]').on('input', function() {
+		var filter = $(this).val().toLowerCase();
+		$('#php-config tbody tr').each(function() {
+			if ($(this).text().toLowerCase().indexOf(filter) !== -1) {
+				$(this).show();
+			} else {
+				$(this).hide();
+			}
+		});
+	});
+
+	// Checkbox toggle
+	$('.data-table :checkbox').on('change', function() {
+		$('#actions').prop('disabled', !$('.data-table :checked').length);
+	}).first().trigger('change');
+</script>
--- public_html/backend/pages/verify.inc.php
+++ public_html/backend/pages/verify.inc.php
@@ -88,5 +88,97 @@
 		}
 	}
 
-	$_page = new ent_view('app://backend/template/pages/verify.inc.php');
-	echo $_page;
+?>
+<style>
+html {
+	background: #f8f8f8;
+}
+
+body {
+	display: flex;
+	width: 100vw;
+	height: 100vh;
+	background: url(<?php echo document::href_rlink('app://backend/template/images/background.svg'); ?>);
+	background-size: cover;
+}
+html.dark-mode body {
+	background: #1a2133;
+}
+
+#box-verify-identity {
+	width: 360px;
+	margin: auto;
+	border-radius: 0px 25px 0px 25px;
+	box-shadow: 0px 0px 60px rgba(0, 0, 0, .25);
+	overflow: hidden;
+}
+#box-verify-identity .card-header a {
+	display: block;
+}
+#box-verify-identity .card-header img {
+	margin: 0 auto;
+	max-width: 250px;
+	max-height: 100px;
+}
+
+input[autocomplete="one-time-code"] {
+	--otc-ls: 2ch;
+	--otc-gap: 1.25;
+	--_otp-bgsz: calc(var(--otc-ls) + 1ch);
+
+	all: unset;
+	background: linear-gradient(90deg, var(--otc-bg, #eee) calc(var(--otc-gap) * var(--otc-ls)), transparent 0) 0 0 / var(--_otp-bgsz) 100%;
+	caret-color: var(--otc-cc, #333);
+	clip-path: inset(0% calc(var(--otc-ls) / 2) 0% 0%);
+	font-family: monospace;
+	font-size: var(--otc-fz, 2.25em);
+	font-weight: 700;
+	inline-size: calc(6 * var(--_otp-bgsz));
+	letter-spacing: var(--otc-ls);
+	padding-block: var(--otc-pb, 1ch);
+	padding-inline-start: calc(((var(--otc-ls) - 1ch) / 2) * var(--otc-gap));
+}
+.selector {
+	caret-shape: block;
+}
+
+</style>
+
+<section id="box-verify-identity">
+	<div class="card" style="margin: 0;">
+		<div class="card-header text-center">
+			<a href="<?php echo document::href_ilink(''); ?>">
+				<img src="<?php echo document::href_rlink('storage://images/logotype.png'); ?>" alt="<?php echo settings::get('store_name'); ?>">
+			</a>
+		</div>
+
+		<?php echo functions::form_begin('authentication_form', 'post'); ?>
+			<?php echo functions::form_input_hidden('redirect_url', true); ?>
+
+			<div class="card-body">
+
+				{{notices}}
+
+				<h1><?php echo t('title_two_factor_authentication', 'Two-Factor Authentication'); ?></h1>
+
+				<label class="form-group">
+					<div class="form-label"><?php echo t('title_verification_code', 'Verification Code'); ?></div>
+					<?php echo functions::form_input_text('code', '', 'autocomplete="one-time-code" inputmode="numeric" maxlength="6" pattern="\d{6}"'); ?>
+				</label>
+
+				<label class="form-group">
+					<?php echo functions::form_button('verify', t('title_verify', 'Verify'), 'submit', 'class="btn btn-default btn-block btn-lg"'); ?>
+				</label>
+
+				<label class="form-group text-center">
+					<?php echo functions::form_button('resend', t('title_resend_code', 'Resend Code'), 'submit', 'class="btn btn-default btn-sm"'); ?>
+				</label>
+			</div>
+
+		<?php echo functions::form_end(); ?>
+	</div>
+</section>
+
+<script>
+	$('input[name="code"]').trigger('focus');
+</script>
