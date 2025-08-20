fef4fa1a8383e50b9a93e27f1e223f5f53ca8e4b
timint <info@tim-international.net>
2025-08-20 05:50:06 +0200

    + New session handler abandoning PHP session_*()

--- public_html/includes/nodes/nod_session.inc.php
+++ public_html/includes/nodes/nod_session.inc.php
@@ -6,79 +6,169 @@
 
 		public static function init() {
 
-			register_shutdown_function(['session', 'close']);
+			$_SESSION = &self::$data;
 
-			if (!self::start()) {
-				trigger_error('Failed to start a session', E_USER_WARNING);
-			}
+			register_shutdown_function([__CLASS__, 'save']);
+			//event::register('shutdown', [__CLASS__, 'save']);
 
-			self::$data = &$_SESSION;
+			if (!empty($_COOKIE['LCSESSID']) && self::validate_id($_COOKIE['LCSESSID'])) {
+				self::load($_COOKIE['LCSESSID']);
 
-			if (empty(self::$data['last_ip_address'])) {
-				self::$data['last_ip_address'] = $_SERVER['REMOTE_ADDR'];
-			}
+			} else {
 
-			if (empty(self::$data['last_user_agent'])) {
-				self::$data['last_user_agent'] = $_SERVER['HTTP_USER_AGENT'];
-			}
+				self::reset();
 
-			if ($_SERVER['REMOTE_ADDR'] != self::$data['last_ip_address']
-			|| $_SERVER['HTTP_USER_AGENT'] != self::$data['last_user_agent']) {
-				self::$data['last_ip_address'] = $_SERVER['REMOTE_ADDR'];
-				self::$data['last_user_agent'] = $_SERVER['HTTP_USER_AGENT'];
-				self::regenerate_id();
-			}
+				if (empty(self::$data['last_ip_address'])) {
+					self::$data['last_ip_address'] = $_SERVER['REMOTE_ADDR'];
+				}
 
-			// Collect Urchin Tracking Module (UTM) data
-			if (empty(self::$data['utm_data'])) {
-				self::$data['utm_data'] = [];
-			}
+				if (empty(self::$data['last_user_agent'])) {
+					self::$data['last_user_agent'] = $_SERVER['HTTP_USER_AGENT'];
+				}
 
-			foreach ([
-				'source',
-				'medium',
-				'campaign',
-				'term',
-				'content',
-			] as $key) {
-				if (!empty($_GET['utm_'.$key])) {
-					self::$data['utm'][$key] = $_GET['utm_'.$key];
+				if ($_SERVER['REMOTE_ADDR'] != self::$data['last_ip_address']
+				|| $_SERVER['HTTP_USER_AGENT'] != self::$data['last_user_agent']) {
+					self::$data['last_ip_address'] = $_SERVER['REMOTE_ADDR'];
+					self::$data['last_user_agent'] = $_SERVER['HTTP_USER_AGENT'];
+					self::regenerate_id();
+				}
+
+				// Collect Urchin Tracking Module (UTM) data
+				if (empty(self::$data['utm_data'])) {
+					self::$data['utm_data'] = [];
+				}
+
+				foreach ([
+					'source',
+					'medium',
+					'campaign',
+					'term',
+					'content',
+				] as $key) {
+					if (!empty($_GET['utm_'.$key])) {
+						self::$data['utm'][$key] = $_GET['utm_'.$key];
+					}
+				}
+
+				if (empty(self::$data['is_bot'])) { // Needs an addon to detect bots
+					database::query(
+						"insert into ". DB_TABLE_PREFIX ."statistics
+						(type, entity_type, entity_id, measure_group_type, measure_group_value, `count`)
+						values ('page_views', 'domain', '". database::input($_SERVER['HTTP_HOST']) ."', 'day', '". database::input(date('Y-m-d')) ."', 1)
+						on duplicate key update
+						`count` = `count` + 1;"
+					);
 				}
 			}
 
-			if (empty(self::$data['is_bot'])) { // Needs an addon to detect bots
-				database::query(
-					"insert into ". DB_TABLE_PREFIX ."statistics
-					(type, entity_type, entity_id, measure_group_type, measure_group_value, `count`)
-					values ('page_views', 'domain', '". database::input($_SERVER['HTTP_HOST']) ."', 'day', '". database::input(date('Y-m-d')) ."', 1)
-					on duplicate key update
-					`count` = `count` + 1;"
-				);
+			// Keep track on some updated information
+			self::$data['last_url'] = document::link() ?: null;
+
+			// Count page view when the current request differs from the last one
+			$current_request = $_SERVER['REQUEST_METHOD'] .' '. $_SERVER['REQUEST_URI'];
+			if (isset(self::$data['last_request']) && self::$data['last_request'] !== $current_request) {
+				self::$data['page_views'] = (!empty(self::$data['page_views']) ? (int)self::$data['page_views'] : 0) + 1;
 			}
+
+			if (empty(self::$data['referrer']) && !empty($_SERVER['HTTP_REFERER'])) {
+				if (empty(self::$data['referrer']) && !empty($_SERVER['HTTP_REFERER'])) {
+					self::$data['referrer'] = $_SERVER['HTTP_REFERER'];
+				}
+			}
+
+			self::$data['last_request'] = $current_request;
+
 		}
 
-		public static function start() {
-			return session_start();
+		public static function reset() {
+			self::$data = (new ent_session())->data;
 		}
 
-		public static function close() {
-			return session_write_close();
+		public static function load($session_id) {
+
+			self::reset();
+
+			$session = database::query(
+				"select * from ". DB_TABLE_PREFIX ."sessions
+				where id = '". database::input($session_id) ."'
+				limit 1;"
+			)->fetch(function(&$row){
+				$row['data'] = $row['data'] ? json_decode($row['data'], true) : [];
+			});
+
+			if (!$session) {
+				return false;
+			}
+
+			self::$data = $session['data'];
+
+			return true;
 		}
 
-		public static function clear() {
-			return session_unset();
+		public static function save() {
+
+			// If we don't have an id we should generate one
+			if (empty(self::$data['id'])) {
+				self::generate_id();
+			}
+
+			// Save only the payload without pretty printing to reduce storage
+			database::query(
+				"insert into ". DB_TABLE_PREFIX ."sessions
+				(id, data, updated_at, created_at)
+				values (
+					'". database::input(self::$data['id']) ."',
+					'". database::input(json_encode(self::$data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE)) ."',
+					'". database::input(date('Y-m-d H:i:s')) ."',
+					'". database::input(date('Y-m-d H:i:s')) ."'
+				)
+				on duplicate key update
+					data = '". database::input(json_encode(self::$data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE)) ."',
+					expires_at = '". database::input(date('Y-m-d H:i:s', strtotime('+1 hour'))) ."',
+					updated_at = '". database::input(date('Y-m-d H:i:s')) ."';"
+			);
+
+			return database::affected_rows() ? true : false;
 		}
 
-		public static function destroy() {
-			session_unset();
-			return session_destroy();
+		public static function generate_id() {
+			$id = bin2hex(random_bytes(16));
+			self::$data['id'] = $id;
+			self::set_cookie();
+			return $id;
 		}
 
-		public static function get_id() {
-			return session_id();
+		public static function validate_id($session_id) {
+
+			if (preg_match('#^[0-9a-z]+$#i', $session_id)) {
+				return true;
+			}
+
+			return false;
 		}
 
 		public static function regenerate_id() {
-			return session_regenerate_id(true);
+
+			if (!empty(self::$data['id'])) {
+				database::query(
+					"update ". DB_TABLE_PREFIX ."sessions
+					set expires_at = '". database::input(date('Y-m-d H:i:s')) ."'
+					where id = '". database::input(self::$data['id']) ."'
+					limit 1;"
+				);
+			}
+
+			self::$data['id'] = self::generate_id();
+		}
+
+		public static function set_cookie() {
+
+			$is_secure = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+				|| (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https');
+
+			// Only use SameSite=None when cookie is Secure (required by modern browsers)
+			$samesite = $is_secure ? 'None' : 'Lax';
+
+			header('Set-Cookie: LCSESSID='. rawurlencode(self::$data['id']) .';Path=/;'. ($is_secure ? 'Secure;' : '') .'HttpOnly;SameSite=' . $samesite);
 		}
 	}
