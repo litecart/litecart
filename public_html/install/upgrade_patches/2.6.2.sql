a7b31bc9f24ab449e7f3602844be627c3a9206ea
timint <info@tim-international.net>
2025-10-17 11:37:22 +0200

    * Fix MySQL 8 index length

--- public_html/install/upgrade_patches/2.6.2.sql
+++ public_html/install/upgrade_patches/2.6.2.sql
@@ -1,4 +1,5 @@
 ALTER TABLE `lc_products_attributes`
+CHANGE `custom_value` `custom_value` VARCHAR(215) NOT NULL,
 DROP INDEX `id`,
 ADD UNIQUE INDEX `product_attribute` (`product_id`, `group_id`, `value_id`, `custom_value`) USING BTREE,
 ADD INDEX `custom_value` (`custom_value`);
