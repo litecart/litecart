a5ef242555223beb105c702c91a64c0be082eac2
timint <info@tim-international.net>
2025-10-17 12:41:40 +0200

    * Add missing upgrade default_print_paper_size

--- public_html/install/upgrade_patches/2.6.3.sql
+++ public_html/install/upgrade_patches/2.6.3.sql
@@ -7,3 +7,6 @@ ADD INDEX `end_date` (`end_date`);
 -- -----
 ALTER TABLE `lc_products_to_categories`
 ADD INDEX `category_id` (`category_id`);
+-- -----
+INSERT INTO `lc_settings` (`setting_group_key`, `type`, `title`, `description`, `key`, `value`, `function`, `priority`, `date_updated`, `date_created`) VALUES
+('defaults', 'local', 'Default Print Paper Size', 'Default paper size used for printing.', 'default_print_paper_size', 'A4', 'select("A5","A5R","A4","A4R","US-Letter")', 30, NOW(), NOW());
