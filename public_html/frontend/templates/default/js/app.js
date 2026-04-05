1295eeb719a79171f61446812b114f34b9a37b88
timint <info@tim-international.net>
2026-04-06 00:39:17 +0200

    + Add standalone fallback implementation of waitFor if LiteCore framework is not loaded

--- public_html/frontend/templates/default/js/app.js
+++ public_html/frontend/templates/default/js/app.js
@@ -442,3 +442,29 @@ waitFor('jQuery', ($) => {
 		});
 	});
 });
+
+
+/* Minimal waitFor() implementation
+ * Calls callback when objectName is defined in the global scope
+ * waitTime is the time between retries, in milliseconds (default 50ms)
+ * retries is the number of times to retry before giving up (default 100)
+ */
+window.waitFor = waitFor || ((objectName, callback, waitTime=50, retries=100) => {
+
+	if (typeof(objectName) !== 'string') {
+		throw new TypeError('First argument to waitFor() must be a string');
+	}
+
+	if (typeof(window[objectName]) !== 'undefined') {
+		callback(window[objectName]);
+
+	} else if (retries > 0) {
+
+		setTimeout(() => {
+			waitFor(objectName, callback, waitTime, --retries);
+		}, waitTime);
+
+	} else {
+		console.warn(`waitFor(${objectName}) timed out`);
+	}
+});
