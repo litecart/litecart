3dcbcb4f4fa54dc26c4a1a2db0f12fd0b436ed6b
timint <info@tim-international.net>
2026-04-06 13:47:52 +0200

    * Fix momentum scroll overflow

--- public_html/frontend/templates/default/partials/box_campaign_products.inc.php
+++ public_html/frontend/templates/default/partials/box_campaign_products.inc.php
@@ -4,11 +4,9 @@
 		<h2 class="card-title"><?php echo t('title_campaign_products', 'Campaign Products'); ?></h2>
 	</div>
 
-	<div class="card-body">
-		<div data-toggle="momentum-scroll">
-			<div class="listing products columns scroll-content">
-				<?php foreach ($products as $product) echo f::draw_listing_product($product); ?>
-			</div>
+	<div data-toggle="momentum-scroll">
+		<div class="listing products columns scroll-content">
+			<?php foreach ($products as $product) echo f::draw_listing_product($product); ?>
 		</div>
 	</div>
 
