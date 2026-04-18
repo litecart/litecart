c9a66e3478ee8d8e63b8f234bd3e4c9133d3223b
timint <info@tim-international.net>
2026-04-18 16:21:59 +0200

    - Remove Import/Export via CSV in favour of Data Studio

--- public_html/backend/apps/pages/config.inc.php
+++ public_html/backend/apps/pages/config.inc.php
@@ -12,21 +12,10 @@
 		],
 
 		'menu' => [
-			[
-				'title' => t('title_pages', 'Pages'),
-				'doc' => 'pages',
-				'params' => [],
-			],
-			[
-				'title' => t('title_csv_import_export', 'CSV Import/Export'),
-				'doc' => 'csv',
-				'params' => [],
-			],
 		],
 
 		'docs' => [
 			'pages' => 'pages.inc.php',
 			'edit_page' => 'edit_page.inc.php',
-			'csv' => 'csv.inc.php',
 		],
 	];
