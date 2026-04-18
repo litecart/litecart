4e637da2292165d7483c288650f2996509d0be2d
timint <info@tim-international.net>
2026-04-18 16:19:47 +0200

    * StreamWrapper security adjustments

--- public_html/includes/streams/stream_app.inc.php
+++ public_html/includes/streams/stream_app.inc.php
@@ -2,26 +2,76 @@
 
 	class stream_app {
 
+		private static $_cache = [];
 		private $_directory = [];
 		private $_stream;
 		public $context;
 
-		public function dir_opendir($path) {
+		public function dir_opendir($path, $options) {
+
+			$microtime = microtime(true);
+
 			$path = $this->_resolve_path($path);
-			$this->_directory = opendir($path);
+			$relative_path = preg_replace('#^'. preg_quote(FS_DIR_APP, '#') .'#', '', $path);
+
+			$this->_directory = [];
+
+			if (!isset(self::$_cache[$path])) {
+
+				// File System				
+				foreach (glob($path.'*', GLOB_NOSORT) as $file) { // glob() seems faster than opendir() for this operation
+
+					$file = str_replace('\\', '/', $file) . (is_dir($file) ? '/' : '');
+					$basename = basename($file) . (is_dir($file) ? '/' : '');
+
+					$this->_directory[$basename] = $file;
+				}
+
+				// Virtual File System
+				foreach (glob(FS_DIR_STORAGE .'addons/*/'.$relative_path.'*', GLOB_NOSORT) as $file) {
+
+					$file = str_replace('\\', '/', $file) . (is_dir($file) ? '/' : '');
+					$basename = basename($file) . (is_dir($file) ? '/' : '');
+
+					$skip_pattern = '(.cache/|.disabled/|/vmod\.xml$)';
+					if (preg_match('#^'. preg_quote(FS_DIR_STORAGE .'addons/', '#') .'[^/]+'. $skip_pattern .'#', $file)) continue;
+
+					$this->_directory[$basename] = $file;
+				}
+
+				uasort($this->_directory, function($a, $b){
+
+					if (is_dir($a) == is_dir($b)) {
+						return (basename($a) < basename($b)) ? -1 : 1;
+					}
+
+					return is_dir($a) ? -1 : 1;
+				});
+
+				if (class_exists('stats')) {
+					stats::$data['streamwrappers'] = microtime(true) - $microtime;
+				}
+
+				self::$_cache[$path] = $this->_directory;
+
+			} else {
+				$this->_directory = self::$_cache[$path];
+			}
+
 			return true;
 		}
 
 		public function dir_readdir() {
 
-			$file = readdir($this->_directory);
+			$result = key($this->_directory);
 
-			// Skip returning . and ..
-			if (is_string($file) && preg_match('#^\.{1,2}$#', $file)) {
-				return $this->dir_readdir();
+			if (!isset($result)) {
+				return false;
 			}
 
-			return $file;
+			next($this->_directory);
+
+			return $result;
 		}
 
 		public function dir_closedir() {
@@ -33,22 +83,38 @@
 		}
 
 		public function dir_rewinddir() {
-			return rewinddir($this->_directory);
+			reset($this->_directory);
+			return true;
 		}
 
 		public function mkdir(string $path, int $mode, int $options): bool {
-			trigger_error('Creating an app:// directory is prohibited', E_USER_WARNING);
-			return false;
+
+			if (!getenv('SUPER_MODE')) {
+				trigger_error('Creating an app:// directory is prohibited', E_USER_WARNING);
+				return false;
+			}
+
+			return mkdir($this->_resolve_path($path), $mode);
 		}
 
 		public function rename(string $path_from, string $path_to): bool {
-			trigger_error('Renaming an app:// resource is prohibited', E_USER_WARNING);
-			return false;
+
+			if (!getenv('SUPER_MODE')) {
+				trigger_error('Renaming an app:// resource is prohibited', E_USER_WARNING);
+				return false;
+			}
+
+			return rename($this->_resolve_path($path_from), $this->_resolve_path($path_to));
 		}
 
 		public function rmdir(string $path, int $options): bool {
-			trigger_error('Removing an app:// directory is prohibited', E_USER_WARNING);
-			return false;
+
+			if (!getenv('SUPER_MODE')) {
+				trigger_error('Removing an app:// directory is prohibited', E_USER_WARNING);
+				return false;
+			}
+
+			return rmdir($this->_resolve_path($path));
 		}
 
 		public function stream_cast(int $cast_as): object {
@@ -64,13 +130,23 @@
 		}
 
 		public function stream_flush(): bool {
-			trigger_error('Flushing data to an app:// resource is prohibited', E_USER_WARNING);
-			return false;
+			
+			if (!getenv('SUPER_MODE')) {
+				trigger_error('Flushing data to an app:// resource is prohibited', E_USER_WARNING);
+				return false;
+			}
+
+			return fflush($this->_stream);
 		}
 
 		public function stream_lock(int $operation): bool {
-			trigger_error('Adding a file lock for an app:// resource is prohibited', E_USER_WARNING);
-			return false;
+			
+			if (!getenv('SUPER_MODE')) {
+				trigger_error('Adding a file lock for an app:// resource is prohibited', E_USER_WARNING);
+				return false;
+			}
+
+			return flock($this->_stream, $operation);
 		}
 
 		public function stream_metadata(string $path, int $option, mixed $value): bool {
@@ -81,7 +157,6 @@
 		public function stream_open(string $path, string $mode, int $options, ?string &$opened_path): bool {
 
 			$path = $this->_resolve_path($path);
-			$path = vmod::check($path);
 			$mode = 'r'; // Force read-only
 
 			$this->_stream = fopen($path, $mode, $options, $opened_path);
@@ -110,29 +185,73 @@
 		}
 
 		public function stream_truncate(int $new_size): bool {
-			trigger_error('Truncating an app:// resource is prohibited', E_USER_WARNING);
-			return false;
+
+			if (!getenv('SUPER_MODE')) {
+				trigger_error('Truncating an app:// resource is prohibited', E_USER_WARNING);
+				return false;
+			}
+
+			return ftruncate($this->_stream, $new_size);
 		}
 
 		public function stream_write(string $data): int|bool {
-			trigger_error('Writing to an app:// resource is prohibited', E_USER_WARNING);
-			return 0;
+			
+			if (!getenv('SUPER_MODE')) {
+				trigger_error('Writing to an app:// resource is prohibited', E_USER_WARNING);
+				return 0;
+			}
+
+			return fwrite($this->_stream, $data);
 		}
 
 		public function unlink(string $path): bool {
-			trigger_error('Removing an app:// resource is prohibited', E_USER_WARNING);
-			return false;
+			
+			if (!getenv('SUPER_MODE')) {
+				trigger_error('Removing an app:// resource is prohibited', E_USER_WARNING);
+				return false;
+			}
+		
+			return unlink($this->_resolve_path($path));
 		}
 
 		public function url_stat(string $path, int $flags): array|false {
-			$path = $this->_resolve_path($path);
-			$path = vmod::check($path);
-			return file_exists($path) ? stat($path) : false;
+
+			$path = $this->_resolve_file($path);
+
+			if (!file_exists($path)) {
+				return false;
+			}
+
+			return stat($path);
 		}
 
 		## Non-Standard StreamWrapper Methods
 
 		private function _resolve_path($path) {
 			return preg_replace('#^app://#', FS_DIR_APP, str_replace('\\', '/', $path));
+		}
+
+		private function _resolve_file($path) {
+
+			$path = $this->_resolve_path($path);
+			$relative_path = preg_replace('#^'. preg_quote(FS_DIR_APP, '#') .'#', '', $path);
+			$parent_folder = dirname($path).'/';
+			$basename = basename($path);
+
+			if (isset(self::$_cache[$parent_folder])) {
+				if (isset(self::$_cache[$parent_folder][$basename])) {
+					return self::$_cache[$parent_folder][$basename];
+				}
+			}
+
+			foreach (glob(FS_DIR_STORAGE .'addons/*/'.$relative_path) as $file) {
+				$file = str_replace('\\', '/', $file);
+				if (preg_match('#^'. preg_quote(FS_DIR_STORAGE .'addons/', '#') .'[^/]+.disabled/#', $file)) continue;
+				$path = $file;
+			}
+
+			$path = vmod::check($path);
+
+			return $path;
 		}
 	}
