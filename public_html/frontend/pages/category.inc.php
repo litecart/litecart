57cb5fdc8df2912dd0b21362b4679934ccfe09d7
timint <info@tim-international.net>
2026-05-07 17:33:51 +0200

    * Fix query

--- public_html/frontend/pages/category.inc.php
+++ public_html/frontend/pages/category.inc.php
@@ -120,11 +120,12 @@
 
 	// Brands
 	$_page->snippets['brands'] = database::query(
-		"select id, name from ". DB_TABLE_PREFIX ."brands
+		"select id, name from ". DB_TABLE_PREFIX ."brands b
 		where id in (
-			select brand_id from ". DB_TABLE_PREFIX ."products
+			select p.brand_id from ". DB_TABLE_PREFIX ."products p
 			". (!empty($_GET['category_id']) ? " left join ". DB_TABLE_PREFIX ."products_to_categories pc on pc.product_id = p.id " : "") ."
-			where status		  and brand_id
+			where p.status
+			and p.brand_id
 			". (!empty($_GET['category_id']) ? "and pc.category_id = " . (int)$_GET['category_id']  : "") ."
 		)
 		order by b.name asc;"
