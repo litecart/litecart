11bcf25e25ab4a0f9ff27b168af0b9eb84ec29ac
s22 Tech <59073912+s22-tech@users.noreply.github.com>
2026-05-13 19:04:44 -0700

    Switching to named parameters
    If this is acceptable, I'll start working on other pages as well.

--- public_html/frontend/templates/default/pages/contact.inc.php
+++ public_html/frontend/templates/default/pages/contact.inc.php
@@ -9,42 +9,70 @@
 
 					<h1><?php echo t('title_contact_us', 'Contact Us'); ?></h1>
 
-					<?php echo f::form_begin('contact_form', 'post', null, true); ?>
+					<?php echo f::form_begin(
+						name      : 'contact_form',
+						method    : 'post',
+						action    : null,
+						multipart : true
+					); ?>
 
 						<div class="grid">
 							<div class="col-md-6">
 								<label class="form-group">
 									<div class="form-label"><?php echo t('title_firstname', 'First Name'); ?></div>
-									<?php echo f::form_input_text('firstname', true, 'required'); ?>
+									<?php echo f::form_input_text(
+										name       : 'firstname',
+										input      : true,
+										parameters : 'required',
+									); ?>
 								</label>
 							</div>
 
 							<div class="col-md-6">
 								<label class="form-group">
 									<div class="form-label"><?php echo t('title_lastname', 'Last Name'); ?></div>
-									<?php echo f::form_input_text('lastname', true, 'required'); ?>
+									<?php echo f::form_input_text(
+										name       : 'lastname',
+										input      : true,
+										parameters : 'required',
+									); ?>
 								</label>
 							</div>
 						</div>
 
 						<label class="form-group">
 							<div class="form-label"><?php echo t('title_email_address', 'Email Address'); ?></div>
-							<?php echo f::form_input_email('email', true, 'required'); ?>
+							<?php echo f::form_input_email(
+								name       : 'email',
+								input      : true,
+								parameters : 'required',
+							); ?>
 						</label>
 
 						<label class="form-group">
 							<div class="form-label"><?php echo t('title_subject', 'Subject'); ?></div>
-							<?php echo f::form_input_text('subject', true, 'required'); ?>
+							<?php echo f::form_input_text(
+								name       : 'subject',
+								input      : true,
+								parameters : 'required',
+							); ?>
 						</label>
 
 						<label class="form-group">
 							<div class="form-label"><?php echo t('title_message', 'Message'); ?></div>
-							<?php echo f::form_textarea('message', true, 'required style="height: 250px;"'); ?>
+							<?php echo f::form_textarea(
+								name       : 'message',
+								input      : true,
+								parameters : 'required style="height: 250px;"',
+							); ?>
 						</label>
 
 						<label class="form-group">
 							<div class="form-label"><?php echo t('title_attachments', 'Attachments'); ?></div>
-							<?php echo f::form_input_file('attachments[]', 'multiple accept=".jpg,.jpeg,.png,.gif,.webp,.avif,.txt,.doc,.docx,.pdf,.mp4"'); ?>
+							<?php echo f::form_input_file(
+								name       : 'attachments[]',
+								parameters : 'multiple accept=".jpg,.jpeg,.png,.gif,.webp,.avif,.txt,.doc,.docx,.pdf,.mp4"',
+							); ?>
 						</label>
 
 						<?php if (settings::get('captcha_enabled')) { ?>
@@ -55,7 +83,12 @@
 						<?php } ?>
 
 						<div>
-							<?php echo f::form_button('send', t('title_send', 'Send'), 'submit', 'style="font-weight: bold;"'); ?>
+							<?php echo f::form_button(
+								name       : 'send',
+								value      : t('title_send', 'Send'),
+								type       : 'submit',
+								parameters : 'style="font-weight: bold;"',
+							); ?>
 						</div>
 
 					<?php echo f::form_end(); ?>
