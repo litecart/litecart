e36c42e4688433a9ed45ac21abca41297f744b0b
T. Almroth <timmy.almroth@tim-international.net>
2026-05-27 09:54:43 +0200

    - Remove router.php as obsolete

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -42,7 +42,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: `php -S localhost:${PORT} -t public_html router.php`,
+    command: `php -S localhost:${PORT} -t public_html`,
     url: BASE_URL,
     timeout: 10_000,
     reuseExistingServer: !process.env.CI,
