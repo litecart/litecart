f4a63e04d4f8d76245930a78ad96d75f0265e4c1
T. Almroth <timmy.almroth@tim-international.net>
2026-07-13 03:11:42 +0200

    + Show PHP configuration file path

--- public_html/backend/pages/about.inc.php
+++ public_html/backend/pages/about.inc.php
@@ -127,6 +127,7 @@
 	];
 
 	$php = [
+		'config_path' => php_ini_loaded_file(),
 		'version' => PHP_VERSION .' ('. ((PHP_INT_SIZE === 8) ? '64-bit' : '32-bit') .')',
 		'whoami' => (function_exists('exec') && !in_array('exec', preg_split('#\s*,\s*#', ini_get('disabled_functions')))) ? exec('whoami') : '',
 		'loaded_extensions' => (function_exists('get_loaded_extensions') && !in_array('get_loaded_extensions', preg_split('#\s*,\s*#', ini_get('disabled_functions')))) ? get_loaded_extensions() : [],
@@ -399,6 +400,10 @@
 					<tr>
 						<th>Version</th>
 						<td><?php echo $php['version']; ?></td>
+					</tr>
+					<tr>
+						<th>Configuration File</th>
+						<td><?php echo $php['config_path'] ?? '<em>Unknown</em>'; ?></td>
 					</tr>
 					<tr>
 						<th>Whoami</th>
