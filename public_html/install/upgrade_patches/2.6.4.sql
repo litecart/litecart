40698ce1ea0ab00652be2c6d405f71742d7f327d
timint <info@tim-international.net>
2026-08-17 20:59:41 +0200

    * Fix upgrade

--- public_html/install/upgrade_patches/2.6.4.sql
+++ public_html/install/upgrade_patches/2.6.4.sql
@@ -1,9 +1,9 @@
 DELETE t1 FROM `lc_products_prices` t1
-JOIN `lc_products_prices` t2 ON t1.product_id = t2.product AND t1.id > t2.id;
+JOIN `lc_products_prices` t2 ON t1.product_id = t2.product_id AND t1.id > t2.id;
 -- -----
 ALTER TABLE `lc_products_prices`
 DROP KEY product_id,
 ADD UNIQUE KEY product_id (product_id);
--- ----
+-- -----
 ALTER TABLE `lc_products_images`
 ADD UNIQUE INDEX `product_id_filename` (`product_id`, `filename`);
