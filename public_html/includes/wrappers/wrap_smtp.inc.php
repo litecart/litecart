9c73ec2d657d02e93114d4b2cb3d7f7688ebd5d1
timint <info@tim-international.net>
2026-08-27 09:53:18 +0200

    * Fix colon

--- public_html/includes/wrappers/wrap_smtp.inc.php
+++ public_html/includes/wrappers/wrap_smtp.inc.php
@@ -58,7 +58,7 @@
 
         switch (true) {
 
-          case (in_array('CRAM-MD5', $auths));
+          case (in_array('CRAM-MD5', $auths)):
             $this->write("AUTH CRAM-MD5\r\n", 334);
             $challenge = base64_decode(substr($this->_last_response, 4));
             $this->write(base64_encode($this->_username .' '. hash_hmac('md5', $challenge, $this->_password)) . "\r\n", 235);
